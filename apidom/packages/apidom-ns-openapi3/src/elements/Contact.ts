fedaa827e1172883371678a163917ca796620227
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-06-10 20:43:09 +0200

    refactor(typescript): transform apidom-ns-openapi3 package
    Refs #18

---
+++ apidom/packages/apidom-ns-openapi3/src/elements/Contact.ts
@@ -0,0 +1,34 @@
+import { Attributes, Meta, ObjectElement } from 'minim';
+
+class Contact extends ObjectElement {
+  constructor(content: Record<string, unknown>, meta: Meta, attributes: Attributes) {
+    super(content, meta, attributes);
+    this.element = 'contact';
+  }
+
+  get name(): string {
+    return this.get('name').toValue();
+  }
+
+  set name(name: string) {
+    this.set('name', name);
+  }
+
+  get url(): string {
+    return this.get('url');
+  }
+
+  set url(url: string) {
+    this.set('url', url);
+  }
+
+  get email(): string {
+    return this.get('email');
+  }
+
+  set email(email: string) {
+    this.set('email', email);
+  }
+}
+
+export default Contact;

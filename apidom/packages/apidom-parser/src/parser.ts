ac58b8a060bf657685b16850bb02b8e9d4f4906d
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-06-10 23:20:14 +0200

    refactor(typescript): transform apidom-parser package (#27)
    Refs #18

---
+++ apidom/packages/apidom-parser/src/parser.ts
@@ -0,0 +1,59 @@
+import stampit from 'stampit';
+import { head } from 'ramda';
+import { isFunction, isArray, isUndefined, isNotUndefined } from 'ramda-adjunct';
+import { ParseResultElement } from 'apidom';
+
+interface ParserOptions {
+  mediaType?: string;
+}
+
+type Detect = (source: string) => boolean;
+type Parse = (source: string, options: ParserOptions) => ParseResultElement;
+
+interface ApiDOMParserAdapter {
+  detect?: Detect;
+  mediaTypes?: string[];
+  parse: Parse;
+}
+
+const ApiDOMParser = stampit().init(function ApiDOMParser() {
+  const adapters: ApiDOMParserAdapter[] = [];
+
+  const detectAdapterCandidates = (source: string) => {
+    return adapters.filter((adapter) => {
+      if (!isFunction(adapter.detect)) return false;
+
+      return adapter.detect(source);
+    });
+  };
+
+  const findAdapter = (source: string, mediaType: string | undefined) => {
+    if (isNotUndefined(mediaType)) {
+      return adapters.find((adapter) => {
+        if (!isArray(adapter.mediaTypes)) return false;
+        if (isUndefined(mediaType)) return false;
+
+        return adapter.mediaTypes.includes(mediaType);
+      });
+    }
+
+    return head(detectAdapterCandidates(source));
+  };
+
+  this.use = function use(adapter: ApiDOMParserAdapter) {
+    adapters.push(adapter);
+    return this;
+  };
+
+  this.parse = async function parse(source: string, options: ParserOptions = {}) {
+    const adapter = findAdapter(source, options.mediaType);
+
+    if (isUndefined(adapter)) {
+      return Promise.reject(new Error('Document did not match any registered parsers'));
+    }
+
+    return adapter.parse(source, options);
+  };
+});
+
+export default ApiDOMParser;
---
+++ apidom/packages/apidom-parser/test/index.ts
@@ -0,0 +1,21 @@
+import 'mocha';
+import fs from 'fs';
+import path from 'path';
+import * as apiDOM from 'apidom';
+/* @ts-ignore */
+import openapi3 from 'apidom-ns-openapi3';
+/* @ts-ignore */
+import openapi3Parser from 'apidom-parser-adapter-openapi3';
+import ApiDOMParser from '../src/parser';
+
+const parser = ApiDOMParser().use(openapi3Parser);
+const spec = fs.readFileSync(path.join(__dirname, 'fixtures', 'sample-api.json')).toString();
+const namespace = apiDOM.createNamespace(openapi3);
+
+describe('apidom-parser', function () {
+  it('test', async function () {
+    const parseResult = await parser.parse(spec);
+    console.log(parseResult);
+    console.log(apiDOM.toJSON(namespace, parseResult));
+  });
+});

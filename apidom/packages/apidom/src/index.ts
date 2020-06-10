61de5070a8598175899ca2c47f3967f1dbf95c13
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-06-10 23:08:02 +0200

    refactor(typescript): transform apidom-parser package
    Refs #18

--- apidom/packages/apidom/src/index.ts
+++ apidom/packages/apidom/src/index.ts
@@ -2,6 +2,10 @@ import { NamespacePlugin, Namespace, Element } from 'minim';
 import { Namespace as ApiDOMNamespace } from './namespace';
 
 export { default as namespace } from './namespace';
+export { default as AnnotationElement } from './elements/Annotation';
+export { default as CommentElement } from './elements/Comment';
+export { default as ParseResultElement } from './elements/ParseResult';
+export { default as SourceMapElement } from './elements/SourceMap';
 
 export const createNamespace = (namespacePlugin: NamespacePlugin): Namespace => {
   const namespace = new ApiDOMNamespace();

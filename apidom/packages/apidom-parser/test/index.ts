9d48576b01b43b2dff451614f6e483fce1e089ce
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-07-14 17:13:51 +0200

    feat: add OpenApi 3.1 support
    Refs #2

--- apidom/packages/apidom-parser/test/index.ts
+++ apidom/packages/apidom-parser/test/index.ts
@@ -3,14 +3,14 @@ import fs from 'fs';
 import path from 'path';
 import * as apiDOM from 'apidom';
 /* @ts-ignore */
-import openapi3 from 'apidom-ns-openapi3';
+import openapi3_1 from 'apidom-ns-openapi3-1';
 /* @ts-ignore */
-import * as openapi3adapter from 'apidom-parser-adapter-openapi3';
+import * as openapi3_1Adapter from 'apidom-parser-adapter-openapi3-1';
 import ApiDOMParser from '../src/parser';
 
-const parser = ApiDOMParser().use(openapi3adapter);
+const parser = ApiDOMParser().use(openapi3_1Adapter);
 const spec = fs.readFileSync(path.join(__dirname, 'fixtures', 'sample-api.json')).toString();
-const namespace = apiDOM.createNamespace(openapi3);
+const namespace = apiDOM.createNamespace(openapi3_1);
 
 describe('apidom-parser', function () {
   it('test', async function () {

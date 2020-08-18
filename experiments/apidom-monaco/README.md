571ba752ef9bb7baf408632a42cee57c799aa301
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-08-18 11:06:20 +0200

    chore: add ApiDOM <-> Monaco integration experiment
    Refs #51

---
+++ experiments/apidom-monaco/README.md
@@ -0,0 +1,23 @@
+# ApiDOM Monaco
+
+This repo demonstrates usage of ApiDOM with [Monaco editor](https://microsoft.github.io/monaco-editor/).
+
+### Installation
+
+Change directory to `ApiDOM` monorepo and run following commands:
+
+```sh
+ $ npm i
+ $ npm run build
+```
+
+Now change directory to this `apidom-monaco` and run following commands:
+
+```sh
+ $ npm i
+ $ npm start
+```
+
+Edit files in `./src` directory for experimenting with ApiDOM and Monca integration.
+
+ 
---
+++ experiments/apidom-monaco/src/index.ts
@@ -0,0 +1,22 @@
+import * as monaco from "monaco-editor-core";
+import * as apiDOM from "apidom";
+import ApiDOMParser from "apidom-parser";
+import * as openapi3_1Adapter from "apidom-parser-adapter-openapi3-1";
+
+import initClient from "./client";
+
+(self as any).MonacoEnvironment = {
+  getWorkerUrl: () => "./editor.worker.js",
+};
+
+(async () => {
+  const parser = ApiDOMParser();
+
+  parser.use(openapi3_1Adapter);
+  const parseResult = await parser.parse('{"openapi": "3.1.0"}');
+  const spec = apiDOM.toValue(parseResult.api);
+
+  console.dir(spec);
+})();
+
+initClient({ monaco, containerId: "editor" });

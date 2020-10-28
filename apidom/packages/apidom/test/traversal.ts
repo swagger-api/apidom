3d1c64dda1d52f761750a901bb0d1206171b4a47
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-10-28 16:12:03 +0100

    feat(traversal): add traverse function
    Refs https://github.com/swagger-api/oss-planning/issues/238

--- apidom/packages/apidom/test/traversal.ts
+++ apidom/packages/apidom/test/traversal.ts
@@ -1,4 +1,5 @@
 import { assert } from 'chai';
+import sinon from 'sinon';
 import { StringElement } from 'minim';
 import { F as stubFalse } from 'ramda';
 import {
@@ -6,6 +7,7 @@ import {
   reject,
   find,
   some,
+  traverse,
   createNamespace,
   isMemberElement,
   isNumberElement,
@@ -101,6 +103,144 @@ describe('traversal', function () {
           const isFound = some(stubFalse, objElement);
 
           assert.isFalse(isFound);
+        });
+      });
+    });
+  });
+
+  context('traverse', function () {
+    context('given ObjectElement', function () {
+      let objElement;
+      let callback;
+
+      beforeEach(function () {
+        objElement = new namespace.elements.Object({ a: 'b', c: 'd' });
+        callback = sinon.spy();
+      });
+
+      specify('should execute callback seven times', function () {
+        traverse(callback, objElement);
+        assert.strictEqual(callback.callCount, 7);
+      });
+
+      specify('should execute callback on this object', function () {
+        traverse(callback, objElement);
+        const { args } = callback.getCall(0);
+
+        assert.strictEqual(args[0], objElement);
+      });
+
+      context('and first key value pair', function () {
+        specify('should execute callback on pair', function () {
+          traverse(callback, objElement);
+          const { args } = callback.getCall(1);
+
+          assert.strictEqual(args[0], objElement.getMember('a'));
+        });
+
+        specify('should execute callback on key', function () {
+          traverse(callback, objElement);
+          const { args } = callback.getCall(2);
+
+          assert.strictEqual(args[0], objElement.getMember('a').key);
+        });
+
+        specify('should execute callback on value', function () {
+          traverse(callback, objElement);
+          const { args } = callback.getCall(3);
+
+          assert.strictEqual(args[0], objElement.get('a'));
+        });
+      });
+
+      context('and second key value pair', function () {
+        specify('should execute callback on pair', function () {
+          traverse(callback, objElement);
+          const { args } = callback.getCall(4);
+
+          assert.strictEqual(args[0], objElement.getMember('c'));
+        });
+
+        specify('should execute callback on key', function () {
+          traverse(callback, objElement);
+          const { args } = callback.getCall(5);
+
+          assert.strictEqual(args[0], objElement.getMember('c').key);
+        });
+
+        specify('should execute callback on value', function () {
+          traverse(callback, objElement);
+          const { args } = callback.getCall(6);
+
+          assert.strictEqual(args[0], objElement.get('c'));
+        });
+      });
+
+      context('given predicate', function () {
+        const predicate = (element) => isStringElement(element) && element.equals('a');
+        specify('should execute callback once', function () {
+          traverse({ callback, predicate }, objElement);
+          assert.strictEqual(callback.callCount, 1);
+        });
+
+        specify('should execute callback on filtered element', function () {
+          traverse({ callback, predicate }, objElement);
+          const { args } = callback.getCall(0);
+
+          assert.strictEqual(args[0], objElement.getMember('a').key);
+        });
+      });
+    });
+
+    context('given ArrayElement', function () {
+      let arrayElement;
+      let callback;
+
+      beforeEach(function () {
+        arrayElement = new namespace.elements.Array(['a', 'b']);
+        callback = sinon.spy();
+      });
+
+      specify('should execute callback three times', function () {
+        traverse(callback, arrayElement);
+
+        assert.strictEqual(callback.callCount, 3);
+      });
+
+      specify('should execute callback on this object', function () {
+        traverse(callback, arrayElement);
+        const { args } = callback.getCall(0);
+
+        assert.strictEqual(args[0], arrayElement);
+      });
+
+      specify('should execute callback on first array item', function () {
+        traverse(callback, arrayElement);
+        const { args } = callback.getCall(1);
+
+        assert.strictEqual(args[0], arrayElement.get(0));
+      });
+
+      specify('should execute callback on second array item', function () {
+        traverse(callback, arrayElement);
+        const { args } = callback.getCall(2);
+
+        assert.strictEqual(args[0], arrayElement.get(1));
+      });
+
+      context('given predicate', function () {
+        const predicate = (element) => isStringElement(element) && element.equals('a');
+
+        specify('should execute callback once', function () {
+          traverse({ callback, predicate }, arrayElement);
+          assert.strictEqual(callback.callCount, 1);
+        });
+
+        specify('should execute callback on filtered element', function () {
+          traverse({ callback, predicate }, arrayElement);
+          const { args } = callback.getCall(0);
+
+          assert.strictEqual(args[0], arrayElement.get(0));
         });
       });
     });

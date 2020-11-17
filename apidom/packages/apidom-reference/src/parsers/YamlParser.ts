263ef2c36ed3052ab5ce4bfe019e9a1f0c4804f2
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-11-17 14:23:45 +0100

    feat(reference): add yaml reference parser
    Refs https://github.com/swagger-api/oss-planning/issues/133

---
+++ apidom/packages/apidom-reference/src/parsers/YamlParser.ts
@@ -0,0 +1,47 @@
+import stampit from 'stampit';
+import { ParseResultElement } from 'apidom';
+// @ts-ignore
+import { parse } from 'apidom-parser-adapter-yaml-1-2';
+
+import File from '../util/File';
+import { ParserError } from '../util/errors';
+
+interface YamlParser {
+  allowEmpty: boolean;
+  sourceMap: boolean;
+
+  canParse(file: File): boolean;
+  parse(file: File): Promise<ParseResultElement>;
+}
+
+const YamlParser: stampit.Stamp<YamlParser> = stampit({
+  props: {
+    /**
+     * Whether to allow "empty" files. This includes zero-byte files.
+     */
+    allowEmpty: true,
+
+    /**
+     * Whether to generate source map during parsing.
+     */
+    sourceMap: true,
+  },
+  init(this: YamlParser, { allowEmpty = this.allowEmpty, sourceMap = this.sourceMap } = {}) {
+    this.allowEmpty = allowEmpty;
+    this.sourceMap = sourceMap;
+  },
+  methods: {
+    canParse(file: File): boolean {
+      return ['.yaml', '.yml'].includes(file.extension);
+    },
+    async parse(file: File): Promise<ParseResultElement> {
+      try {
+        return await parse(file.data, { sourceMap: this.sourceMap });
+      } catch (e) {
+        throw new ParserError(`Error parsing "${file.url}"`, e);
+      }
+    },
+  },
+});
+
+export default YamlParser;

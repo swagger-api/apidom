90447e27eb44d327b4422e4507a988d486780525
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-12-30 14:21:22 +0100

    feat(reference): add public library API
    Refs https://github.com/swagger-api/oss-planning/issues/133

--- apidom/packages/apidom-reference/src/index.ts
+++ apidom/packages/apidom-reference/src/index.ts
@@ -1,11 +1,19 @@
 import { ParseResultElement } from 'apidom';
 
+import { ReferenceSet as IReferenceSet } from './types';
 import defaultOptions from './options';
 import { merge as mergeOptions } from './options/util';
 import parseFn from './parse';
+import resolveFn from './resolve';
 
-// eslint-disable-next-line import/prefer-default-export
-export const parse = (uri: string, options = {}): Promise<ParseResultElement> => {
+export const parse = async (uri: string, options = {}): Promise<ParseResultElement> => {
   const mergedOptions = mergeOptions(defaultOptions, options);
   return parseFn(uri, mergedOptions);
 };
+
+export const resolve = async (uri: string, options = {}): Promise<IReferenceSet> => {
+  const mergedOptions = mergeOptions(defaultOptions, options);
+  return resolveFn(uri, mergedOptions);
+};
+
+export { resolveApiDOM } from './resolve';
--- apidom/packages/apidom-reference/src/resolve.ts
+++ apidom/packages/apidom-reference/src/resolve.ts
@@ -1,5 +1,7 @@
 import { isEmpty } from 'ramda';
+import { Element } from 'apidom';
 
+import { merge as mergeOptions } from './options/util';
 import { ReferenceOptions as IReferenceOptions, ReferenceSet as IReferenceSet } from './types';
 import parse from './parse';
 import * as plugins from './util/plugins';
@@ -7,11 +9,18 @@ import File from './util/File';
 import { ResolverError, UnmatchedResolveStrategyError } from './util/errors';
 
 /**
- * Resolves a file with all it's external references.
+ * Resolves ApiDOM with all it's external references.
  */
-const resolve = async (uri: string, options: IReferenceOptions): Promise<IReferenceSet> => {
-  const parseResult = await parse(uri, options);
-  const file = File({ uri, parseResult, mediaType: options.parse.mediaType });
+export const resolveApiDOM = async <T extends Element>(
+  element: T,
+  options: IReferenceOptions,
+): Promise<IReferenceSet> => {
+  const file = File({
+    uri: options.resolve.baseURI,
+    parseResult: element,
+    mediaType: options.parse.mediaType,
+  });
+
   const resolveStrategies = plugins.filter('canResolve', file, options.resolve.strategies);
 
   // we couldn't find any resolver for this File
@@ -25,6 +34,16 @@ const resolve = async (uri: string, options: IReferenceOptions): Promise<IRefere
   } catch (error) {
     throw new ResolverError(`Error while resolving file "${file.uri}"`, error);
   }
+};
+
+/**
+ * Resolves a file with all it's external references.
+ */
+const resolve = async (uri: string, options: IReferenceOptions): Promise<IReferenceSet> => {
+  const parseResult = await parse(uri, options);
+  const mergedOptions = mergeOptions(options, { resolve: { baseURI: uri } });
+
+  return resolveApiDOM(parseResult, mergedOptions);
 };
 
 export default resolve;

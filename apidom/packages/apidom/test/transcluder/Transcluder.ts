4654b8778c165effa2b6837e13a9a8a65731a821
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-01-11 18:46:26 +0100

    feat(reference): introduce mutating transcluder
    Refs https://github.com/swagger-api/oss-planning/issues/133

---
+++ apidom/packages/apidom/test/transcluder/Transcluder.ts
@@ -0,0 +1,91 @@
+import { assert } from 'chai';
+import { MemberElement, StringElement } from 'minim';
+
+import { from, toValue, NumberElement } from '../../src';
+import Transcluder from '../../src/transcluder';
+
+describe('transcluder', function () {
+  context('Transcluder', function () {
+    context('transclude', function () {
+      context('given parent is Array Element', function () {
+        specify('should transclude', function () {
+          const element = from([1, 2, 3]);
+          const search = element.get(1);
+          const replace = new NumberElement(4);
+          const transcluder = Transcluder({ element });
+          const transcludedElement = transcluder.transclude(search, replace);
+          const transcludedValue = toValue(transcludedElement);
+
+          assert.deepEqual(transcludedValue, [1, 4, 3]);
+        });
+      });
+
+      context('given parent is Member Element', function () {
+        specify('should transclude', function () {
+          const element = from([1, { prop: 'value' }, 3]);
+          const search = element.get(1).get('prop');
+          const replace = new NumberElement(4);
+          const transcluder = Transcluder({ element });
+          const transcludedElement = transcluder.transclude(search, replace);
+          const transcludedValue = toValue(transcludedElement);
+
+          assert.deepEqual(transcludedValue, [1, { prop: 4 }, 3]);
+        });
+      });
+
+      context('given parent is Object Element', function () {
+        specify('should transclude', function () {
+          const element = from([1, { prop: 'value', prop2: 'value2' }, 3]);
+          const search = element.get(1).getMember('prop');
+          const replace = new MemberElement(
+            new StringElement('prop1'),
+            new StringElement('value1'),
+          );
+          const transcluder = Transcluder({ element });
+          const transcludedElement = transcluder.transclude(search, replace);
+          const transcludedValue = toValue(transcludedElement);
+
+          assert.deepEqual(transcludedValue, [1, { prop1: 'value1', prop2: 'value2' }, 3]);
+        });
+      });
+
+      context('given multiple transclude operations', function () {
+        specify('should transclude all', function () {
+          const element = from([1, { prop: 'value', prop2: 'value2' }, 3]);
+          const search1 = element.get(1).getMember('prop');
+          const replace1 = new MemberElement(
+            new StringElement('prop1'),
+            new StringElement('value1'),
+          );
+          const search2 = element.get(2);
+          const replace2 = new NumberElement(4);
+          const transcluder = Transcluder({ element });
+
+          const transcludedElementV1 = transcluder.transclude(search1, replace1);
+          const transcludedValueV1 = toValue(transcludedElementV1);
+
+          const transcludedElementV2 = transcluder.transclude(search2, replace2);
+          const transcludedValueV2 = toValue(transcludedElementV2);
+
+          assert.deepEqual(transcludedValueV1, [1, { prop1: 'value1', prop2: 'value2' }, 3]);
+          assert.deepEqual(transcludedValueV2, [1, { prop1: 'value1', prop2: 'value2' }, 4]);
+        });
+      });
+
+      context('given frozen element tree', function () {
+        specify('should throw error', function () {
+          const element = from([1, { prop: 'value', prop2: 'value2' }, 3]);
+          element.freeze();
+          const search = element.get(1).getMember('prop');
+          const replace = new MemberElement(
+            new StringElement('prop1'),
+            new StringElement('value1'),
+          );
+          const transcluder = Transcluder({ element });
+
+          assert.throw(() => transcluder.transclude(search, replace), TypeError);
+        });
+      });
+    });
+  });
+});

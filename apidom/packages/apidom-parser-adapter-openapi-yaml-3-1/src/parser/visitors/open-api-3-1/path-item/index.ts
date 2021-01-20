523a0c1f638cd1a75a94472bccaf6c90b7f9b64e
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-01-20 14:03:27 +0100

    fix(openapi3-1-yaml): fix PathItem visitor
    Decoration of Operation Object now happens properly

--- apidom/packages/apidom-parser-adapter-openapi-yaml-3-1/src/parser/visitors/open-api-3-1/path-item/index.ts
+++ apidom/packages/apidom-parser-adapter-openapi-yaml-3-1/src/parser/visitors/open-api-3-1/path-item/index.ts
@@ -1,8 +1,7 @@
 import stampit from 'stampit';
-import { StringElement } from 'minim';
 import { always } from 'ramda';
-import { YamlMapping } from 'apidom-ast';
-import { isOperationElement, OperationElement } from 'apidom-ns-openapi-3-1';
+import { YamlKeyValuePair } from 'apidom-ast';
+import { isOperationElement } from 'apidom-ns-openapi-3-1';
 
 import FixedFieldsYamlMappingVisitor from '../../generics/FixedFieldsYamlMappingVisitor';
 import { KindVisitor } from '../../generics';
@@ -15,18 +14,24 @@ const PathItemVisitor = stampit(KindVisitor, FixedFieldsYamlMappingVisitor, {
     this.element = new this.namespace.elements.PathItem();
   },
   methods: {
-    mapping(mappingNode: YamlMapping) {
+    keyValuePair(keyValuePairNode: YamlKeyValuePair) {
       // @ts-ignore
-      const result = FixedFieldsYamlMappingVisitor.compose.methods.mapping.call(this, mappingNode);
+      const result = FixedFieldsYamlMappingVisitor.compose.methods.keyValuePair.call(
+        this,
+        keyValuePairNode,
+      );
 
-      // decorate Operation elements with HTTP method
-      this.element
-        .filter(isOperationElement)
-        .forEach((operationElement: OperationElement, httpMethodElementCI: StringElement) => {
-          const httpMethod = httpMethodElementCI.toValue().toUpperCase();
-          const httpMethodElementCS = new this.namespace.elements.String(httpMethod);
-          operationElement.setMetaProperty('httpMethod', httpMethodElementCS);
-        });
+      const memberElement = this.element.last;
+
+      if (!isOperationElement(memberElement.value)) {
+        return result;
+      }
+
+      // decorate Operation element with HTTP method
+      const { key: httpMethodElementCI, value: operationElement } = memberElement;
+      const httpMethod = httpMethodElementCI.toValue().toUpperCase();
+      const httpMethodElementCS = new this.namespace.elements.String(httpMethod);
+      operationElement.setMetaProperty('httpMethod', httpMethodElementCS);
 
       return result;
     },

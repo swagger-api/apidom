41845038a4624ca0c492b1dd618dbcddc581bd38
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-01-20 09:47:27 +0100

    fix(openapi3-1): fix problem in Paht Item object visiting
    Two problems were fixed
    1.) prioritization of visitor function getter was wrong
    2.) never calling leave() method (due to 1.)

--- apidom/packages/apidom-parser-adapter-openapi-yaml-3-1/src/parser/visitors/open-api-3-1/path-item/index.ts
+++ apidom/packages/apidom-parser-adapter-openapi-yaml-3-1/src/parser/visitors/open-api-3-1/path-item/index.ts
@@ -7,28 +7,30 @@ import { isOperationElement, OperationElement } from 'apidom-ns-openapi-3-1';
 import FixedFieldsYamlMappingVisitor from '../../generics/FixedFieldsYamlMappingVisitor';
 import { KindVisitor } from '../../generics';
 
-const PathItemVisitor = stampit(KindVisitor, FixedFieldsYamlMappingVisitor).init(
-  function PathItemVisitor() {
-    this.element = new this.namespace.elements.PathItem();
-    this.specPath = always(['document', 'objects', 'PathItem']);
-
-    this.mapping = {
-      enter(mappingNode: YamlMapping) {
-        // @ts-ignore
-        return FixedFieldsYamlMappingVisitor.compose.methods.mapping.call(this, mappingNode);
-      },
-      leave() {
-        // decorate Operation elements with HTTP method
-        this.element
-          .filter(isOperationElement)
-          .forEach((operationElement: OperationElement, httpMethodElementCI: StringElement) => {
-            const httpMethod = httpMethodElementCI.toValue().toUpperCase();
-            const httpMethodElementCS = new this.namespace.elements.String(httpMethod);
-            operationElement.setMetaProperty('httpMethod', httpMethodElementCS);
-          });
-      },
-    };
+const PathItemVisitor = stampit(KindVisitor, FixedFieldsYamlMappingVisitor, {
+  props: {
+    specPath: always(['document', 'objects', 'PathItem']),
   },
-);
+  init() {
+    this.element = new this.namespace.elements.PathItem();
+  },
+  methods: {
+    mapping(mappingNode: YamlMapping) {
+      // @ts-ignore
+      const result = FixedFieldsYamlMappingVisitor.compose.methods.mapping.call(this, mappingNode);
+
+      // decorate Operation elements with HTTP method
+      this.element
+        .filter(isOperationElement)
+        .forEach((operationElement: OperationElement, httpMethodElementCI: StringElement) => {
+          const httpMethod = httpMethodElementCI.toValue().toUpperCase();
+          const httpMethodElementCS = new this.namespace.elements.String(httpMethod);
+          operationElement.setMetaProperty('httpMethod', httpMethodElementCS);
+        });
+
+      return result;
+    },
+  },
+});
 
 export default PathItemVisitor;

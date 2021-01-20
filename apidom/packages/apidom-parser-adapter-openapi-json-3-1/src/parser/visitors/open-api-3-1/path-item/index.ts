0bea447a25aedf1197581051fd2b7f9f32d35d95
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-01-20 09:47:27 +0100

    fix(openapi3-1): fix problem in Paht Item object visiting
    Two problems were fixed
    1.) prioritization of visitor function getter was wrong
    2.) never calling leave() method (due to 1.)

--- apidom/packages/apidom-parser-adapter-openapi-json-3-1/src/parser/visitors/open-api-3-1/path-item/index.ts
+++ apidom/packages/apidom-parser-adapter-openapi-json-3-1/src/parser/visitors/open-api-3-1/path-item/index.ts
@@ -7,28 +7,30 @@ import { JsonObject } from 'apidom-ast';
 import FixedFieldsJsonObjectVisitor from '../../generics/FixedFieldsJsonObjectVisitor';
 import { ValueVisitor } from '../../generics';
 
-const PathItemVisitor = stampit(ValueVisitor, FixedFieldsJsonObjectVisitor).init(
-  function PathItemVisitor() {
-    this.element = new this.namespace.elements.PathItem();
-    this.specPath = always(['document', 'objects', 'PathItem']);
-
-    this.object = {
-      enter(objectNode: JsonObject) {
-        // @ts-ignore
-        return FixedFieldsJsonObjectVisitor.compose.methods.object.call(this, objectNode);
-      },
-      leave() {
-        // decorate Operation elements with HTTP method
-        this.element
-          .filter(isOperationElement)
-          .forEach((operationElement: OperationElement, httpMethodElementCI: StringElement) => {
-            const httpMethod = httpMethodElementCI.toValue().toUpperCase();
-            const httpMethodElementCS = new this.namespace.elements.String(httpMethod);
-            operationElement.setMetaProperty('httpMethod', httpMethodElementCS);
-          });
-      },
-    };
+const PathItemVisitor = stampit(ValueVisitor, FixedFieldsJsonObjectVisitor, {
+  props: {
+    specPath: always(['document', 'objects', 'PathItem']),
   },
-);
+  init() {
+    this.element = new this.namespace.elements.PathItem();
+  },
+  methods: {
+    object(objectNode: JsonObject) {
+      // @ts-ignore
+      const result = FixedFieldsJsonObjectVisitor.compose.methods.object.call(this, objectNode);
+
+      // decorate Operation elements with HTTP method
+      this.element
+        .filter(isOperationElement)
+        .forEach((operationElement: OperationElement, httpMethodElementCI: StringElement) => {
+          const httpMethod = httpMethodElementCI.toValue().toUpperCase();
+          const httpMethodElementCS = new this.namespace.elements.String(httpMethod);
+          operationElement.setMetaProperty('httpMethod', httpMethodElementCS);
+        });
+
+      return result;
+    },
+  },
+});
 
 export default PathItemVisitor;

88cd7e1a33e441aec57d72ddd54fa71ef8634d89
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-04-09 14:05:39 +0200

    feat(ns-openapi-3-1): add  Contents of String-Encoded Data vocabulary
    Refs #337

---
+++ apidom/packages/apidom-ns-openapi-3-1/src/refractor/visitors/open-api-3-1/schema/ContentMediaTypeVisitor.ts
@@ -0,0 +1,7 @@
+import stampit from 'stampit';
+
+import FallbackVisitor from '../../FallbackVisitor';
+
+const ContentMediaTypeVisitor = stampit(FallbackVisitor);
+
+export default ContentMediaTypeVisitor;

ed8535a14e68d9aca71f1d5ce267cddc121a3c3e
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-04-09 14:00:21 +0200

    feat(ns-openapi-3-1): add Semantic Content vocabulary to Schema
    Refs #337

---
+++ apidom/packages/apidom-ns-openapi-3-1/src/refractor/visitors/open-api-3-1/schema/JsonPointerVisitor.ts
@@ -0,0 +1,7 @@
+import stampit from 'stampit';
+
+import FallbackVisitor from '../../FallbackVisitor';
+
+const JsonPointerVisitor = stampit(FallbackVisitor);
+
+export default JsonPointerVisitor;
---
+++ apidom/packages/apidom-ns-openapi-3-1/src/refractor/visitors/open-api-3-1/schema/UriTemplateVisitor.ts
@@ -0,0 +1,7 @@
+import stampit from 'stampit';
+
+import FallbackVisitor from '../../FallbackVisitor';
+
+const UriTemplateVisitor = stampit(FallbackVisitor);
+
+export default UriTemplateVisitor;

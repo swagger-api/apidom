f585e1f7f3bfedfaa2065c37a7ab9369ddd2dc07
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-04-20 10:04:47 +0200

    feat(reference): add POC implementation of JSON Schema deference
    This POC is part of OpenApi 3.1.0 dereferencing strategy
    and is based on assumptions documented in attached issue.
    
    Refs #366

--- apidom/packages/apidom-ns-openapi-3-1/src/index.ts
+++ apidom/packages/apidom-ns-openapi-3-1/src/index.ts
@@ -35,6 +35,7 @@ export {
   isResponseElement,
   isResponsesElement,
   isSchemaElement,
+  isSchemaElementExternal,
   isSecurityRequirementElement,
   isServerElement,
   isServerVariableElement,

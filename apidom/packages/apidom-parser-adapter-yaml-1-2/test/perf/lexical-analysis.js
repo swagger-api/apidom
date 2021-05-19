b54827ce700b969b76d5c2c20ad0008c21bbf597
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-05-19 17:00:51 +0200

    perf(parser-adapter-yaml-1-2): analyze performance profile
    Parsing stage result: 4.74 ops/sec ±0.72% (623 runs sampled)
    Lexical Analysis result: 653 ops/sec ±3.40% (670 runs sampled)
    
    Parsing stage on medium size documents takes
    on average: 210,970464135 ms
    Lexical analysis phase on medium size documents takes
    on average: 1,639344262 ms
    
    Observation:
    
    Lexical analysis phase takes less than 1% of entire Parsing stage. This
    means that we did good in choosing very fast underlying parser (TreeSitter)
    for Lexical Analysis but we have huge opportunity to make
    Syntactic Analysis (second phase of Parsing stage) faster by analyzing
    TreeSitter CST directly into ApiDOM. Estimates performance gains are
    within 250% - 300%. What we currently do is following transformation CST
    -> YAML AST -> ApiDOM which require two full traversal passes.
    
    Closes #408"

---
+++ apidom/packages/apidom-parser-adapter-yaml-1-2/test/perf/lexical-analysis.js
@@ -0,0 +1,360 @@
+require('@babel/register')({ extensions: ['.js', '.ts'], rootMode: 'upward' });
+
+const Parser = require('tree-sitter');
+const YAMLLanguage = require('tree-sitter-yaml');
+const Benchmark = require('benchmark');
+
+const yaml = `
+---
+openapi: 3.1.0
+info:
+  title: Webhook Example
+  version: 1.0.0
+  description: description
+  summary: summary
+  termsOfService: tos
+components:
+  schemas:
+    Pet:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet1:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet2:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet3:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet4:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet5:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet6:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet7:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet8:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet9:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet10:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet11:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet12:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet13:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet14:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet15:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet16:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet17:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet18:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet19:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet20:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet21:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet22:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet23:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet24:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+    Pet25:
+      required:
+      - id
+      - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+`;
+
+const parser = new Parser();
+parser.setLanguage(YAMLLanguage);
+
+const options = {
+  name: 'lexical-analysis',
+  defer: true,
+  minSamples: 600,
+  expected: '653 ops/sec ±3.40% (670 runs sampled)',
+  async fn(deferred) {
+    await parser.parse(yaml);
+    deferred.resolve();
+  },
+};
+
+module.exports = options;
+
+// we're running as a script
+if (module.parent === null) {
+  const bench = new Benchmark({
+    ...options,
+    onComplete(event) {
+      console.info(String(event.target));
+    },
+    onError(event) {
+      console.error(event);
+    },
+  });
+  bench.run();
+}

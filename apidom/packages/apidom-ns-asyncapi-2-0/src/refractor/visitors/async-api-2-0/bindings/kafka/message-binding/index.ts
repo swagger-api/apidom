51d79ae00d3ddcb7eace662d2ab57450e984c17a
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-05-27 17:14:00 +0200

    feat(ns-asyncapi-2-0): add support for Kafka Message Binding Object
    Refs #387

---
+++ apidom/packages/apidom-ns-asyncapi-2-0/src/refractor/visitors/async-api-2-0/bindings/kafka/message-binding/index.ts
@@ -0,0 +1,18 @@
+import stampit from 'stampit';
+import { always } from 'ramda';
+
+import KafkaMessageBindingElement from '../../../../../../elements/bindings/kafka/KafkaMessageBinding';
+import FallbackVisitor from '../../../../FallbackVisitor';
+import FixedFieldsVisitor from '../../../../generics/FixedFieldsVisitor';
+
+const KafkaMessageBindingVisitor = stampit(FixedFieldsVisitor, FallbackVisitor, {
+  props: {
+    specPath: always(['document', 'objects', 'HttpMessageBinding']),
+    canSupportSpecificationExtensions: false,
+  },
+  init() {
+    this.element = new KafkaMessageBindingElement();
+  },
+});
+
+export default KafkaMessageBindingVisitor;

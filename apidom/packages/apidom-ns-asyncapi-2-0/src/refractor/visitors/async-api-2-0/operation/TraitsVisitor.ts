d5a8bcfcaf4daac49e18fd01fecf5d78afc30d27
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-05-31 16:56:18 +0200

    feat(ns-asyncapi-2-0): add full support for Operation Object
    Refs #387

---
+++ apidom/packages/apidom-ns-asyncapi-2-0/src/refractor/visitors/async-api-2-0/operation/TraitsVisitor.ts
@@ -0,0 +1,34 @@
+import stampit from 'stampit';
+import { ArrayElement, Element, BREAK } from 'apidom';
+
+import SpecificationVisitor from '../../SpecificationVisitor';
+import FallbackVisitor from '../../FallbackVisitor';
+import { isReferenceLikeElement } from '../../../predicates';
+
+const TraitsVisitor = stampit(SpecificationVisitor, FallbackVisitor, {
+  init() {
+    this.element = new ArrayElement();
+  },
+  methods: {
+    ArrayElement(arrayElement: ArrayElement) {
+      arrayElement.forEach((item: Element) => {
+        let element;
+
+        if (isReferenceLikeElement(item)) {
+          element = this.toRefractedElement(['document', 'objects', 'Reference'], item);
+          element.setMetaProperty('referenced-element', 'operationTrait');
+        } else {
+          element = this.toRefractedElement(['document', 'objects', 'OperationTrait'], item);
+        }
+
+        this.element.push(element);
+      });
+
+      this.copyMetaAndAttributes(arrayElement, this.element);
+
+      return BREAK;
+    },
+  },
+});
+
+export default TraitsVisitor;

9c116160ed4a475ccd91e71975413f1162bb8b42
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-06-17 16:11:10 +0200

    test(ns-openapi-3-1): add tests for Request Body Object dereferencing
    Refs #425

--- apidom/packages/apidom-reference/test/dereference/strategies/openapi-3-1/request-body-object/index.ts
+++ apidom/packages/apidom-reference/test/dereference/strategies/openapi-3-1/request-body-object/index.ts
@@ -10,9 +10,9 @@ const rootFixturePath = path.join(__dirname, 'fixtures');
 describe('dereference', function () {
   context('strategies', function () {
     context('openapi-3-1', function () {
-      context('Example Object', function () {
-        context('given in components/examples field', function () {
-          const fixturePath = path.join(rootFixturePath, 'components-examples');
+      context('Request Body Object', function () {
+        context('given in components/requestBodies field', function () {
+          const fixturePath = path.join(rootFixturePath, 'components-request-bodies');
 
           specify('should dereference', async function () {
             const rootFilePath = path.join(fixturePath, 'root.json');
@@ -25,22 +25,8 @@ describe('dereference', function () {
           });
         });
 
-        context('given in Parameter Object', function () {
-          const fixturePath = path.join(rootFixturePath, 'parameter-object');
-
-          specify('should dereference', async function () {
-            const rootFilePath = path.join(fixturePath, 'root.json');
-            const actual = await dereference(rootFilePath, {
-              parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
-            });
-            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
-
-            assert.deepEqual(toValue(actual), expected);
-          });
-        });
-
-        context('given in Media Type Object', function () {
-          const fixturePath = path.join(rootFixturePath, 'media-type-object');
+        context('given in Operation Object', function () {
+          const fixturePath = path.join(rootFixturePath, 'operation-object');
 
           specify('should dereference', async function () {
             const rootFilePath = path.join(fixturePath, 'root.json');

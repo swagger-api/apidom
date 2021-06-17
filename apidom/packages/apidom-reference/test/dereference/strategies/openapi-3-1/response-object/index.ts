7a583b211e8a1c3535faf861c7749100580765b1
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-06-17 13:16:24 +0200

    test(ns-openapi-3-1): add tests for Response Object dereferencing
    Refs #425

---
+++ apidom/packages/apidom-reference/test/dereference/strategies/openapi-3-1/response-object/index.ts
@@ -0,0 +1,44 @@
+import path from 'path';
+import { assert } from 'chai';
+import { toValue } from 'apidom';
+
+import { loadJsonFile } from '../../../../helpers';
+import { dereference } from '../../../../../src';
+
+const rootFixturePath = path.join(__dirname, 'fixtures');
+
+describe('dereference', function () {
+  context('strategies', function () {
+    context('openapi-3-1', function () {
+      context('Response Object', function () {
+        context('given components/responses', function () {
+          const fixturePath = path.join(rootFixturePath, 'components-responses');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+
+        context('given Response Object in Responses Object', function () {
+          const fixturePath = path.join(rootFixturePath, 'responses');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+      });
+    });
+  });
+});

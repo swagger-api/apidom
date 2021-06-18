3d1426ce693b1f8f965a649513aed90e282e4d4a
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-06-18 14:38:53 +0200

    test(ns-openapi-3-1): add tests for Link Object deferencing
    Refs #425

--- apidom/packages/apidom-reference/test/dereference/strategies/openapi-3-1/link-object/index.ts
+++ apidom/packages/apidom-reference/test/dereference/strategies/openapi-3-1/link-object/index.ts
@@ -10,9 +10,23 @@ const rootFixturePath = path.join(__dirname, 'fixtures');
 describe('dereference', function () {
   context('strategies', function () {
     context('openapi-3-1', function () {
-      context('Security Scheme Object', function () {
-        context('given in components/securitySchemes field', function () {
-          const fixturePath = path.join(rootFixturePath, 'components-security-schemes');
+      context('Link Object', function () {
+        context('given in components/links field', function () {
+          const fixturePath = path.join(rootFixturePath, 'components-links');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+
+        context('given in Response Object', function () {
+          const fixturePath = path.join(rootFixturePath, 'response-object');
 
           specify('should dereference', async function () {
             const rootFilePath = path.join(fixturePath, 'root.json');

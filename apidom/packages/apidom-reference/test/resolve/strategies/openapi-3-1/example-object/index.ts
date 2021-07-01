81c44536949890b6bcd93a7bbd880dac104018b5
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-07-01 16:56:44 +0200

    feat(reference): add OAS 3.1 Example.externalValue ext. resolution
    Closes #467

---
+++ apidom/packages/apidom-reference/test/resolve/strategies/openapi-3-1/example-object/index.ts
@@ -0,0 +1,134 @@
+import path from 'path';
+import { assert } from 'chai';
+
+import { resolve } from '../../../../../src';
+import { ResolverError } from '../../../../../src/util/errors';
+
+const rootFixturePath = path.join(__dirname, 'fixtures');
+
+describe('resolve', function () {
+  context('strategies', function () {
+    context('openapi-3-1', function () {
+      context('Example Object', function () {
+        context('given externalValue field', function () {
+          context('and pointing to a JSON file', function () {
+            const fixturePath = path.join(rootFixturePath, 'external-value-json');
+
+            specify('should resolve', async function () {
+              const rootFilePath = path.join(fixturePath, 'root.json');
+              const refSet = await resolve(rootFilePath, {
+                parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
+              });
+
+              assert.strictEqual(refSet.size, 2);
+            });
+          });
+
+          context('and pointing to a JSON file and having JSON Pointer', function () {
+            const fixturePath = path.join(rootFixturePath, 'external-value-pointer');
+
+            specify('should resolve', async function () {
+              const rootFilePath = path.join(fixturePath, 'root.json');
+              const refSet = await resolve(rootFilePath, {
+                parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
+              });
+
+              assert.strictEqual(refSet.size, 2);
+            });
+          });
+
+          context('and pointing to a YAML file', function () {
+            const fixturePath = path.join(rootFixturePath, 'external-value-yaml');
+
+            specify('should resolve', async function () {
+              const rootFilePath = path.join(fixturePath, 'root.json');
+              const refSet = await resolve(rootFilePath, {
+                parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
+              });
+
+              assert.strictEqual(refSet.size, 2);
+            });
+          });
+
+          context('and pointing to a text file', function () {
+            const fixturePath = path.join(rootFixturePath, 'external-value-text');
+
+            specify('should resolve', async function () {
+              const rootFilePath = path.join(fixturePath, 'root.json');
+              const refSet = await resolve(rootFilePath, {
+                parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
+              });
+
+              assert.strictEqual(refSet.size, 2);
+            });
+          });
+
+          context('and pointing to a binary file', function () {
+            const fixturePath = path.join(rootFixturePath, 'external-value-binary');
+
+            specify('should resolve', async function () {
+              const rootFilePath = path.join(fixturePath, 'root.json');
+              const refSet = await resolve(rootFilePath, {
+                parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
+              });
+
+              assert.strictEqual(refSet.size, 2);
+            });
+          });
+
+          context('and with unresolvable URI', function () {
+            const fixturePath = path.join(rootFixturePath, 'external-value-unresolvable');
+
+            specify('should throw error', async function () {
+              const rootFilePath = path.join(fixturePath, 'root.json');
+
+              try {
+                await resolve(rootFilePath, {
+                  parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
+                });
+                assert.fail('should throw ResolverError');
+              } catch (e) {
+                assert.instanceOf(e, ResolverError);
+              }
+            });
+          });
+
+          context('with external resolution disabled', function () {
+            const fixturePath = path.join(rootFixturePath, 'external-value-ignore-external');
+
+            specify('should not resolve', async function () {
+              const rootFilePath = path.join(fixturePath, 'root.json');
+              const refSet = await resolve(rootFilePath, {
+                parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
+                resolve: { external: false },
+              });
+
+              assert.strictEqual(refSet.size, 1);
+            });
+          });
+
+          context('given both value and externalValue fields are defined', function () {
+            const fixturePath = path.join(rootFixturePath, 'external-value-value-both-defined');
+
+            specify('should throw error', async function () {
+              const rootFilePath = path.join(fixturePath, 'root.json');
+
+              try {
+                await resolve(rootFilePath, {
+                  parse: { mediaType: 'application/vnd.oai.openapi+json;version=3.1.0' },
+                });
+                assert.fail('should throw ResolverError');
+              } catch (e) {
+                assert.strictEqual(
+                  e.cause.cause.message,
+                  'ExampleElement value and externalValue fields are mutually exclusive.',
+                );
+                assert.instanceOf(e, ResolverError);
+              }
+            });
+          });
+        });
+      });
+    });
+  });
+});

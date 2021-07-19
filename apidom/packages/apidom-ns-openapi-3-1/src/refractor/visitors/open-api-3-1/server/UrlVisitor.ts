dbf6b038b83f1b7b4456c385cc3f4f36ea8c7b95
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-07-19 11:15:27 +0200

    feat(ns-openapi-3-1): add metadata to Server.url field

--- apidom/packages/apidom-ns-openapi-3-1/src/refractor/visitors/open-api-3-1/server/UrlVisitor.ts
+++ apidom/packages/apidom-ns-openapi-3-1/src/refractor/visitors/open-api-3-1/server/UrlVisitor.ts
@@ -1,5 +1,17 @@
+import stampit from 'stampit';
+import { StringElement, BREAK } from 'apidom';
+
 import FallbackVisitor from '../../FallbackVisitor';
 
-const UrlVisitor = FallbackVisitor;
+const UrlVisitor = stampit(FallbackVisitor, {
+  methods: {
+    StringElement(stringElement: StringElement) {
+      this.element = stringElement.clone();
+      this.element.classes.push('server-url');
+
+      return BREAK;
+    },
+  },
+});
 
 export default UrlVisitor;

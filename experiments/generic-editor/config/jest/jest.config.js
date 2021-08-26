2a599bc52f67400ed3c951fef325050af8f0f5c6
Timothy Lai <timothy.lai@gmail.com>
2021-08-26 15:35:13 -0700

    refactor(editor): move jest setupTests to /test

--- experiments/generic-editor/config/jest/jest.config.js
+++ experiments/generic-editor/config/jest/jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
   setupFiles: ['react-app-polyfill/jsdom'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  setupFilesAfterEnv: ['<rootDir>/test/setupTests.js'],
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
     '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',

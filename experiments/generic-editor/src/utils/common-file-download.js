ff2792414f99b087f6a5c1b3ca6803f996ce5af2
frantuma <frantuma@yahoo.com>
2021-09-03 11:39:34 +0200

    chore: remove generic-editor (moved to own repo)

--- experiments/generic-editor/src/utils/common-file-download.js
+++
@@ -1,8 +0,0 @@
-const FileDownload = require('js-file-download'); // replaces deprecated react-file-download
-
-// react component helper to trigger browser to save data to file
-export const getFileDownload = ({ blob, filename }) => {
-  FileDownload(blob, filename);
-};
-
-export default { getFileDownload };

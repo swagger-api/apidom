bac94f33ed01dbfb6060c4e98ebec59c068bd0c1
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-09-08 17:05:38 +0300

    style: eslint config changes

--- apidom/.eslintrc.js
+++ apidom/.eslintrc.js
@@ -8,13 +8,11 @@ module.exports = {
     project: ['./tsconfig.json'],
   },
   rules: {
-    'prettier/prettier': 'error',
     quotes: ['error', 'single', { avoidEscape: true }],
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/no-redeclare': 'off', // we should enable this in future and fix all the reporter issues
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/return-await': 'error',
     'no-unused-vars': 'off',
     'import/no-extraneous-dependencies': 'off', // ['error', { devDependencies: true }],
     'import/no-mutable-exports': 0,
@@ -31,13 +29,13 @@ module.exports = {
     'no-labels': 0,
     'no-restricted-syntax': 0,
     'no-nested-ternary': 0,
-    'no-return-await': 'off',
   },
-  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  plugins: ['@typescript-eslint', 'import'],
   extends: [
+    'airbnb-base',
     'airbnb-typescript/base', // uses rules from AirBnb codestyle
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/eslint-recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin\
+    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended', // enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    'prettier', // uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
   ],
 };

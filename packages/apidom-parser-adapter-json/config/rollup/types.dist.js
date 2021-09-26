04a03ce1bf01ed68d3d778bf6523d9cbf38c83cc
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-09-26 10:09:35 +0300

    build: bundle typescript into dist files
    Refs #549

---
+++ packages/apidom-parser-adapter-json/config/rollup/types.dist.js
@@ -0,0 +1,10 @@
+import dts from 'rollup-plugin-dts';
+
+const config = [
+  {
+    input: './types/adapter-browser.d.ts',
+    output: [{ file: 'types/dist.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
+export default config;

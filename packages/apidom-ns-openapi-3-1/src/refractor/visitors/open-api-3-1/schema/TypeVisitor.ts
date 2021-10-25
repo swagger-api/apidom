433a2c29aaccad4bf63e27fc4b79ea5f47f6c050
frantuma <frantuma@yahoo.com>
2021-10-25 18:49:29 +0200

    feat: add schema type related metadata to namespaces

--- packages/apidom-ns-openapi-3-1/src/refractor/visitors/open-api-3-1/schema/TypeVisitor.ts
+++ packages/apidom-ns-openapi-3-1/src/refractor/visitors/open-api-3-1/schema/TypeVisitor.ts
@@ -1,5 +1,23 @@
+import stampit from 'stampit';
+import { StringElement, ArrayElement, BREAK } from '@swagger-api/apidom-core';
+
 import FallbackVisitor from '../../FallbackVisitor';
 
-const TypeVisitor = FallbackVisitor;
+const TypeVisitor = stampit(FallbackVisitor, {
+  methods: {
+    StringElement(stringElement: StringElement) {
+      this.element = stringElement.clone();
+      this.element.classes.push('json-schema-type');
+
+      return BREAK;
+    },
+    ArrayElement(arrayElement: ArrayElement) {
+      this.element = arrayElement.clone();
+      this.element.classes.push('json-schema-type');
+
+      return BREAK;
+    },
+  },
+});
 
 export default TypeVisitor;

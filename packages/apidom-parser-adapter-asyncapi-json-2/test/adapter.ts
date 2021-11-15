326c90b10a67373777992ec55a4fbf537e5a5251
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-11-15 16:37:38 +0100

    test: consolidate JSON adapters tests

--- packages/apidom-parser-adapter-asyncapi-json-2/test/adapter.ts
+++ packages/apidom-parser-adapter-asyncapi-json-2/test/adapter.ts
@@ -1,8 +1,25 @@
+import fs from 'fs';
+import path from 'path';
 import { assert } from 'chai';
+import { isParseResultElement } from '@swagger-api/apidom-core';
+import { isAsyncApi2Element } from '@swagger-api/apidom-ns-asyncapi-2';
 
 import * as adapter from '../src/adapter';
 
+const spec = fs.readFileSync(path.join(__dirname, 'fixtures', 'sample-api.json')).toString();
+
 describe('adapter', function () {
+  it('should detect proper media type', function () {
+    assert.isTrue(adapter.detect(spec));
+  });
+
+  it('should parse', async function () {
+    const parseResult = await adapter.parse(spec, { sourceMap: true });
+
+    assert.isTrue(isParseResultElement(parseResult));
+    assert.isTrue(isAsyncApi2Element(parseResult.api));
+  });
+
   context('given zero byte empty file', function () {
     specify('should return empty parse result', async function () {
       const parseResult = await adapter.parse('', { sourceMap: true });

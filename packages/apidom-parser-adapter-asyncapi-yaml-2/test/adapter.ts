221bac3f84ea2388ead0c3b4c29a540d989f9704
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-11-15 13:44:13 +0100

    fix(ns-openapi-3-1): fix bug in replace empty el. plugin
    Bug manifested itself in missing source maps
    after using the refractorPluginReplaceEmptyElements
    plugin.
    
    Refs #812

--- packages/apidom-parser-adapter-asyncapi-yaml-2/test/adapter.ts
+++ packages/apidom-parser-adapter-asyncapi-yaml-2/test/adapter.ts
@@ -1,8 +1,26 @@
+import fs from 'fs';
+import path from 'path';
 import { assert } from 'chai';
+import dedent from 'dedent';
+import { isParseResultElement, SourceMapElement } from '@swagger-api/apidom-core';
+import { isAsyncApi2Element } from '@swagger-api/apidom-ns-asyncapi-2';
 
 import * as adapter from '../src/adapter';
 
+const spec = fs.readFileSync(path.join(__dirname, 'fixtures', 'sample-api.yaml')).toString();
+
 describe('adapter', function () {
+  it('should detect proper media type', function () {
+    assert.isTrue(adapter.detect(spec));
+  });
+
+  it('should parse', async function () {
+    const parseResult = await adapter.parse(spec, { sourceMap: true });
+
+    assert.isTrue(isParseResultElement(parseResult));
+    assert.isTrue(isAsyncApi2Element(parseResult.api));
+  });
+
   context('given zero byte empty file', function () {
     specify('should return empty parse result', async function () {
       const parseResult = await adapter.parse('', { sourceMap: true });
@@ -24,6 +42,26 @@ describe('adapter', function () {
       const parseResult = await adapter.parse(' %YAML x ', { sourceMap: true });
 
       assert.isTrue(parseResult.isEmpty);
+    });
+  });
+
+  context('given YAML with empty node', function () {
+    specify('should generate source maps', async function () {
+      const yamlSource = dedent`
+        asyncapi: 2.2.0
+        info:
+      `;
+
+      const { result } = await adapter.parse(yamlSource, { sourceMap: true });
+      // @ts-ignore
+      const infoValue = result.get('info');
+      const sourceMap = infoValue.meta.get('sourceMap');
+      const { positionStart, positionEnd } = sourceMap;
+      const expectedEmptyPosition = [1, 5, 21];
+
+      assert.instanceOf(sourceMap, SourceMapElement);
+      assert.isTrue(positionStart.equals(expectedEmptyPosition));
+      assert.isTrue(positionEnd.equals(expectedEmptyPosition));
     });
   });
 });

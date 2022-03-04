27388ef609d1ca6d1b2d60f3ebf63e79b05cabb3
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-03-04 13:13:45 +0100

    feat(ns-api-design-systems): validate request header

--- packages/apidom-ns-api-design-systems/src/refractor/plugins/openapi-3-1/standard-identifier-accessors.ts
+++ packages/apidom-ns-api-design-systems/src/refractor/plugins/openapi-3-1/standard-identifier-accessors.ts
@@ -2,6 +2,8 @@ import {
   PathItemElement,
   OperationElement,
   ResponseElement,
+  ParameterElement,
+  isStringElement,
 } from '@swagger-api/apidom-ns-openapi-3-1';
 
 const plugin = () => () => {
@@ -25,7 +27,44 @@ const plugin = () => () => {
           },
         ];
 
+        // fold PathItem.parameters to Operation.parameters
+        // @ts-ignore
+        parentPathItem?.parameters?.forEach((parameter: ParameterElement) => {
+          if (
+            isStringElement(parameter.in) &&
+            isStringElement(parameter.name) &&
+            parameter.in.toValue() === 'header'
+          ) {
+            standardIdentifiers.push({
+              subject: ['http', 'request', 'header'],
+              value: parameter.name.clone(),
+            });
+            standardIdentifiers.push({
+              subject: ['http', 'message', 'header'],
+              value: parameter.name.clone(),
+            });
+          }
+        });
+
         element.setMetaProperty('ads-a-standard-identifier', standardIdentifiers);
+      },
+      ParameterElement(element: ParameterElement) {
+        if (
+          isStringElement(element.in) &&
+          isStringElement(element.name) &&
+          element.in.toValue() === 'header'
+        ) {
+          element.setMetaProperty('ads-a-standard-identifier', [
+            {
+              subject: ['http', 'request', 'header'],
+              value: element.name.clone(),
+            },
+            {
+              subject: ['http', 'message', 'header'],
+              value: element.name.clone(),
+            },
+          ]);
+        }
       },
       ResponseElement(element: ResponseElement) {
         if (!element.meta.hasKey('http-status-code')) return;

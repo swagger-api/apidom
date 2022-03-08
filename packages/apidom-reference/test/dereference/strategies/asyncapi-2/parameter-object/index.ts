6b781c6d1acbe65b8669d1adf366c3c9b8c0e687
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-03-08 16:02:45 +0100

    test(reference): add dereference tests for Parameter Object
    Refs #427

---
+++ packages/apidom-reference/test/dereference/strategies/asyncapi-2/parameter-object/index.ts
@@ -0,0 +1,45 @@
+import path from 'path';
+import { assert } from 'chai';
+import { toValue } from '@swagger-api/apidom-core';
+import { mediaTypes } from '@swagger-api/apidom-ns-asyncapi-2';
+
+import { loadJsonFile } from '../../../../helpers';
+import { dereference } from '../../../../../src';
+
+const rootFixturePath = path.join(__dirname, 'fixtures');
+
+describe('dereference', function () {
+  context('strategies', function () {
+    context('asyncapi-2', function () {
+      context('Parameter Object', function () {
+        context('given in Parameters Object', function () {
+          const fixturePath = path.join(rootFixturePath, 'parameters-object');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+
+        context('given in components/parameters field', function () {
+          const fixturePath = path.join(rootFixturePath, 'components-parameters');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+      });
+    });
+  });
+});

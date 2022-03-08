7444661a87677b148d26dd2ddf5c1cae61ab2d95
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-03-08 16:23:46 +0100

    test(reference): add dereference tests for Schema Object
    Refs #427

---
+++ packages/apidom-reference/test/dereference/strategies/asyncapi-2/schema-object/index.ts
@@ -0,0 +1,73 @@
+import path from 'path';
+import { assert } from 'chai';
+import { toValue } from '@swagger-api/apidom-core';
+import { mediaTypes } from '@swagger-api/apidom-ns-asyncapi-2';
+
+import { loadJsonFile } from '../../../../helpers';
+import { dereference } from '../../../../../src';
+
+const rootFixturePath = path.join(__dirname, 'fixtures');
+
+describe('dereference', function () {
+  context('strategies', function () {
+    context('asyncapi-2', function () {
+      context('Schema Object', function () {
+        context('given in components/schemas field', function () {
+          const fixturePath = path.join(rootFixturePath, 'components-schemas');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+
+        context('given in Parameter Object', function () {
+          const fixturePath = path.join(rootFixturePath, 'parameter-object');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+
+        context('given in Message Object', function () {
+          const fixturePath = path.join(rootFixturePath, 'message-object');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+
+        context('given in Message Trait Object', function () {
+          const fixturePath = path.join(rootFixturePath, 'message-trait-object');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+      });
+    });
+  });
+});

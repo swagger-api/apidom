f63e38a96cef2e0bfa0760046bb0c45cab95e820
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-03-10 15:16:17 +0100

    test(reference): add dereference tests for Server Object
    Refs #427

---
+++ packages/apidom-reference/test/dereference/strategies/asyncapi-2/server-object/index.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import { assert } from 'chai';
+import { toValue } from '@swagger-api/apidom-core';
+import { mediaTypes } from '@swagger-api/apidom-ns-asyncapi-2';
+
+import { loadJsonFile } from '../../../../helpers';
+import { dereference } from '../../../../../src';
+
+const rootFixturePath = path.join(__dirname, 'fixtures');
+
+describe('dereference', function () {
+  context('strategies', function () {
+    context('asyncapi-2', function () {
+      context('Server Object', function () {
+        context('given in components/servers field', function () {
+          const fixturePath = path.join(rootFixturePath, 'components-servers');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+      });
+    });
+  });
+});

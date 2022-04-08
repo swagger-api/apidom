7bbecfbfcd85b32675b8a85376139c86d97c5090
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-04-08 09:44:49 +0200

    feat(apidom-ls): add support for 2.3
    - message-bindings

--- packages/apidom-ls/scripts/transform-spec-fragment.js
+++ packages/apidom-ls/scripts/transform-spec-fragment.js
@@ -8,10 +8,8 @@ const transform = (str) => {
 };
 
 transform(
-  `#### Message Object
-
-Describes a message received on a given channel and operation.
+  `[HTTP Message Binding](https://github.com/asyncapi/bindings/blob/master/http#message)
 \\
 \\
-A message is the mechanism by which information is exchanged via a channel between servers and applications. A message MUST contain a payload and MAY also contain headers. The headers MAY be subdivided into [protocol](https://www.asyncapi.com/docs/specifications/v2.3.0#definitionsProtocol)-defined headers and header properties defined by the application which can act as supporting metadata. The payload contains the data, defined by the application, which MUST be serialized into a format (JSON, XML, Avro, binary, etc.). Since a message is a generic mechanism, it can support multiple interaction patterns such as event, command, request, or response. `,
+Protocol-specific information for an HTTP message.`,
 );

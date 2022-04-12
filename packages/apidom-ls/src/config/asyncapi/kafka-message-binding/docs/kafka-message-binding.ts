ab69a421ecb1ee055052845faad45669ebebf9a3
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-04-12 15:14:46 +0200

    fix(apidom-ls): enhance docs config formatting for AsyncAPI
    Refs #1313

--- packages/apidom-ls/src/config/asyncapi/kafka-message-binding/docs/kafka-message-binding.ts
+++ packages/apidom-ls/src/config/asyncapi/kafka-message-binding/docs/kafka-message-binding.ts
@@ -8,7 +8,7 @@ const kafkaMessageBindingDocs = [
     docs: 'The version of this binding. If omitted, "latest" MUST be assumed.',
   },
   {
-    docs: 'This object contains information about the message representation in Kafka.',
+    docs: "#### [Message Binding Object](https://github.com/asyncapi/bindings/blob/master/kafka/README.md#message-binding-object)\n\nThis object contains information about the message representation in Kafka.\n\n##### Fixed Fields\n\nField Name | Type | Description\n---|:---:|---\n`key` | [Schema Object](https://www.asyncapi.com/docs/specifications/v2.3.0#schemaObject) \\| [AVRO Schema Object](https://avro.apache.org/docs/current/spec.html) | The message key. **NOTE**: You can also use the [reference object](https://asyncapi.io/docs/specifications/v2.3.0#referenceObject) way.\n`bindingVersion` | string | The version of this binding. If omitted, \"latest\" MUST be assumed.\n\nThis object MUST contain only the properties defined above.\n\n\n\n\\\nYAML\n```yaml\nchannels:\n  test:\n    publish:\n      message:\n        bindings:\n          kafka:\n            key:\n              type: string\n              enum: ['myKey']\n            bindingVersion: '0.1.0'\n```",
   },
 ];
 export default kafkaMessageBindingDocs;

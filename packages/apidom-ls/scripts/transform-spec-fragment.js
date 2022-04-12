1c9d2871e3e4b85e04bc0a622e113b20e4d2abc7
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-04-12 16:03:23 +0200

    fix(apidom-ls): enhance docs config formatting for AsyncAPI (#1316)
    Refs #1313

--- packages/apidom-ls/scripts/transform-spec-fragment.js
+++ packages/apidom-ls/scripts/transform-spec-fragment.js
@@ -4,15 +4,10 @@
  */
 
 const transform = (str) => {
-  const transformed = JSON.stringify(str.trim().replace(/(\r\n|\n|\r)/gm, '\n'));
-  const jsonTransformed = transformed.replace(/```json/m, '\n\\\nJSON\n```json');
+  const jsonTransformed = str.replace(/```json/m, '\n\\\nJSON\n```json');
   const yamlTransformed = jsonTransformed.replace(/```yaml/m, '\n\\\nYAML\n```yaml');
-  console.log(yamlTransformed);
+  const transformed = JSON.stringify(yamlTransformed.trim().replace(/(\r\n|\n|\r)/gm, '\n'));
+  console.log(transformed);
 };
 
-transform(
-  `\`\`\`json
-  test
-  \`\`\`
-  `,
-);
+transform(``);

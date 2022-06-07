e1dd8d519b4a53fe1f8b535122624a27d99817e9
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-06-07 13:27:56 +0200

    feat(yaml-1-2): add YAML 1.2 detection mechanism (#1553)
    Refs #422
    Closes #1551

--- packages/apidom-parser-adapter-yaml-1-2/src/adapter.ts
+++ packages/apidom-parser-adapter-yaml-1-2/src/adapter.ts
@@ -2,13 +2,4 @@ import { createNamespace } from '@swagger-api/apidom-core';
 
 export { default as mediaTypes } from './media-types';
 
-export const detect: (source?: string) => boolean = () => {
-  /**
-   * We always return false here as there is no simple
-   * way of synchronously determining if the string is YAML.
-   * Media type should be used to activate this adapter.
-   */
-  return false;
-};
-
 export const namespace = createNamespace();

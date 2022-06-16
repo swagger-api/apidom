cb58abfbfd5844dfb170429b9c08c699fa1b72f3
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-06-16 10:55:43 +0200

    feat(adapter-openapi-json-3-1): export detection regexp
    Refs #1584

--- packages/apidom-parser-adapter-openapi-json-3-1/src/adapter.ts
+++ packages/apidom-parser-adapter-openapi-json-3-1/src/adapter.ts
@@ -6,8 +6,10 @@ import openApiNamespace, { OpenApi3_1Element } from '@swagger-api/apidom-ns-open
 
 export { default as mediaTypes } from './media-types';
 
+export const detectionRegExp = /"openapi"\s*:\s*"3\.1\.\d+"/;
+
 export const detect = async (source: string): Promise<boolean> =>
-  /"openapi"\s*:\s*"3\.1\.\d+"/g.test(source) && (await detectJSON(source));
+  detectionRegExp.test(source) && (await detectJSON(source));
 
 export const parse = async (
   source: string,

3c2da310116493ae08f9b90b953a0896c858a936
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-06-17 17:31:08 +0200

    feat(parser-adapter-json): enhance JSON detection
    This new detection eliminates some really non-sensical
    cases.

--- packages/apidom-parser-adapter-json/src/adapter.ts
+++ packages/apidom-parser-adapter-json/src/adapter.ts
@@ -3,3 +3,6 @@ import { createNamespace } from '@swagger-api/apidom-core';
 export { default as mediaTypes } from './media-types';
 
 export const namespace = createNamespace();
+
+export const detectionRegExp =
+  /(^\s*true\s$)|(^\s*false\s*$)|(^\s*null\s*$)|(^\s*\d+\s*$)|(^\s*{\s*)|(^\s*\[\s*)|(^\s*"([^"\\\\]*|\\\\["\\\\bfnrt/]|\\\\u[0-9a-f]{4})*"\s*$)/;

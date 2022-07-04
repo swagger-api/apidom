b133a8ed6f333ce8a98d37d914f2de0c9200a04b
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-07-04 15:44:45 +0200

    feat(apidom-ls): add AsyncAPI AMQP 0-9-1 Operation Bindings rules
    Refs #1647

---
+++ packages/apidom-ls/src/config/asyncapi/bindings/amqp/operation-binding/lint/index.ts
@@ -0,0 +1,29 @@
+import allowedFieldsLint from './allowed-fields';
+import expirationTypeLint from './expiration--type';
+import userIdTypeLint from './user-id--type';
+import ccTypeLint from './cc--type';
+import priorityTypeLint from './priority--type';
+import deliveryModeEqualsLint from './delivery-mode--equals';
+import mandatoryTypeLint from './mandatory--type';
+import bccTypeLint from './bcc--type';
+import replyToTypeLint from './reply-to--type';
+import timestampTypeLint from './timestamp--type';
+import ackTypeLint from './act--type';
+import bindingVersionTypeLint from './binding-version--type';
+
+const lints = [
+  expirationTypeLint,
+  userIdTypeLint,
+  ccTypeLint,
+  priorityTypeLint,
+  deliveryModeEqualsLint,
+  mandatoryTypeLint,
+  bccTypeLint,
+  replyToTypeLint,
+  timestampTypeLint,
+  ackTypeLint,
+  bindingVersionTypeLint,
+  allowedFieldsLint,
+];
+
+export default lints;
--- packages/apidom-ls/src/config/asyncapi/components/lint/servers--values-type.ts
+++ packages/apidom-ls/src/config/asyncapi/components/lint/servers--values-type.ts
@@ -4,7 +4,7 @@ import { LinterMeta } from '../../../../apidom-language-types';
 const serversValuesTypeLint: LinterMeta = {
   code: ApilintCodes.ASYNCAPI2_COMPONENTS_FIELD_SERVERS_VALUES_TYPE,
   source: 'apilint',
-  message: '"servers" members must be Server object',
+  message: '"servers" values must be Server object',
   severity: 1,
   linterFunction: 'apilintChildrenOfElementsOrClasses',
   linterParams: [['server']],

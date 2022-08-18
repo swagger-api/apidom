9c766d707a896d4deadb77f4afade134a0cf0bb6
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-08-18 13:31:09 +0200

    fix(json-schema): fix media type versions

--- packages/apidom-ns-json-schema-draft-4/src/media-types.ts
+++ packages/apidom-ns-json-schema-draft-4/src/media-types.ts
@@ -9,7 +9,7 @@ export class JSONSchemaDraft4MediaTypes extends MediaTypes<string> {
     return this.filter((mediaType) => mediaType.includes(effectiveFormat));
   }
 
-  findBy(version = 'draft-4', format: Format = 'generic') {
+  findBy(version = 'draft-04', format: Format = 'generic') {
     const search =
       format === 'generic' ? `schema;version=${version}` : `schema+${format};version=${version}`;
     const found = this.find((mediaType) => mediaType.includes(search));
@@ -23,9 +23,9 @@ export class JSONSchemaDraft4MediaTypes extends MediaTypes<string> {
 }
 
 const mediaTypes = new JSONSchemaDraft4MediaTypes(
-  'application/schema;version=draft-4',
-  'application/schema+json;version=draft-4',
-  'application/schema+yaml;version=draft-4',
+  'application/schema;version=draft-04',
+  'application/schema+json;version=draft-04',
+  'application/schema+yaml;version=draft-04',
 );
 
 export default mediaTypes;

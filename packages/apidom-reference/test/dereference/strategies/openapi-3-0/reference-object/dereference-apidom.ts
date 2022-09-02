511502f5db8b978897d514751382751b3b268031
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-09-02 15:18:15 +0200

    feat(referene): add OpenAPI 3.0.x dereference strategy
    Refs #1896

---
+++ packages/apidom-reference/test/dereference/strategies/openapi-3-0/reference-object/dereference-apidom.ts
@@ -0,0 +1,54 @@
+import path from 'node:path';
+import { assert } from 'chai';
+import {
+  mediaTypes,
+  isParameterElement,
+  OpenApi3_0Element,
+} from '@swagger-api/apidom-ns-openapi-3-0';
+import { evaluate } from '@swagger-api/apidom-json-pointer';
+
+import { parse, dereferenceApiDOM } from '../../../../../src';
+
+describe('dereference', function () {
+  context('strategies', function () {
+    context('openapi-3-0', function () {
+      context('Reference Object', function () {
+        context('given single ReferenceElement passed to dereferenceApiDOM', function () {
+          const fixturePath = path.join(__dirname, 'fixtures', 'external-only', 'root.json');
+
+          specify('should dereference', async function () {
+            const parseResult = await parse(fixturePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const referenceElement = evaluate(
+              '/components/parameters/externalRef',
+              parseResult.api as OpenApi3_0Element,
+            );
+            const dereferenced = await dereferenceApiDOM(referenceElement, {
+              parse: { mediaType: mediaTypes.latest('json') },
+              resolve: { baseURI: fixturePath },
+            });
+
+            assert.isTrue(isParameterElement(dereferenced));
+          });
+
+          specify('should dereference and contain metadata about origin', async function () {
+            const parseResult = await parse(fixturePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const referenceElement = evaluate(
+              '/components/parameters/externalRef',
+              parseResult.api as OpenApi3_0Element,
+            );
+            const dereferenced = await dereferenceApiDOM(referenceElement, {
+              parse: { mediaType: mediaTypes.latest('json') },
+              resolve: { baseURI: fixturePath },
+            });
+
+            assert.match(dereferenced.meta.get('ref-origin').toValue(), /external-only\/ex\.json$/);
+          });
+        });
+      });
+    });
+  });
+});

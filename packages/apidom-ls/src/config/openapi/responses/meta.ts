696f627b6103706a7c857d2ffb77c1c51bd41a8b
Timothy Lai <timothy.lai@smartbear.com>
2022-09-07 10:31:22 -0700

    refactor(apidom-ls): oas31 docs remove placeholder imports

--- packages/apidom-ls/src/config/openapi/responses/meta.ts
+++ packages/apidom-ls/src/config/openapi/responses/meta.ts
@@ -1,11 +1,7 @@
-// import lint from './lint';
-// import completion from './completion';
 import documentation from './documentation';
 import { FormatMeta } from '../../../apidom-language-types';
 
 const meta: FormatMeta = {
-  // lint,
-  // completion,
   documentation,
 };
 

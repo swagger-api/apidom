c4c850937ebc8068621b3c808f6f66cf891f81e6
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-09-09 11:18:34 +0200

    refactor(ns-openapi-3-1): extend Header Object from 3.0.x ns
    Refs #1912

--- packages/apidom-ns-openapi-3-1/src/elements/Header.ts
+++ packages/apidom-ns-openapi-3-1/src/elements/Header.ts
@@ -1,108 +1,15 @@
-import {
-  ObjectElement,
-  StringElement,
-  BooleanElement,
-  Attributes,
-  Meta,
-} from '@swagger-api/apidom-core';
+import { HeaderElement } from '@swagger-api/apidom-ns-openapi-3-0';
 
 import SchemaElement from './Schema';
 
-class Header extends ObjectElement {
-  constructor(content?: Record<string, unknown>, meta?: Meta, attributes?: Attributes) {
-    super(content, meta, attributes);
-    this.element = 'header';
-  }
-
-  get required(): BooleanElement {
-    return this.get('required');
-  }
-
-  set required(required: BooleanElement) {
-    this.set('required', required);
-  }
-
-  get deprecated(): BooleanElement {
-    return this.get('deprecated');
-  }
-
-  set deprecated(deprecated: BooleanElement) {
-    this.set('deprecated', deprecated);
-  }
-
-  get allowEmptyValue(): BooleanElement {
-    return this.get('allowEmptyValue');
-  }
-
-  set allowEmptyValue(allowEmptyValue: BooleanElement) {
-    this.set('allowEmptyValue', allowEmptyValue);
-  }
-
-  get style(): StringElement {
-    return this.get('style');
-  }
-
-  set style(style: StringElement) {
-    this.set('style', style);
-  }
-
-  get explode(): BooleanElement {
-    return this.get('explode');
-  }
-
-  set explode(explode: BooleanElement) {
-    this.set('explode', explode);
-  }
-
-  get allowReserved(): BooleanElement {
-    return this.get('allowReserved');
-  }
-
-  set allowReserved(allowReserved: BooleanElement) {
-    this.set('allowReserved', allowReserved);
-  }
-
-  get schema(): SchemaElement {
+class Header extends HeaderElement {
+  get schema(): SchemaElement | undefined {
     return this.get('schema');
   }
 
-  set schema(schema: SchemaElement) {
+  set schema(schema: SchemaElement | undefined) {
     this.set('schema', schema);
   }
-
-  get example(): unknown {
-    return this.get('example');
-  }
-
-  set example(example: unknown) {
-    this.set('example', example);
-  }
-
-  get examples(): ObjectElement {
-    return this.get('examples');
-  }
-
-  set examples(examples: ObjectElement) {
-    this.set('examples', examples);
-  }
-
-  get contentProp(): ObjectElement {
-    return this.get('content');
-  }
-
-  set contentProp(content: ObjectElement) {
-    this.set('content', content);
-  }
 }
-
-Object.defineProperty(Header.prototype, 'description', {
-  get(): StringElement {
-    return this.get('description');
-  },
-  set(description: StringElement) {
-    this.set('description', description);
-  },
-  enumerable: true,
-});
 
 export default Header;

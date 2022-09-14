ab25a597f9fbf082f37274598e1ff5f23e2fece0
Tim Lai <timothy.lai@smartbear.com>
2022-09-14 14:24:53 -0700

    feat(ls): docs for OpenApi 3.1.0 OAuth Flow and OAuth Flows objects (#2011)

---
+++ packages/apidom-ls/src/config/openapi/oauth-flows/documentation.ts
@@ -0,0 +1,32 @@
+const documentation = [
+  /**
+   * The following Fixed Fields are provided as reference, but are more
+   * comprehensively described by parent OAuth Flow Object meta documentation
+   */
+  // {
+  //   target: 'implicit',
+  //   docs: 'Configuration for the OAuth Implicit flow',
+  //   targetSpecs: [{ namespace: 'openapi', version: '3.1.0' }],
+  // },
+  // {
+  //   target: 'password',
+  //   docs: 'Configuration for the OAuth Resource Owner Password flow',
+  //   targetSpecs: [{ namespace: 'openapi', version: '3.1.0' }],
+  // },
+  // {
+  //   target: 'clientCredentials',
+  //   docs: 'Configuration for the OAuth Client Credentials flow. Previously called `application` in OpenAPI 2.0.',
+  //   targetSpecs: [{ namespace: 'openapi', version: '3.1.0' }],
+  // },
+  // {
+  //   target: 'authorizationCode',
+  //   docs: 'Configuration for the OAuth Authorization Code flow. Previously called `accessCode` in OpenAPI 2.0.',
+  //   targetSpecs: [{ namespace: 'openapi', version: '3.1.0' }],
+  // },
+  {
+    docs: '#### OAuth Flows Object\nAllows configuration of the supported OAuth Flows.\n\n##### Fixed Fields\n\nField Name | Type | Description\n---|:---:|---\nimplicit | [OAuth Flow Object](https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.0.md#oauthFlowObject) | Configuration for the OAuth Implicit flow\npassword | [OAuth Flow Object](https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.0.md#oauthFlowObject) | Configuration for the OAuth Resource Owner Password flow\nclientCredentials | [OAuth Flow Object](https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.0.md#oauthFlowObject) | Configuration for the OAuth Client Credentials flow. Previously called `application` in OpenAPI 2.0.\nauthorizationCode | [OAuth Flow Object](https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.0.md#oauthFlowObject) | Configuration for the OAuth Authorization Code flow. Previously called `accessCode` in OpenAPI 2.0.\n\n\\\nThis object MAY be extended with [Specification Extensions](https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.0.md#specificationExtensions).\n',
+    targetSpecs: [{ namespace: 'openapi', version: '3.1.0' }],
+  },
+];
+
+export default documentation;

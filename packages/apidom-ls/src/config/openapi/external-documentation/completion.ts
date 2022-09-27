b72f4a973599a2c2fe541542e217da2c513c395f
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-09-27 12:42:35 +0200

    feat(ls): provide OpenAPI 3.0.x External Docs completion rules
    Refs #2032

---
+++ packages/apidom-ls/src/config/openapi/external-documentation/completion.ts
@@ -0,0 +1,36 @@
+import {
+  ApidomCompletionItem,
+  CompletionFormat,
+  CompletionType,
+} from '../../../apidom-language-types';
+
+const completion: ApidomCompletionItem[] = [
+  {
+    label: 'description',
+    insertText: 'description',
+    kind: 14,
+    format: CompletionFormat.QUOTED,
+    type: CompletionType.PROPERTY,
+    insertTextFormat: 2,
+    documentation: {
+      kind: 'markdown',
+      value:
+        'A description of the target documentation. [CommonMark syntax](https://spec.commonmark.org/) MAY be used for rich text representation.',
+    },
+  },
+  {
+    label: 'url',
+    insertText: 'url',
+    kind: 14,
+    format: CompletionFormat.QUOTED,
+    type: CompletionType.PROPERTY,
+    insertTextFormat: 2,
+    documentation: {
+      kind: 'markdown',
+      value:
+        '**REQUIRED**. The URL for the target documentation. This MUST be in the form of a URL.',
+    },
+  },
+];
+
+export default completion;

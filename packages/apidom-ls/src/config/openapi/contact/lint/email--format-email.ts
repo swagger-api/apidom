d06741eb7f8bceffad77faa1a35371625c3fcff0
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-09-29 12:12:26 +0200

    feat(ls): provide OpenAPI 3.0.x Contact lint rules
    Refs #2033

--- packages/apidom-ls/src/config/openapi/contact/lint/email--format-email.ts
+++ packages/apidom-ls/src/config/openapi/contact/lint/email--format-email.ts
@@ -2,7 +2,7 @@ import ApilintCodes from '../../../codes';
 import { LinterMeta } from '../../../../apidom-language-types';
 
 const emailFormatEmailLint: LinterMeta = {
-  code: ApilintCodes.OPENAPI2_CONTACT_FIELD_EMAIL_FORMAT_EMAIL,
+  code: ApilintCodes.OPENAPI3_0_CONTACT_FIELD_EMAIL_FORMAT_EMAIL,
   source: 'apilint',
   message: "'email' must be a valid email",
   severity: 1,

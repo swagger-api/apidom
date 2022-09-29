f0d18ac3b1c33b6cee78366f82e09063d4fadb95
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-09-29 12:23:13 +0200

    feat(ls): provide OpenAPI 3.0.x License lint rules
    Refs #2033

---
+++ packages/apidom-ls/src/config/openapi/license/lint/index.ts
@@ -0,0 +1,8 @@
+import allowedFields3_0Lint from './allowed-fields-3-0';
+import nameTypeLint from './name--type';
+import nameRequiredLint from './name--required';
+import urlFormatURILint from './url--format-uri';
+
+const contactLints = [nameTypeLint, nameRequiredLint, urlFormatURILint, allowedFields3_0Lint];
+
+export default contactLints;

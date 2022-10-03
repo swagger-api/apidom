852f95a336c5132744738387e6c7a119b1153810
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-10-03 09:45:11 +0200

    feat(ls): provide OpenAPI 3.0.x Tag lint rules
    Refs #2033

--- packages/apidom-ls/src/config/openapi/tag/lint/description--type.ts
+++ packages/apidom-ls/src/config/openapi/tag/lint/description--type.ts
@@ -1,16 +1,16 @@
 import ApilintCodes from '../../../codes';
 import { LinterMeta } from '../../../../apidom-language-types';
 
-const nameTypeLint: LinterMeta = {
-  code: ApilintCodes.ASYNCAPI2_TAG_FIELD_NAME_TYPE,
+const descriptionTypeLint: LinterMeta = {
+  code: ApilintCodes.OPENAPI_3_0_TAG_FIELD_DESCRIPTION_TYPE,
   source: 'apilint',
-  message: "'name' value must be a string",
+  message: "'description' value must be a string",
   severity: 1,
   linterFunction: 'apilintType',
   linterParams: ['string'],
   marker: 'value',
-  target: 'name',
+  target: 'description',
   data: {},
 };
 
-export default nameTypeLint;
+export default descriptionTypeLint;

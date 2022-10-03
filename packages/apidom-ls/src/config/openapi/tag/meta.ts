2ef49b4e85218abd0552813df28fd9b1971ccb6f
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-10-03 09:38:37 +0200

    feat(ls): provide OpenAPI 3.0.x Tag completion rules
    Refs #2032

--- packages/apidom-ls/src/config/openapi/tag/meta.ts
+++ packages/apidom-ls/src/config/openapi/tag/meta.ts
@@ -1,7 +1,9 @@
+import completion from './completion';
 import documentation from './documentation';
 import { FormatMeta } from '../../../apidom-language-types';
 
 const meta: FormatMeta = {
+  completion,
   documentation,
 };
 

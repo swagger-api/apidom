e7d02c0e37a69fccc00df7094570e91caf5acfcb
Timothy Lai <timothy.lai@smartbear.com>
2022-10-04 15:24:16 -0700

    feat(ls): oas31 lint add info object required

--- packages/apidom-ls/src/config/openapi/openapi3_1/lint/index.ts
+++ packages/apidom-ls/src/config/openapi/openapi3_1/lint/index.ts
@@ -1,7 +1,8 @@
 import allowedFieldsLint from './allowed-fields';
+import infoRequiredLint from './info--required';
 import jsonSchemaDialectTypeLint from './jsonSchemaDialect--type';
 import webhooksLint from './webhooks--type';
 
-const lints = [allowedFieldsLint, jsonSchemaDialectTypeLint, webhooksLint];
+const lints = [allowedFieldsLint, infoRequiredLint, jsonSchemaDialectTypeLint, webhooksLint];
 
 export default lints;
---
+++ packages/apidom-ls/src/config/openapi/openapi3_1/lint/info--required.ts
@@ -0,0 +1,24 @@
+import ApilintCodes from '../../../codes';
+import { LinterMeta } from '../../../../apidom-language-types';
+
+const infoRequiredLint: LinterMeta = {
+  code: ApilintCodes.OPENAPI3_1_OPEN_API_FIELD_INFO_TYPE_REQUIRED,
+  source: 'apilint',
+  message: "should always have a 'info' section",
+  severity: 1,
+  linterFunction: 'hasRequiredField',
+  linterParams: ['info'],
+  marker: 'key',
+  data: {
+    quickFix: [
+      {
+        message: "add 'info' section",
+        action: 'addChild',
+        snippetYaml: 'info: \n  \n',
+        snippetJson: '"info": {\n  \n  },\n',
+      },
+    ],
+  },
+};
+
+export default infoRequiredLint;

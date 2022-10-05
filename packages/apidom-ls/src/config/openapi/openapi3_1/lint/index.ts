a98ca842e65fd8bd94cf5e2a8436d1079ec5199f
Timothy Lai <timothy.lai@smartbear.com>
2022-10-05 13:07:55 -0700

    feat(ls): additional oas31 lint rules

--- packages/apidom-ls/src/config/openapi/openapi3_1/lint/index.ts
+++ packages/apidom-ls/src/config/openapi/openapi3_1/lint/index.ts
@@ -1,8 +1,33 @@
 import allowedFieldsLint from './allowed-fields';
+import componentsTypeLint from './components--type';
+import externalDocsTypeLint from './external-docs--type';
 import infoRequiredLint from './info--required';
+import infoTypeLint from './info--type';
 import jsonSchemaDialectFormatURILint from './jsonSchemaDialect--format-uri';
+import pathsTypeLint from './paths--type';
+import securityItemsTypeLint from './security--items-type';
+import securityTypeLint from './security--type';
+import serversItemsTypeLint from './servers--items-type';
+import serversTypeLint from './servers--type';
+import tagsItemsTypeLint from './tags--items-type';
+import tagsTypeLint from './tags--type';
 import webhooksLint from './webhooks--type';
 
-const lints = [allowedFieldsLint, infoRequiredLint, jsonSchemaDialectFormatURILint, webhooksLint];
+const lints = [
+  allowedFieldsLint,
+  componentsTypeLint,
+  externalDocsTypeLint,
+  infoRequiredLint,
+  infoTypeLint,
+  jsonSchemaDialectFormatURILint,
+  pathsTypeLint,
+  securityItemsTypeLint,
+  securityTypeLint,
+  serversItemsTypeLint,
+  serversTypeLint,
+  tagsItemsTypeLint,
+  tagsTypeLint,
+  webhooksLint,
+];
 
 export default lints;

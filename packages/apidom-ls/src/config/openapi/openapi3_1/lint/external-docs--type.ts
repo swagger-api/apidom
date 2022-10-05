f758bca90f58199fa01787633ed92ac028045416
Timothy Lai <timothy.lai@smartbear.com>
2022-10-05 13:07:55 -0700

    feat(ls): additional oas31 lint rules

---
+++ packages/apidom-ls/src/config/openapi/openapi3_1/lint/external-docs--type.ts
@@ -0,0 +1,16 @@
+import ApilintCodes from '../../../codes';
+import { LinterMeta } from '../../../../apidom-language-types';
+
+const externalDocsTypeLint: LinterMeta = {
+  code: ApilintCodes.OPENAPI3_1_OPEN_API_FIELD_EXTERNAL_DOCS_TYPE,
+  source: 'apilint',
+  message: 'externalDocs must be an object',
+  severity: 1,
+  linterFunction: 'apilintElementOrClass',
+  linterParams: ['externalDocumentation'],
+  marker: 'value',
+  target: 'externalDocs',
+  data: {},
+};
+
+export default externalDocsTypeLint;

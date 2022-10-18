20a09d5f778947df0b7f490290381a8c56c1e15c
Tim Lai <timothy.lai@smartbear.com>
2022-10-18 11:20:09 -0700

    feat(ls): provide OpenAPI 3.1.0 License lint rules (#2136)

--- packages/apidom-ls/src/config/openapi/license/lint/index.ts
+++ packages/apidom-ls/src/config/openapi/license/lint/index.ts
@@ -1,8 +1,17 @@
 import allowedFields3_0Lint from './allowed-fields-3-0';
+import allowedFields3_1Lint from './allowed-fields-3-1';
 import nameTypeLint from './name--type';
 import nameRequiredLint from './name--required';
+import identifierMutuallyExclusiveLint from './identifier--mutually-exclusive';
 import urlFormatURILint from './url--format-uri';
 
-const lints = [nameTypeLint, nameRequiredLint, urlFormatURILint, allowedFields3_0Lint];
+const lints = [
+  nameTypeLint,
+  nameRequiredLint,
+  identifierMutuallyExclusiveLint,
+  urlFormatURILint,
+  allowedFields3_0Lint,
+  allowedFields3_1Lint,
+];
 
 export default lints;

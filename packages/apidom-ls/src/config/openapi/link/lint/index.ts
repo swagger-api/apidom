98b0c8b9691046e027d812fedf205da72e2c4373
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-10-24 15:28:32 +0200

    feat(ls): provide OpenAPI 3.0.x Link lint rules
    Refs #2033

---
+++ packages/apidom-ls/src/config/openapi/link/lint/index.ts
@@ -0,0 +1,15 @@
+import allowedFields3_0Lint from './allowed-fields-3-0';
+import operationRefFormatUriLink from './operation-ref--format-uri';
+import operationIdTypeLint from './operation-id--type';
+import descriptionTypeLint from './description--type';
+import serverTypeLint from './server--type';
+
+const links = [
+  operationRefFormatUriLink,
+  operationIdTypeLint,
+  descriptionTypeLint,
+  serverTypeLint,
+  allowedFields3_0Lint,
+];
+
+export default links;
---
+++ packages/apidom-ls/src/config/openapi/link/lint/operation-ref--format-uri.ts
@@ -0,0 +1,15 @@
+import ApilintCodes from '../../../codes';
+import { LinterMeta } from '../../../../apidom-language-types';
+
+const operationRefFormatURILint: LinterMeta = {
+  code: ApilintCodes.OPENAPI3_0_LINK_FIELD_OPERATION_REF_FORMAT_URI,
+  source: 'apilint',
+  message: "'operationRef' value must be a valid URI-reference",
+  severity: 1,
+  linterFunction: 'apilintValidURI',
+  marker: 'value',
+  target: 'operationRef',
+  data: {},
+};
+
+export default operationRefFormatURILint;

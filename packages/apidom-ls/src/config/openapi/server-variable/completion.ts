5c4b48c27570dce969bd25eac55fbc29629d0480
Timothy Lai <timothy.lai@smartbear.com>
2022-10-25 14:50:52 -0700

    feat(ls): provide OpenAPI 3.1.0 completion for Server Variable object

--- packages/apidom-ls/src/config/openapi/server-variable/completion.ts
+++ packages/apidom-ls/src/config/openapi/server-variable/completion.ts
@@ -25,6 +25,20 @@ const completion: ApidomCompletionItem[] = [
     ],
   },
   {
+    label: 'enum',
+    insertText: 'enum',
+    kind: 14,
+    format: CompletionFormat.ARRAY,
+    type: CompletionType.PROPERTY,
+    insertTextFormat: 2,
+    documentation: {
+      kind: 'markdown',
+      value:
+        'An enumeration of string values to be used if the substitution options are from a limited set. The array MUST NOT be empty.',
+    },
+    targetSpecs: [{ namespace: 'openapi', version: '3.1.0' }],
+  },
+  {
     label: 'default',
     insertText: 'default',
     kind: 14,
@@ -42,6 +56,20 @@ const completion: ApidomCompletionItem[] = [
       { namespace: 'openapi', version: '3.0.2' },
       { namespace: 'openapi', version: '3.0.3' },
     ],
+  },
+  {
+    label: 'default',
+    insertText: 'default',
+    kind: 14,
+    format: CompletionFormat.QUOTED,
+    type: CompletionType.PROPERTY,
+    insertTextFormat: 2,
+    documentation: {
+      kind: 'markdown',
+      value:
+        "**REQUIRED.** The default value to use for substitution, which SHALL be sent if an alternate value is *not* supplied. Note this behavior is different than the [Schema Object's](https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.0.md#schemaObject) treatment of default values, because in those cases parameter values are optional.If the [`enum`](https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.0.md#serverVariableEnum) is defined, the value MUST exist in the enum's values.",
+    },
+    targetSpecs: [{ namespace: 'openapi', version: '3.1.0' }],
   },
   {
     label: 'description',

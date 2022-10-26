c054a23c941eba91745f003be33aeb29892abf48
Timothy Lai <timothy.lai@smartbear.com>
2022-10-26 14:35:05 -0700

    feat(ls): provide OpenAPI 3.1.0 lint rules for Link object

--- packages/apidom-ls/src/config/openapi/link/lint/index.ts
+++ packages/apidom-ls/src/config/openapi/link/lint/index.ts
@@ -1,4 +1,5 @@
 import allowedFields3_0Lint from './allowed-fields-3-0';
+import allowedFields3_1Lint from './allowed-fields-3-1';
 import operationRefFormatUriLink from './operation-ref--format-uri';
 import operationIdTypeLint from './operation-id--type';
 import descriptionTypeLint from './description--type';
@@ -10,6 +11,7 @@ const links = [
   descriptionTypeLint,
   serverTypeLint,
   allowedFields3_0Lint,
+  allowedFields3_1Lint,
 ];
 
 export default links;

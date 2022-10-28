ae4c69bef8f51b1b32b067ad3d697f7e78345a49
Vladimír Gorej <vladimir.gorej@gmail.com>
2022-10-28 14:01:48 +0200

    fix(ls): provide OpenAPI 3.1 Request Body allowed fields lint rule (#2216)
    Refs #2061

--- packages/apidom-ls/src/config/openapi/request-body/lint/index.ts
+++ packages/apidom-ls/src/config/openapi/request-body/lint/index.ts
@@ -1,4 +1,5 @@
 import allowedFields3_0Lint from './allowed-fields-3-0';
+import allowedFields3_1Lint from './allowed-fields-3-1';
 import descriptionTypeLint from './description--type';
 import contentValuesTypeLint from './content--values-type';
 import contentRequiredLint from './content--required';
@@ -10,6 +11,7 @@ const lints = [
   contentValuesTypeLint,
   requiredTypeLint,
   allowedFields3_0Lint,
+  allowedFields3_1Lint,
 ];
 
 export default lints;

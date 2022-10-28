2536994aa8b71d452e16e97ccf079f1dac88ac24
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-10-28 13:14:41 +0200

    feat(ls): provide OpenAPI 3.1.0 Example allowed fields link rule
    Refs #2061

--- packages/apidom-ls/src/config/openapi/example/lint/index.ts
+++ packages/apidom-ls/src/config/openapi/example/lint/index.ts
@@ -1,4 +1,5 @@
 import allowedFields3_0Lint from './allowed-fields-3-0';
+import allowedFields3_1Lint from './allowed-fields-3-1';
 import summaryTypeLint from './summary--type';
 import descriptionTypeLint from './description--type';
 import valueMutuallyExclusiveLint from './value--mutually-exclusive';
@@ -10,6 +11,7 @@ const lints = [
   valueMutuallyExclusiveLint,
   externalValueFormatURILint,
   allowedFields3_0Lint,
+  allowedFields3_1Lint,
 ];
 
 export default lints;

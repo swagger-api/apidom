92ccc71b49816a3dfd8b2b8898dbd3a3119a5fbf
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-10-28 12:44:13 +0200

    fix(ls): provide spec extension link rule for Discriminator
    This change is specific to OpenAPI 3.1.0.
    
    Refs #2061

--- packages/apidom-ls/src/config/openapi/discriminator/lint/index.ts
+++ packages/apidom-ls/src/config/openapi/discriminator/lint/index.ts
@@ -1,4 +1,5 @@
 import allowedFields3_0Lint from './allowed-fields-3-0';
+import allowedFields3_1Lint from './allowed-fields-3-1';
 import propertyNameTypeLint from './property-name--type';
 import propertyNameRequiredLint from './property-name--required';
 import mappingTypeLint from './mapping--type';
@@ -8,6 +9,7 @@ const lints = [
   propertyNameRequiredLint,
   mappingTypeLint,
   allowedFields3_0Lint,
+  allowedFields3_1Lint,
 ];
 
 export default lints;

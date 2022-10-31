15e436c85719bcf1733ae19a1c827d951a34f9eb
Vladimír Gorej <vladimir.gorej@gmail.com>
2022-10-31 15:08:50 +0100

    feat(ns-asyncapi-2): add latest bindings versions in elements layer (#2224)
    Refs #2080

--- packages/apidom-ns-asyncapi-2/src/elements/bindings/kafka/KafkaChannelBinding.ts
+++ packages/apidom-ns-asyncapi-2/src/elements/bindings/kafka/KafkaChannelBinding.ts
@@ -1,10 +1,48 @@
-import { ObjectElement, Attributes, Meta } from '@swagger-api/apidom-core';
+import {
+  StringElement,
+  NumberElement,
+  ObjectElement,
+  Attributes,
+  Meta,
+} from '@swagger-api/apidom-core';
 
 class KafkaChannelBinding extends ObjectElement {
   constructor(content?: Record<string, unknown>, meta?: Meta, attributes?: Attributes) {
     super(content, meta, attributes);
     this.element = 'kafkaChannelBinding';
     this.classes.push('channel-binding');
+  }
+
+  get topic(): StringElement | undefined {
+    return this.get('topic');
+  }
+
+  set topic(topic: StringElement | undefined) {
+    this.set('topic', topic);
+  }
+
+  get partitions(): NumberElement | undefined {
+    return this.get('partitions');
+  }
+
+  set partitions(partitions: NumberElement | undefined) {
+    this.set('partitions', partitions);
+  }
+
+  get replicas(): NumberElement | undefined {
+    return this.get('replicas');
+  }
+
+  set replicas(replicas: NumberElement | undefined) {
+    this.set('replicas', replicas);
+  }
+
+  get bindingVersion(): StringElement | undefined {
+    return this.get('bindingVersion');
+  }
+
+  set bindingVersion(bindingVersion: StringElement | undefined) {
+    this.set('bindingVersion', bindingVersion);
   }
 }
 
--- packages/apidom-ns-asyncapi-2/src/elements/bindings/kafka/KafkaServerBinding.ts
+++ packages/apidom-ns-asyncapi-2/src/elements/bindings/kafka/KafkaServerBinding.ts
@@ -1,10 +1,34 @@
-import { ObjectElement, Attributes, Meta } from '@swagger-api/apidom-core';
+import { StringElement, ObjectElement, Attributes, Meta } from '@swagger-api/apidom-core';
 
 class KafkaServerBinding extends ObjectElement {
   constructor(content?: Record<string, unknown>, meta?: Meta, attributes?: Attributes) {
     super(content, meta, attributes);
     this.element = 'kafkaServerBinding';
     this.classes.push('server-binding');
+  }
+
+  get schemaRegistryUrl(): StringElement | undefined {
+    return this.get('schemaRegistryUrl');
+  }
+
+  set schemaRegistryUrl(schemaRegistryUrl: StringElement | undefined) {
+    this.set('schemaRegistryUrl', schemaRegistryUrl);
+  }
+
+  get schemaRegistryVendor(): StringElement | undefined {
+    return this.get('schemaRegistryVendor');
+  }
+
+  set schemaRegistryVendor(schemaRegistryVendor: StringElement | undefined) {
+    this.set('schemaRegistryVendor', schemaRegistryVendor);
+  }
+
+  get bindingVersion(): StringElement | undefined {
+    return this.get('bindingVersion');
+  }
+
+  set bindingVersion(bindingVersion: StringElement | undefined) {
+    this.set('bindingVersion', bindingVersion);
   }
 }
 

c1ab7e43227911c68d9afb7163275b26ba351462
Vladimír Gorej <vladimir.gorej@gmail.com>
2022-11-02 08:58:08 +0100

    refactor(ns-asyncapi-2): reuse visitors + remove unused ones (#2234)
    Refs #2081

--- packages/apidom-ns-asyncapi-2/src/refractor/visitors/async-api-2/bindings/amqp/operation-binding/AckVisitor.ts
+++
@@ -1,5 +0,0 @@
-import FallbackVisitor from '../../../../FallbackVisitor';
-
-const AckVisitor = FallbackVisitor;
-
-export default AckVisitor;
--- packages/apidom-ns-asyncapi-2/src/refractor/visitors/async-api-2/bindings/amqp/operation-binding/DeliveryModeVisitor.ts
+++
@@ -1,5 +0,0 @@
-import FallbackVisitor from '../../../../FallbackVisitor';
-
-const DeliveryModeVisitor = FallbackVisitor;
-
-export default DeliveryModeVisitor;

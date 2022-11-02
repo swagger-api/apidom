1b96465e9b87cf7f890dc74fa19de651b52c9f6a
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-11-02 11:52:49 +0100

    test: provide tests for AsyncAPI 2.5.0
    Refs #2083

---
+++ packages/apidom-reference/test/dereference/strategies/asyncapi-2/bindings/http/index.ts
@@ -0,0 +1,45 @@
+import path from 'node:path';
+import { assert } from 'chai';
+import { toValue } from '@swagger-api/apidom-core';
+import { mediaTypes } from '@swagger-api/apidom-ns-asyncapi-2';
+
+import { loadJsonFile } from '../../../../../helpers';
+import { dereference } from '../../../../../../src';
+
+const rootFixturePath = path.join(__dirname, 'fixtures');
+
+describe('dereference', function () {
+  context('strategies', function () {
+    context('asyncapi-2', function () {
+      context('HttpMessageBinding Object', function () {
+        context('given in components/messageBindings field', function () {
+          const fixturePath = path.join(rootFixturePath, 'components-message-bindings');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+
+        context('given in components/operationBindings field', function () {
+          const fixturePath = path.join(rootFixturePath, 'components-operation-bindings');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+      });
+    });
+  });
+});

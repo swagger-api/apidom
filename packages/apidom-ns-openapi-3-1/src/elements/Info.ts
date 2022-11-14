9ca3aa4abd40b447c12e9cd2f756e34edce8daa7
Vladimír Gorej <vladimir.gorej@gmail.com>
2022-11-14 09:36:15 +0100

    fix(ns-openapi-3-1): add Info.summary getters/setters (#2272)

--- packages/apidom-ns-openapi-3-1/src/elements/Info.ts
+++ packages/apidom-ns-openapi-3-1/src/elements/Info.ts
@@ -1,3 +1,4 @@
+import { StringElement } from '@swagger-api/apidom-core';
 import { InfoElement } from '@swagger-api/apidom-ns-openapi-3-0';
 
 import LicenseElement from './License';
@@ -9,6 +10,14 @@ class Info extends InfoElement {
 
   set license(licenseElement: LicenseElement | undefined) {
     this.set('license', licenseElement);
+  }
+
+  get summary(): StringElement | undefined {
+    return this.get('summary');
+  }
+
+  set summary(summary: StringElement | undefined) {
+    this.set('summary', summary);
   }
 }
 

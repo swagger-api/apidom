87ebbc831922c263708158537a57395c1e0a8688
frantuma <frantuma@yahoo.com>
2022-11-20 21:59:11 +0100

    test(ls): add test ensuring valid specs have no validation errors

---
+++ packages/apidom-ls/test/validate-valid.ts
@@ -0,0 +1,54 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { assert } from 'chai';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { Diagnostic, DiagnosticSeverity } from 'vscode-languageserver-types';
+
+import getLanguageService from '../src/apidom-language-service';
+import {
+  LanguageService,
+  LanguageServiceContext,
+  ValidationContext,
+} from '../src/apidom-language-types';
+import { metadata } from './metadata';
+import { logPerformance, logLevel } from './test-utils';
+
+describe('apidom-ls-validate', function () {
+  const context: LanguageServiceContext = {
+    metadata: metadata(),
+    performanceLogs: logPerformance,
+    logLevel,
+  };
+
+  it('test validation of supposedly valid specs', async function () {
+    const validationContext: ValidationContext = {
+      comments: DiagnosticSeverity.Error,
+      maxNumberOfProblems: 100,
+      relatedInformation: false,
+    };
+
+    const languageService: LanguageService = getLanguageService(context);
+    const oasPath = path.join(__dirname, 'fixtures', 'validation', 'oas', 'valid');
+    const asyncPath = path.join(__dirname, 'fixtures', 'validation', 'asyncapi', 'valid');
+    let dir = await fs.promises.opendir(oasPath);
+    try {
+      for await (const dirent of dir) {
+        console.log(`expecting ${path.join(dir.path, dirent.name)} to be valid`);
+        const specString = fs.readFileSync(path.join(dir.path, dirent.name)).toString();
+        const doc: TextDocument = TextDocument.create('foo://bar/doc.json', 'yaml', 0, specString);
+        const result = await languageService.doValidation(doc, validationContext);
+        assert.deepEqual(result, [] as Diagnostic[]);
+      }
+      dir = await fs.promises.opendir(asyncPath);
+      for await (const dirent of dir) {
+        console.log(`expecting ${path.join(dir.path, dirent.name)} to be valid`);
+        const specString = fs.readFileSync(path.join(dir.path, dirent.name)).toString();
+        const doc: TextDocument = TextDocument.create('foo://bar/doc.json', 'yaml', 0, specString);
+        const result = await languageService.doValidation(doc, validationContext);
+        assert.deepEqual(result, [] as Diagnostic[]);
+      }
+    } finally {
+      languageService.terminate();
+    }
+  });
+});

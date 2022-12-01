5bec67e4100f1fffd5b2a9e58edb1c7fbb693a20
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-12-01 15:27:21 +0100

    test(reference): add swagger-client JSON parser plugin
    This plugin mimics the JSON parsing mechanism of swagger-client.
    
    Refs #2317

---
+++ packages/apidom-reference/test/dereference/strategies/openapi-3-1-swagger-client/parsers/json/index.ts
@@ -0,0 +1,49 @@
+import stampit from 'stampit';
+import { ParseResultElement, from } from '@swagger-api/apidom-core';
+
+import { Parser as IParser, File as IFile } from '../../../../../../src/types';
+import Parser from '../../../../../../src/parse/parsers/Parser';
+import { ParserError } from '../../../../../../src';
+
+const JsonParser: stampit.Stamp<IParser> = stampit(Parser, {
+  props: {
+    name: 'json-swagger-client',
+    fileExtensions: ['.json'],
+    mediaTypes: ['application/json'],
+  },
+  methods: {
+    async canParse(file: IFile): Promise<boolean> {
+      const hasSupportedFileExtension =
+        this.fileExtensions.length === 0 ? true : this.fileExtensions.includes(file.extension);
+      const hasSupportedMediaType = this.mediaTypes.includes(file.mediaType);
+
+      if (!hasSupportedFileExtension) return false;
+      if (hasSupportedMediaType) return true;
+      if (!hasSupportedMediaType) {
+        try {
+          JSON.parse(file.toString());
+          return true;
+        } catch {
+          return false;
+        }
+      }
+      return false;
+    },
+    async parse(file: IFile): Promise<ParseResultElement> {
+      const source = file.toString();
+
+      try {
+        const element = from(JSON.parse(source));
+        const parseResultElement = new ParseResultElement();
+
+        element.classes.push('result');
+        parseResultElement.push(element);
+        return parseResultElement;
+      } catch (error: any) {
+        throw new ParserError(`Error parsing "${file.uri}"`, error);
+      }
+    },
+  },
+});
+
+export default JsonParser;

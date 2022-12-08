24e1635c6a2d4e8a1861510c648b642633f25abb
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-12-08 10:37:47 +0100

    fix(ls): fix linting of OpenAPI 3 Server Variable enum field
    OpenAPI 3.0.x - enum SHOULD NOT be empty
    OpenAPI 3.1.0 - enum MUST NOT be empty

--- packages/apidom-ls/src/config/openapi/server-variable/lint/enum--type-3-0.ts
+++ packages/apidom-ls/src/config/openapi/server-variable/lint/enum--type-3-0.ts
@@ -3,16 +3,23 @@ import { DiagnosticSeverity } from 'vscode-languageserver-types';
 import ApilintCodes from '../../../codes';
 import { LinterMeta } from '../../../../apidom-language-types';
 
-const enumTypeLint: LinterMeta = {
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const enumType3_0Lint: LinterMeta = {
   code: ApilintCodes.OPENAPI3_O_SERVER_VARIABLE_FIELD_ENUM_TYPE,
   source: 'apilint',
-  message: "'enum' must be an array of strings",
+  message: "'enum' should be an array of strings",
   severity: DiagnosticSeverity.Error,
   linterFunction: 'apilintArrayOfType',
   linterParams: ['string'],
   marker: 'key',
   target: 'enum',
   data: {},
+  targetSpecs: [
+    { namespace: 'openapi', version: '3.0.0' },
+    { namespace: 'openapi', version: '3.0.1' },
+    { namespace: 'openapi', version: '3.0.2' },
+    { namespace: 'openapi', version: '3.0.3' },
+  ],
 };
 
-export default enumTypeLint;
+export default enumType3_0Lint;
--- packages/apidom-ls/src/config/openapi/server-variable/lint/index.ts
+++ packages/apidom-ls/src/config/openapi/server-variable/lint/index.ts
@@ -1,11 +1,15 @@
 import allowedFieldsLint from './allowed-fields';
-import enumTypeLint from './enum--type';
+import enumType3_0Lint from './enum--type-3-0';
+import enumNonEmpty3_0Lint from './enum--non-empty-3-0';
+import enumType3_1Lint from './enum--type-3-1';
 import defaultTypeLint from './default--type';
 import defaultRequiredLint from './default--required';
 import descriptionTypeLint from './description--type';
 
 const lints = [
-  enumTypeLint,
+  enumType3_0Lint,
+  enumNonEmpty3_0Lint,
+  enumType3_1Lint,
   defaultTypeLint,
   defaultRequiredLint,
   descriptionTypeLint,

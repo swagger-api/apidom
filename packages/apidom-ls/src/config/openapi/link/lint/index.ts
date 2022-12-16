4342e3d46ff95785e480fca9059ee48ab9338ccd
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-12-16 11:01:57 +0100

    fix(ls): add OpenAPI 3.x lint rule for Link.operationRef field
    The rule is concerned with mutuall exclusivity
    between Link.operationId and Link.operationRef fields.

--- packages/apidom-ls/src/config/openapi/link/lint/index.ts
+++ packages/apidom-ls/src/config/openapi/link/lint/index.ts
@@ -1,12 +1,14 @@
 import allowedFields3_0Lint from './allowed-fields-3-0';
 import allowedFields3_1Lint from './allowed-fields-3-1';
-import operationRefFormatURILink from './operation-ref--format-uri';
+import operationRefFormatURILint from './operation-ref--format-uri';
+import operationRefMutuallyExclusiveLint from './operation-ref--mutually-exclusive';
 import operationIdTypeLint from './operation-id--type';
 import descriptionTypeLint from './description--type';
 import serverTypeLint from './server--type';
 
 const links = [
-  operationRefFormatURILink,
+  operationRefFormatURILint,
+  operationRefMutuallyExclusiveLint,
   operationIdTypeLint,
   descriptionTypeLint,
   serverTypeLint,

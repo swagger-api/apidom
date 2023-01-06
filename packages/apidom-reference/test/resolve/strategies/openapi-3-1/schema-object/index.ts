467e75c172e4057f74c55f948187000b628e3ad9
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-01-06 14:33:47 +0100

    test(reference): add test for resolving resolvable URL for OAS 3.1.0

--- packages/apidom-reference/test/resolve/strategies/openapi-3-1/schema-object/index.ts
+++ packages/apidom-reference/test/resolve/strategies/openapi-3-1/schema-object/index.ts
@@ -276,6 +276,19 @@ describe('resolve', function () {
           });
         });
 
+        context('given Schema Objects with $ref keyword containing resolvable URL', function () {
+          const fixturePath = path.join(rootFixturePath, '$ref-url-resolvable');
+
+          specify('should resolve', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const refSet = await resolve(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+
+            assert.strictEqual(refSet.size, 2);
+          });
+        });
+
         context(
           'given Schema Objects with $ref keyword containing Uniform Resource Name',
           function () {

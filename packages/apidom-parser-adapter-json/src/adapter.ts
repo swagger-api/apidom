678b44003c6241d7e97d36d5c440d2696de4e761
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-01-25 14:15:05 +0100

    fix(parser-adapter-json): fix issues in JSON detection regexp (#2502)
    One of the main issues was inefficient regular expression
    detected by CodeQL.
    
    Closes #2503

--- packages/apidom-parser-adapter-json/src/adapter.ts
+++ packages/apidom-parser-adapter-json/src/adapter.ts
@@ -5,4 +5,5 @@ export { default as mediaTypes } from './media-types';
 export const namespace = createNamespace();
 
 export const detectionRegExp =
-  /(^\s*true\s$)|(^\s*false\s*$)|(^\s*null\s*$)|(^\s*\d+\s*$)|(^\s*{\s*)|(^\s*\[\s*)|(^\s*"([^"\\\\]*|\\\\["\\\\bfnrt/]|\\\\u[0-9a-f]{4})*"\s*$)/;
+  // eslint-disable-next-line no-control-regex
+  /(?<true>^\s*true\s*$)|(?<false>^\s*false\s*$)|(?<null>^\s*null\s*$)|(?<number>^\s*\d+\s*$)|(?<object>^\s*{\s*)|(?<array>^\s*\[\s*)|(?<string>^\s*"(((?=\\)\\(["\\/bfnrt]|u[0-9a-fA-F]{4}))|[^"\\\x00-\x1F\x7F])*"\s*$)/;

8f8e45f06cce2a7fb6f1372b50fcb1adecf44d2f
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-01-25 13:43:40 +0100

    fix: regex

--- packages/apidom-parser-adapter-json/src/adapter.ts
+++ packages/apidom-parser-adapter-json/src/adapter.ts
@@ -6,4 +6,4 @@ export const namespace = createNamespace();
 
 export const detectionRegExp =
   // eslint-disable-next-line no-control-regex
-  /(?<true>^\s*true\s*$)|(?<false>^\s*false\s*$)|(?<null>^\s*null\s*$)|(?<number>^\s*\d+\s*$)|(?<object>^\s*{\s*)|(?<array>^\s*\[\s*)|(?<string>^\s*"(((?=\\)\\(["\\\b\f\n\r\t]|u[0-9a-fA-F]{4}))|[^"\\\0-\x1F\x7F]+)*"\s*$)/;
+  /(?<true>^\s*true\s*$)|(?<false>^\s*false\s*$)|(?<null>^\s*null\s*$)|(?<number>^\s*\d+\s*$)|(?<object>^\s*{\s*)|(?<array>^\s*\[\s*)|(?<string>^\s*"(((?=\\)\\(["\\/bfnrt]|u[0-9a-fA-F]{4}))|[^"\\\x00-\x1F\x7F]+)*"\s*$)/;

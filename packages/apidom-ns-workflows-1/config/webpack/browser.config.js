ae1b9edf20ae50174e8e0fd2946a3b45cffef0b9
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-02-07 09:28:43 +0100

    chore: scaffold namespace package for Workflows Specification 1.0.0 (#2528)
    Refs #2521

---
+++ packages/apidom-ns-workflows-1/config/webpack/browser.config.js
@@ -0,0 +1,74 @@
+import path from 'node:path';
+import { nonMinimizeTrait, minimizeTrait } from './traits.config.js';
+
+const browser = {
+  mode: 'production',
+  entry: ['./src/index.ts'],
+  target: 'web',
+  performance: {
+    maxEntrypointSize: 2000000,
+    maxAssetSize: 2000000,
+  },
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'apidom-ns-workflows-1.browser.js',
+    libraryTarget: 'umd',
+    library: 'apidomNsWorkflows1',
+  },
+  resolve: {
+    extensions: ['.ts', '.mjs', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: true,
+            rootMode: 'upward',
+          },
+        },
+      },
+    ],
+  },
+  ...nonMinimizeTrait,
+};
+
+const browserMin = {
+  mode: 'production',
+  entry: ['./src/index.ts'],
+  target: 'web',
+  performance: {
+    maxEntrypointSize: 280000,
+    maxAssetSize: 280000,
+  },
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'apidom-ns-workflows-1.browser.min.js',
+    libraryTarget: 'umd',
+    library: 'apidomNsWorkflows1',
+  },
+  resolve: {
+    extensions: ['.ts', '.mjs', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: true,
+            rootMode: 'upward',
+          },
+        },
+      },
+    ],
+  },
+  ...minimizeTrait,
+};
+
+export default [browser, browserMin];
---
+++ packages/apidom-ns-workflows-1/src/index.ts
@@ -0,0 +1,3 @@
+const variable = 'value';
+
+export default variable;

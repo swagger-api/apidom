d1976cdefa31d2e9b036ae1902d7949962fe5997
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-04-24 12:29:21 +0200

    chore: ramda types

--- packages/apidom-core/src/traversal/find.ts
+++ packages/apidom-core/src/traversal/find.ts
@@ -1,17 +1,15 @@
-import { curry, pathOr, Pred } from 'ramda';
+import { pathOr, Pred } from 'ramda';
 import { Element } from 'minim';
 
 import { PredicateVisitor, BREAK, visit } from './visitor';
 
 // find first element that satisfies the provided predicate
-// find :: Pred -> Element -> Element | Undefined
-const find = curry(<T extends Element>(predicate: Pred, element: T): T | undefined => {
+const find = <T extends Element>(predicate: Pred, element: T): T | undefined => {
   const visitor = PredicateVisitor({ predicate, returnOnTrue: BREAK });
 
-  // @ts-ignore
   visit(element, visitor);
 
   return pathOr(undefined, [0], visitor.result);
-});
+};
 
 export default find;
--- packages/apidom-core/src/traversal/some.ts
+++ packages/apidom-core/src/traversal/some.ts
@@ -1,13 +1,12 @@
-import { curry, Pred } from 'ramda';
+import { Pred } from 'ramda';
 import { isNotUndefined } from 'ramda-adjunct';
 import { Element } from 'minim';
 
 import find from './find';
 
 // tests whether at least one element passes the predicate
-// some :: Pred -> Element -> Boolean
-const some = curry(<T extends Element>(predicate: Pred, element: T): boolean => {
-  return isNotUndefined(find(predicate)(element));
-});
+const some = <T extends Element>(predicate: Pred, element: T): boolean => {
+  return isNotUndefined(find(predicate, element));
+};
 
 export default some;

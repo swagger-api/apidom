55d1b3838996560ca3c7ba0b0da87fac136d89a1
frantuma <frantuma@yahoo.com>
2023-05-05 09:54:35 +0200

    feat(apidom-ls): update exports for JSON Schema validators

--- packages/apidom-ls/src/index.ts
+++ packages/apidom-ls/src/index.ts
@@ -12,16 +12,6 @@ export {
 } from '@swagger-api/apidom-core';
 
 export { default as getLanguageService } from './apidom-language-service';
-export { JsonSchemaValidationProvider } from './services/validation/providers/json-schema-validation-provider';
-export { Asyncapi20JsonSchemaValidationProvider } from './services/validation/providers/asyncapi-20-json-schema-validation-provider';
-export { Asyncapi21JsonSchemaValidationProvider } from './services/validation/providers/asyncapi-21-json-schema-validation-provider';
-export { Asyncapi22JsonSchemaValidationProvider } from './services/validation/providers/asyncapi-22-json-schema-validation-provider';
-export { Asyncapi23JsonSchemaValidationProvider } from './services/validation/providers/asyncapi-23-json-schema-validation-provider';
-export { Asyncapi24JsonSchemaValidationProvider } from './services/validation/providers/asyncapi-24-json-schema-validation-provider';
-export { Asyncapi25JsonSchemaValidationProvider } from './services/validation/providers/asyncapi-25-json-schema-validation-provider';
-export { Asyncapi26JsonSchemaValidationProvider } from './services/validation/providers/asyncapi-26-json-schema-validation-provider';
-export { OpenAPi31JsonSchemaValidationProvider } from './services/validation/providers/openapi-31-json-schema-validation-provider';
-export { AdsValidationProvider } from './services/validation/providers/ads-validation-provider';
 
 export {
   perfStart,

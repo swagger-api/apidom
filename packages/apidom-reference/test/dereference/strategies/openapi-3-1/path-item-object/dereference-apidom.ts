2e0263a5c8e35a06d7d5229dff9d78733df234b6
Vladimir Gorej <vladimir.gorej@smartbear.com>
2023-07-13 14:24:11 +0200

    feat(reference): support native fragment - Path Item
    Refs #2934

--- packages/apidom-reference/test/dereference/strategies/openapi-3-1/path-item-object/dereference-apidom.ts
+++ packages/apidom-reference/test/dereference/strategies/openapi-3-1/path-item-object/dereference-apidom.ts
@@ -13,41 +13,85 @@ describe('dereference', function () {
   context('strategies', function () {
     context('openapi-3-1', function () {
       context('Path Item Object', function () {
-        context('given single PathItemElement passed to dereferenceApiDOM', function () {
-          const fixturePath = path.join(__dirname, 'fixtures', 'external-only', 'root.json');
+        context(
+          'given single PathItemElement passed to dereferenceApiDOM with internal references',
+          function () {
+            const fixturePath = path.join(__dirname, 'fixtures', 'internal-only', 'root.json');
 
-          specify('should dereference', async function () {
-            const parseResult = await parse(fixturePath, {
-              parse: { mediaType: mediaTypes.latest('json') },
-            });
-            const pathItemElement = evaluate(
-              compile(['paths', '/path1']),
-              parseResult.api as OpenApi3_1Element,
-            );
-            const dereferenced = await dereferenceApiDOM(pathItemElement, {
-              parse: { mediaType: mediaTypes.latest('json') },
-              resolve: { baseURI: fixturePath },
-            });
+            specify('should dereference', async function () {
+              const parseResult = await parse(fixturePath, {
+                parse: { mediaType: mediaTypes.latest('json') },
+              });
+              const jsonPointer = compile(['paths', '/path1']);
+              const pathItemElement = evaluate(jsonPointer, parseResult.api as OpenApi3_1Element);
+              const dereferenced = await dereferenceApiDOM(pathItemElement, {
+                parse: { mediaType: mediaTypes.latest('json') },
+                resolve: { baseURI: `${fixturePath}#${jsonPointer}` },
+              });
 
-            assert.isTrue(isPathItemElement(dereferenced));
-          });
-
-          specify('should dereference and contain metadata about origin', async function () {
-            const parseResult = await parse(fixturePath, {
-              parse: { mediaType: mediaTypes.latest('json') },
-            });
-            const pathItemElement = evaluate(
-              compile(['paths', '/path1']),
-              parseResult.api as OpenApi3_1Element,
-            );
-            const dereferenced = await dereferenceApiDOM(pathItemElement, {
-              parse: { mediaType: mediaTypes.latest('json') },
-              resolve: { baseURI: fixturePath },
+              assert.isTrue(isPathItemElement(dereferenced));
             });
 
-            assert.match(dereferenced.meta.get('ref-origin').toValue(), /external-only\/ex\.json$/);
-          });
-        });
+            specify('should dereference and contain metadata about origin', async function () {
+              const jsonPointer = compile(['paths', '/path1']);
+              const parseResult = await parse(fixturePath, {
+                parse: { mediaType: mediaTypes.latest('json') },
+              });
+              const pathItemElement = evaluate(jsonPointer, parseResult.api as OpenApi3_1Element);
+              const dereferenced = await dereferenceApiDOM(pathItemElement, {
+                parse: { mediaType: mediaTypes.latest('json') },
+                resolve: { baseURI: `${fixturePath}#${jsonPointer}` },
+              });
+
+              assert.match(
+                dereferenced.meta.get('ref-origin').toValue(),
+                /internal-only\/root\.json$/,
+              );
+            });
+          },
+        );
+
+        context(
+          'given single PathItemElement passed to dereferenceApiDOM with external references',
+          function () {
+            const fixturePath = path.join(__dirname, 'fixtures', 'external-only', 'root.json');
+
+            specify('should dereference', async function () {
+              const parseResult = await parse(fixturePath, {
+                parse: { mediaType: mediaTypes.latest('json') },
+              });
+              const pathItemElement = evaluate(
+                compile(['paths', '/path1']),
+                parseResult.api as OpenApi3_1Element,
+              );
+              const dereferenced = await dereferenceApiDOM(pathItemElement, {
+                parse: { mediaType: mediaTypes.latest('json') },
+                resolve: { baseURI: fixturePath },
+              });
+
+              assert.isTrue(isPathItemElement(dereferenced));
+            });
+
+            specify('should dereference and contain metadata about origin', async function () {
+              const parseResult = await parse(fixturePath, {
+                parse: { mediaType: mediaTypes.latest('json') },
+              });
+              const pathItemElement = evaluate(
+                compile(['paths', '/path1']),
+                parseResult.api as OpenApi3_1Element,
+              );
+              const dereferenced = await dereferenceApiDOM(pathItemElement, {
+                parse: { mediaType: mediaTypes.latest('json') },
+                resolve: { baseURI: fixturePath },
+              });
+
+              assert.match(
+                dereferenced.meta.get('ref-origin').toValue(),
+                /external-only\/ex\.json$/,
+              );
+            });
+          },
+        );
       });
     });
   });

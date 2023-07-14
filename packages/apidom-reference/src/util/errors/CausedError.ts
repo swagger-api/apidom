32bd184c33eb7555a78b636db5e57ec331ba522f
Vladimír Gorej <vladimir.gorej@smartbear.com>
2023-07-14 12:14:48 +0200

    Update packages/apidom-reference/src/util/errors/CausedError.ts

--- packages/apidom-reference/src/util/errors/CausedError.ts
+++ packages/apidom-reference/src/util/errors/CausedError.ts
@@ -1,6 +1,10 @@
 import { hasIn } from 'ramda';
 import { isPlainObject } from 'ramda-adjunct';
 
+/**
+ * This needs to stay here until our minimum supported version of Node.js is >= 16.9.0.
+ * Node.js is >= 16.9.0 supports error causes natively.
+ */
 class CausedError extends Error {
   constructor(message: string, options?: { cause?: Error }) {
     super(message);

aba543498bcc841d4db06ba434af5cd029433b8c
Vladimír Gorej <vladimir.gorej@smartbear.com>
2023-08-28 10:29:58 +0200

    feat(error): introduce generic custom errors (#3077)
    Refs #3039

--- packages/apidom-error/src/index.ts
+++ packages/apidom-error/src/index.ts
@@ -1,4 +1,8 @@
+// base error classes
 export { default as ApiDOMError } from './ApiDOMError';
 export { default as ApiDOMAggregateError } from './ApiDOMAggregateError';
 export { default as ApiDOMStructuredError } from './ApiDOMStructuredError';
 export type { default as ApiDOMErrorOptions } from './ApiDOMErrorOptions';
+// generic custom error classes
+export { default as UnsupportedOperationError } from './UnsupportedOperationError';
+export { default as NotImplementedError } from './NotImplementedError';

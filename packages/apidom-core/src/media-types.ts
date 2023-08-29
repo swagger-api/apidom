b83509dadbf389cb6a9feb44725035dfddf91266
Vladimir Gorej <vladimir.gorej@smartbear.com>
2023-08-29 12:34:01 +0200

    refactor(core): use more appropriate semantic errors
    Refs #3039

--- packages/apidom-core/src/media-types.ts
+++ packages/apidom-core/src/media-types.ts
@@ -1,21 +1,21 @@
-import { ApiDOMError } from '@swagger-api/apidom-error';
+import { NotImplementedError } from '@swagger-api/apidom-error';
 
 class MediaTypes<T> extends Array<T> {
   unknownMediaType = 'application/octet-stream';
 
   // eslint-disable-next-line class-methods-use-this
   filterByFormat() {
-    throw new ApiDOMError('Not implemented!');
+    throw new NotImplementedError('"filterByFormat" method is not implemented.');
   }
 
   // eslint-disable-next-line class-methods-use-this
   findBy() {
-    throw new ApiDOMError('Not implemented');
+    throw new NotImplementedError('"findBy" method is not implemented.');
   }
 
   // eslint-disable-next-line class-methods-use-this
   latest() {
-    throw new ApiDOMError('Not implemented!');
+    throw new NotImplementedError('"latest" method is not implemented.');
   }
 }
 

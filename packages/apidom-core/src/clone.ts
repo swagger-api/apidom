ebe72b58071f69466537e934746ec93d288c96b2
Vladimir Gorej <vladimir.gorej@smartbear.com>
2023-09-20 13:54:48 +0200

    fix: types

--- packages/apidom-core/src/clone.ts
+++ packages/apidom-core/src/clone.ts
@@ -1,61 +1,100 @@
 import { ArraySlice, ObjectSlice, KeyValuePair, Element } from 'minim';
-import { ApiDOMError } from '@swagger-api/apidom-error';
+import { ApiDOMStructuredError } from '@swagger-api/apidom-error';
 
 import { isElement } from './predicates';
 
+type FinalCloneTypes = KeyValuePair | ArraySlice | ObjectSlice;
+
+const cloneShallowKeyValuePair = (keyValuePair: KeyValuePair) => {
+  const { key, value } = keyValuePair;
+  return new KeyValuePair(key, value);
+};
+
+const cloneShallowArraySlice = (arraySlice: ArraySlice) => {
+  const items = [...arraySlice];
+  return new ArraySlice(items);
+};
+
+const cloneShallowObjectSlice = (objectSlice: ObjectSlice) => {
+  const items = [...objectSlice];
+  return new ObjectSlice(items);
+};
+
 /* eslint-disable no-underscore-dangle */
-
-export const cloneShallow = <T extends Element>(element: T): T => {
-  if (!isElement(element)) {
-    throw new ApiDOMError('cloneShallow was provided a value that is not an ApiDOM element.');
-  }
-
+const cloneShallowElement = <T extends Element>(element: T): T => {
   // @ts-ignore
-  const clone = new element.constructor();
-  clone.element = element.element;
+  const copy = new element.constructor();
+
+  copy.element = element.element;
 
   if (element.meta.length > 0) {
-    clone._meta = cloneShallow(element.meta);
+    copy._meta = cloneShallowElement(element.meta);
   }
 
   if (element.attributes.length > 0) {
-    clone._attributes = cloneShallow(element.attributes);
+    copy._attributes = cloneShallowElement(element.attributes);
   }
 
-  if (element.content) {
-    if (isElement(element.content)) {
-      clone.content = cloneShallow(element.content as unknown as Element);
-    } else if (Array.isArray(element.content)) {
-      clone.content = [...element.content];
-    } else if ((element.content as unknown) instanceof ObjectSlice) {
-      clone.content = new ObjectSlice([...element.content]);
-    } else if ((element.content as unknown) instanceof ArraySlice) {
-      clone.content = new ArraySlice([...element.content]);
-    } else if ((element.content as unknown) instanceof KeyValuePair) {
-      clone.content = new KeyValuePair(
-        (element.content as KeyValuePair).key,
-        (element.content as KeyValuePair).value,
-      );
-    } else {
-      clone.content = element.content;
-    }
+  if (isElement(element.content)) {
+    const content = element.content as unknown as Element;
+    copy.content = cloneShallowElement(content);
+  } else if (Array.isArray(element.content)) {
+    copy.content = [...element.content];
+  } else if ((element.content as unknown) instanceof KeyValuePair) {
+    copy.content = cloneShallowKeyValuePair(element.content);
   } else {
-    clone.content = element.content;
+    copy.content = element.content;
   }
 
-  return clone;
+  return copy;
 };
-
-export const cloneDeep = <T extends Element | KeyValuePair | ArraySlice>(element: T): T => {
-  if (
-    !isElement(element) &&
-    !((element as unknown) instanceof KeyValuePair) &&
-    !((element as unknown) instanceof ArraySlice)
-  ) {
-    throw new ApiDOMError('cloneShallow was provided a value that is not an ApiDOM element.');
-  }
-
-  return element.clone() as T;
-};
-
 /* eslint-enable */
+
+export const cloneShallow = <T extends Element | FinalCloneTypes>(value: T): T => {
+  if (value instanceof KeyValuePair) {
+    return cloneShallowKeyValuePair(value) as T;
+  }
+
+  if (value instanceof ObjectSlice) {
+    return cloneShallowObjectSlice(value) as T;
+  }
+
+  if (value instanceof ArraySlice) {
+    return cloneShallowArraySlice(value) as T;
+  }
+
+  if (isElement(value)) {
+    return cloneShallowElement(value) as T;
+  }
+
+  throw new ApiDOMStructuredError("Value provided to cloneShallow function couldn't be cloned", {
+    value,
+  });
+};
+
+const invokeClone = <T extends Element | FinalCloneTypes>(value: T): T => {
+  if (typeof value?.clone === 'function') {
+    return value.clone() as T;
+  }
+  return value;
+};
+
+export const cloneDeep = <T extends Element | FinalCloneTypes>(value: T): T => {
+  if (value instanceof ObjectSlice) {
+    const items = [...value].map(invokeClone) as T[];
+    return new ObjectSlice(items) as T;
+  }
+
+  if (value instanceof ArraySlice) {
+    const items = [...value].map(invokeClone) as T[];
+    return new ArraySlice(items) as T;
+  }
+
+  if (typeof value?.clone === 'function') {
+    return value.clone() as T;
+  }
+
+  throw new ApiDOMStructuredError("Value provided to cloneDeep function couldn't be cloned", {
+    value,
+  });
+};

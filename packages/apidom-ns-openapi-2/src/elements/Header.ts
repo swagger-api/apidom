ca6af8dedd88be110d0f4a6e26415f58a068fce2
Vladimir Gorej <vladimir.gorej@smartbear.com>
2023-10-10 16:08:22 +0200

    fix: naming

--- packages/apidom-ns-openapi-2/src/elements/Header.ts
+++ packages/apidom-ns-openapi-2/src/elements/Header.ts
@@ -15,7 +15,7 @@ import {
 } from '@swagger-api/apidom-ns-json-schema-draft-4';
 
 /* eslint-disable class-methods-use-this */
-class Items extends JSONSchemaElement {
+class Header extends JSONSchemaElement {
   constructor(content?: Record<string, unknown>, meta?: Meta, attributes?: Attributes) {
     super(content, meta, attributes);
     this.element = 'header';
@@ -29,19 +29,19 @@ class Items extends JSONSchemaElement {
    */
 
   get idProp(): StringElement | undefined {
-    throw new UnsupportedOperationError('idProp getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('idProp getter in Header class is not not supported.');
   }
 
   set idProp(idProps: StringElement | undefined) {
-    throw new UnsupportedOperationError('idProp setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('idProp setter in Header class is not not supported.');
   }
 
   get $schema(): StringElement | undefined {
-    throw new UnsupportedOperationError('$schema getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('$schema getter in Header class is not not supported.');
   }
 
   set $schema($schema: StringElement | undefined) {
-    throw new UnsupportedOperationError('$schema setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('$schema setter in Header class is not not supported.');
   }
 
   /**
@@ -50,13 +50,13 @@ class Items extends JSONSchemaElement {
 
   get additionalItems(): this | JSONReferenceElement | BooleanElement | undefined {
     throw new UnsupportedOperationError(
-      'additionalItems getter in Items class is not not supported.',
+      'additionalItems getter in Header class is not not supported.',
     );
   }
 
   set additionalItems(additionalItems: this | JSONReferenceElement | BooleanElement | undefined) {
     throw new UnsupportedOperationError(
-      'additionalItems setter in Items class is not not supported.',
+      'additionalItems setter in Header class is not not supported.',
     );
   }
 
@@ -74,47 +74,47 @@ class Items extends JSONSchemaElement {
 
   get maxProperties(): NumberElement | undefined {
     throw new UnsupportedOperationError(
-      'maxProperties getter in Items class is not not supported.',
+      'maxProperties getter in Header class is not not supported.',
     );
   }
 
   set maxProperties(maxProperties: NumberElement | undefined) {
     throw new UnsupportedOperationError(
-      'maxProperties setter in Items class is not not supported.',
+      'maxProperties setter in Header class is not not supported.',
     );
   }
 
   get minProperties(): NumberElement | undefined {
     throw new UnsupportedOperationError(
-      'minProperties getter in Items class is not not supported.',
+      'minProperties getter in Header class is not not supported.',
     );
   }
 
   set minProperties(minProperties: NumberElement | undefined) {
     throw new UnsupportedOperationError(
-      'minProperties setter in Items class is not not supported.',
+      'minProperties setter in Header class is not not supported.',
     );
   }
 
   get required(): ArrayElement | undefined {
-    throw new UnsupportedOperationError('required getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('required getter in Header class is not not supported.');
   }
 
   set required(required: ArrayElement | undefined) {
-    throw new UnsupportedOperationError('required setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('required setter in Header class is not not supported.');
   }
 
   get properties(): ObjectElement | undefined {
-    throw new UnsupportedOperationError('properties getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('properties getter in Header class is not not supported.');
   }
 
   set properties(properties: ObjectElement | undefined) {
-    throw new UnsupportedOperationError('properties setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('properties setter in Header class is not not supported.');
   }
 
   get additionalProperties(): this | JSONReferenceElement | BooleanElement | undefined {
     throw new UnsupportedOperationError(
-      'additionalProperties getter in Items class is not not supported.',
+      'additionalProperties getter in Header class is not not supported.',
     );
   }
 
@@ -122,28 +122,32 @@ class Items extends JSONSchemaElement {
     additionalProperties: this | JSONReferenceElement | BooleanElement | undefined,
   ) {
     throw new UnsupportedOperationError(
-      'additionalProperties setter in Items class is not not supported.',
+      'additionalProperties setter in Header class is not not supported.',
     );
   }
 
   get patternProperties(): ObjectElement | undefined {
     throw new UnsupportedOperationError(
-      'patternProperties getter in Items class is not not supported.',
+      'patternProperties getter in Header class is not not supported.',
     );
   }
 
   set patternProperties(patternProperties: ObjectElement | undefined) {
     throw new UnsupportedOperationError(
-      'patternProperties setter in Items class is not not supported.',
+      'patternProperties setter in Header class is not not supported.',
     );
   }
 
   get dependencies(): ObjectElement | undefined {
-    throw new UnsupportedOperationError('dependencies getter in Items class is not not supported.');
+    throw new UnsupportedOperationError(
+      'dependencies getter in Header class is not not supported.',
+    );
   }
 
   set dependencies(dependencies: ObjectElement | undefined) {
-    throw new UnsupportedOperationError('dependencies setter in Items class is not not supported.');
+    throw new UnsupportedOperationError(
+      'dependencies setter in Header class is not not supported.',
+    );
   }
 
   /**
@@ -159,43 +163,43 @@ class Items extends JSONSchemaElement {
   }
 
   get allOf(): ArrayElement | undefined {
-    throw new UnsupportedOperationError('allOf getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('allOf getter in Header class is not not supported.');
   }
 
   set allOf(allOf: ArrayElement | undefined) {
-    throw new UnsupportedOperationError('allOf setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('allOf setter in Header class is not not supported.');
   }
 
   get anyOf(): ArrayElement | undefined {
-    throw new UnsupportedOperationError('anyOf getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('anyOf getter in Header class is not not supported.');
   }
 
   set anyOf(anyOf: ArrayElement | undefined) {
-    throw new UnsupportedOperationError('anyOf setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('anyOf setter in Header class is not not supported.');
   }
 
   get oneOf(): ArrayElement | undefined {
-    throw new UnsupportedOperationError('oneOf getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('oneOf getter in Header class is not not supported.');
   }
 
   set oneOf(oneOf: ArrayElement | undefined) {
-    throw new UnsupportedOperationError('oneOf setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('oneOf setter in Header class is not not supported.');
   }
 
   get not(): this | JSONReferenceElement | undefined {
-    throw new UnsupportedOperationError('not getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('not getter in Header class is not not supported.');
   }
 
   set not(not: this | JSONReferenceElement | undefined) {
-    throw new UnsupportedOperationError('not setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('not setter in Header class is not not supported.');
   }
 
   get definitions(): ObjectElement | undefined {
-    throw new UnsupportedOperationError('definitions getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('definitions getter in Header class is not not supported.');
   }
 
   set definitions(definitions: ObjectElement | undefined) {
-    throw new UnsupportedOperationError('definitions setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('definitions setter in Header class is not not supported.');
   }
 
   /**
@@ -205,11 +209,11 @@ class Items extends JSONSchemaElement {
    */
 
   get title(): StringElement | undefined {
-    throw new UnsupportedOperationError('title getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('title getter in Header class is not not supported.');
   }
 
   set title(title: StringElement | undefined) {
-    throw new UnsupportedOperationError('title setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('title setter in Header class is not not supported.');
   }
 
   get description(): StringElement | undefined {
@@ -241,37 +245,37 @@ class Items extends JSONSchemaElement {
    */
 
   get base(): StringElement | undefined {
-    throw new UnsupportedOperationError('base getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('base getter in Header class is not not supported.');
   }
 
   set base(base: StringElement | undefined) {
-    throw new UnsupportedOperationError('base setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('base setter in Header class is not not supported.');
   }
 
   get links(): ArrayElement | undefined {
-    throw new UnsupportedOperationError('links getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('links getter in Header class is not not supported.');
   }
 
   set links(links: ArrayElement | undefined) {
-    throw new UnsupportedOperationError('links setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('links setter in Header class is not not supported.');
   }
 
   get media(): MediaElement | undefined {
-    throw new UnsupportedOperationError('media getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('media getter in Header class is not not supported.');
   }
 
   set media(media: MediaElement | undefined) {
-    throw new UnsupportedOperationError('media setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('media setter in Header class is not not supported.');
   }
 
   get readOnly(): BooleanElement | undefined {
-    throw new UnsupportedOperationError('readOnly getter in Items class is not not supported.');
+    throw new UnsupportedOperationError('readOnly getter in Header class is not not supported.');
   }
 
   set readOnly(readOnly: BooleanElement | undefined) {
-    throw new UnsupportedOperationError('readOnly setter in Items class is not not supported.');
+    throw new UnsupportedOperationError('readOnly setter in Header class is not not supported.');
   }
 }
 /* eslint-enable class-methods-use-this */
 
-export default Items;
+export default Header;

3fc7524695855d8e93956c4ba78aaa4e3e140e29
Vladimir Gorej <vladimir.gorej@smartbear.com>
2023-10-17 07:40:53 +0200

    fix: types

--- packages/apidom-core/src/traversal/filter.ts
+++ packages/apidom-core/src/traversal/filter.ts
@@ -1,10 +1,12 @@
-import { Pred } from 'ramda';
 import { ArraySlice, Element } from 'minim';
 
 import { PredicateVisitor, visit } from './visitor';
 
 // finds all elements matching the predicate
-const filter = <T extends Element>(predicate: Pred, element: T): ArraySlice => {
+const filter = <T extends Element>(
+  predicate: (val: unknown) => boolean,
+  element: T,
+): ArraySlice => {
   const visitor = PredicateVisitor({ predicate });
 
   visit(element, visitor);

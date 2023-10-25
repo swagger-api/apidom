a9ba0a5e2ad04395682cd9e7bb2feea2e4473fe3
Vladimír Gorej <vladimir.gorej@smartbear.com>
2023-10-25 16:52:33 +0200

    feat(core): support cycles in deep cloning (#3322)
    Before this change, deepClone function supported
    directed acyclic trees. With this change the
    function will handle directed cyclic graphs.
    
    Refs #3290

--- packages/apidom-core/src/clone/index.ts
+++ packages/apidom-core/src/clone/index.ts
@@ -6,26 +6,78 @@ import ShallowCloneError from './errors/ShallowCloneError';
 
 type FinalCloneTypes = KeyValuePair | ArraySlice | ObjectSlice;
 
-const invokeClone = <T extends Element | FinalCloneTypes>(value: T): T => {
-  if (typeof value?.clone === 'function') {
-    return value.clone() as T;
-  }
-  return value;
+type DeepCloneOptions<T extends Element | FinalCloneTypes> = {
+  visited?: WeakMap<T, T>;
 };
 
-export const cloneDeep = <T extends Element | FinalCloneTypes>(value: T): T => {
+export const cloneDeep = <T extends Element | FinalCloneTypes>(
+  value: T,
+  options: DeepCloneOptions<T> = {},
+): T => {
+  const { visited = new WeakMap<T, T>() } = options;
+  const passThroughOptions = { ...options, visited };
+
+  // detect cycle and return memoized value
+  if (visited.has(value)) {
+    return visited.get(value) as T;
+  }
+
+  if (value instanceof KeyValuePair) {
+    const { key, value: val } = value;
+    const keyCopy = isElement(key)
+      ? cloneDeep(key, passThroughOptions as DeepCloneOptions<Element>)
+      : key;
+    const valueCopy = isElement(val)
+      ? cloneDeep(val, passThroughOptions as DeepCloneOptions<Element>)
+      : val;
+    const copy = new KeyValuePair(keyCopy, valueCopy) as T;
+    visited.set(value, copy);
+    return copy;
+  }
+
   if (value instanceof ObjectSlice) {
-    const items = [...(value as ObjectSlice)].map(invokeClone) as T[];
-    return new ObjectSlice(items) as T;
+    const mapper = (element: T) => cloneDeep(element, passThroughOptions);
+    const items = [...(value as ObjectSlice)].map(mapper) as T[];
+    const copy = new ObjectSlice(items) as T;
+    visited.set(value, copy);
+    return copy;
   }
 
   if (value instanceof ArraySlice) {
-    const items = [...(value as ArraySlice)].map(invokeClone) as T[];
-    return new ArraySlice(items) as T;
+    const mapper = (element: T) => cloneDeep(element, passThroughOptions);
+    const items = [...(value as ArraySlice)].map(mapper) as T[];
+    const copy = new ArraySlice(items) as T;
+    visited.set(value, copy);
+    return copy;
   }
 
-  if (typeof value?.clone === 'function') {
-    return value.clone() as T;
+  if (isElement(value)) {
+    const copy = cloneShallow(value); // eslint-disable-line @typescript-eslint/no-use-before-define
+
+    visited.set(value, copy);
+
+    if (value.content) {
+      if (isElement(value.content)) {
+        copy.content = cloneDeep(
+          value.content,
+          passThroughOptions as DeepCloneOptions<Element>,
+        ) as any;
+      } else if ((value.content as unknown) instanceof KeyValuePair) {
+        copy.content = cloneDeep(
+          value.content as unknown as KeyValuePair,
+          passThroughOptions as DeepCloneOptions<KeyValuePair>,
+        ) as any;
+      } else if (Array.isArray(value.content)) {
+        const mapper = (element: unknown) => cloneDeep(element as T, passThroughOptions);
+        copy.content = value.content.map(mapper);
+      } else {
+        copy.content = value.content;
+      }
+    } else {
+      copy.content = value.content;
+    }
+
+    return copy;
   }
 
   throw new DeepCloneError("Value provided to cloneDeep function couldn't be cloned", {

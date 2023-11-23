c0910d174846157c3c6997296480ccb9f3b263c2
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-11-23 10:00:09 +0100

    test(reference): add tests for OpenAPI 2.0 JSONReference deref
    Refs #3102

---
+++ packages/apidom-reference/test/dereference/strategies/openapi-2/json-reference-object/dereference-apidom.ts
@@ -0,0 +1,256 @@
+import path from 'node:path';
+import { assert } from 'chai';
+import { mediaTypes, isSchemaElement, SwaggerElement } from '@swagger-api/apidom-ns-openapi-2';
+import { toValue } from '@swagger-api/apidom-core';
+import { evaluate } from '@swagger-api/apidom-json-pointer';
+
+import { parse, dereferenceApiDOM } from '../../../../../src';
+import { ServerTerminable, createHTTPServer } from '../../../../helpers';
+
+describe('dereference', function () {
+  context('strategies', function () {
+    context('openapi-2', function () {
+      context('JSONReference Object', function () {
+        context(
+          'given single JSONReferenceElement passed to dereferenceApiDOM with internal references',
+          function () {
+            context('given dereferencing using local file system', function () {
+              const fixturePath = path.join(__dirname, 'fixtures', 'internal-only', 'root.json');
+
+              specify('should dereference', async function () {
+                const parseResult = await parse(fixturePath, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                });
+                const referenceElement = evaluate(
+                  '/definitions/schema1',
+                  parseResult.api as SwaggerElement,
+                );
+                const dereferenced = await dereferenceApiDOM(referenceElement, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                  resolve: {
+                    baseURI: `${fixturePath}#/definitions/schema1`,
+                  },
+                });
+
+                assert.isTrue(isSchemaElement(dereferenced));
+              });
+
+              specify('should dereference and contain metadata about origin', async function () {
+                const parseResult = await parse(fixturePath, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                });
+                const referenceElement = evaluate(
+                  '/definitions/schema1',
+                  parseResult.api as SwaggerElement,
+                );
+                const dereferenced = await dereferenceApiDOM(referenceElement, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                  resolve: { baseURI: `${fixturePath}#/definitions/schema1` },
+                });
+
+                assert.match(
+                  toValue(dereferenced.meta.get('ref-origin')),
+                  /internal-only\/root\.json$/,
+                );
+              });
+            });
+
+            context('given dereferencing using HTTP protocol', function () {
+              const fixturePath = path.join(__dirname, 'fixtures', 'internal-only', 'root.json');
+              const httpPort = 8123;
+              let httpServer: ServerTerminable;
+
+              beforeEach(function () {
+                const cwd = path.join(__dirname, 'fixtures', 'internal-only');
+                httpServer = createHTTPServer({ port: httpPort, cwd });
+              });
+
+              afterEach(async function () {
+                await httpServer.terminate();
+              });
+
+              specify('should dereference', async function () {
+                const parseResult = await parse(fixturePath, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                });
+                const referenceElement = evaluate(
+                  '/definitions/schema1',
+                  parseResult.api as SwaggerElement,
+                );
+                const dereferenced = await dereferenceApiDOM(referenceElement, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                  resolve: {
+                    baseURI: `http://localhost:${httpPort}/root.json#/definitions/schema1`,
+                  },
+                });
+
+                assert.isTrue(isSchemaElement(dereferenced));
+              });
+
+              specify('should dereference and contain metadata about origin', async function () {
+                const parseResult = await parse(fixturePath, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                });
+                const referenceElement = evaluate(
+                  '/definitions/schema1',
+                  parseResult.api as SwaggerElement,
+                );
+                const dereferenced = await dereferenceApiDOM(referenceElement, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                  resolve: {
+                    baseURI: `http://localhost:${httpPort}/root.json#/definitions/schema1`,
+                  },
+                });
+
+                assert.match(toValue(dereferenced.meta.get('ref-origin')), /\/root\.json$/);
+              });
+            });
+          },
+        );
+
+        context(
+          'given single JSONReferenceElement passed to dereferenceApiDOM with external references',
+          function () {
+            context('given dereferencing using local file system', function () {
+              const fixturePath = path.join(__dirname, 'fixtures', 'external-only', 'root.json');
+
+              specify('should dereference', async function () {
+                const parseResult = await parse(fixturePath, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                });
+                const referenceElement = evaluate(
+                  '/definitions/schema',
+                  parseResult.api as SwaggerElement,
+                );
+                const dereferenced = await dereferenceApiDOM(referenceElement, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                  resolve: { baseURI: fixturePath },
+                });
+
+                assert.isTrue(isSchemaElement(dereferenced));
+              });
+
+              specify('should dereference and contain metadata about origin', async function () {
+                const parseResult = await parse(fixturePath, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                });
+                const referenceElement = evaluate(
+                  '/definitions/schema',
+                  parseResult.api as SwaggerElement,
+                );
+                const dereferenced = await dereferenceApiDOM(referenceElement, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                  resolve: { baseURI: fixturePath },
+                });
+
+                assert.match(
+                  toValue(dereferenced.meta.get('ref-origin')),
+                  /external-only\/ex\.json$/,
+                );
+              });
+            });
+
+            context('given dereferencing using HTTP protocol', function () {
+              const fixturePath = path.join(__dirname, 'fixtures', 'external-only', 'root.json');
+              const httpPort = 8123;
+              let httpServer: ServerTerminable;
+
+              beforeEach(function () {
+                const cwd = path.join(__dirname, 'fixtures', 'external-only');
+                httpServer = createHTTPServer({ port: httpPort, cwd });
+              });
+
+              afterEach(async function () {
+                await httpServer.terminate();
+              });
+
+              specify('should dereference', async function () {
+                const parseResult = await parse(fixturePath, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                });
+                const referenceElement = evaluate(
+                  '/definitions/schema',
+                  parseResult.api as SwaggerElement,
+                );
+                const dereferenced = await dereferenceApiDOM(referenceElement, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                  resolve: { baseURI: `http://localhost:${httpPort}/root.json` },
+                });
+
+                assert.isTrue(isSchemaElement(dereferenced));
+              });
+
+              specify('should dereference and contain metadata about origin', async function () {
+                const parseResult = await parse(fixturePath, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                });
+                const referenceElement = evaluate(
+                  '/definitions/schema',
+                  parseResult.api as SwaggerElement,
+                );
+                const dereferenced = await dereferenceApiDOM(referenceElement, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                  resolve: { baseURI: `http://localhost:${httpPort}/root.json` },
+                });
+
+                assert.match(toValue(dereferenced.meta.get('ref-origin')), /\/ex\.json$/);
+              });
+            });
+
+            context('given dereferencing using HTTP protocol and absolute URLs', function () {
+              const fixturePath = path.join(
+                __dirname,
+                'fixtures',
+                'external-only-absolute-url',
+                'root.json',
+              );
+              const httpPort = 8123;
+              let httpServer: ServerTerminable;
+
+              beforeEach(function () {
+                const cwd = path.join(__dirname, 'fixtures', 'external-only-absolute-url');
+                httpServer = createHTTPServer({ port: httpPort, cwd });
+              });
+
+              afterEach(async function () {
+                await httpServer.terminate();
+              });
+
+              specify('should dereference', async function () {
+                const parseResult = await parse(fixturePath, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                });
+                const referenceElement = evaluate(
+                  '/definitions/schema',
+                  parseResult.api as SwaggerElement,
+                );
+                const dereferenced = await dereferenceApiDOM(referenceElement, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                  resolve: { baseURI: `http://localhost:${httpPort}/root.json` },
+                });
+
+                assert.isTrue(isSchemaElement(dereferenced));
+              });
+
+              specify('should dereference and contain metadata about origin', async function () {
+                const parseResult = await parse(fixturePath, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                });
+                const referenceElement = evaluate(
+                  '/definitions/schema',
+                  parseResult.api as SwaggerElement,
+                );
+                const dereferenced = await dereferenceApiDOM(referenceElement, {
+                  parse: { mediaType: mediaTypes.latest('json') },
+                  resolve: { baseURI: `http://localhost:${httpPort}/root.json` },
+                });
+
+                assert.match(toValue(dereferenced.meta.get('ref-origin')), /\/ex\.json$/);
+              });
+            });
+          },
+        );
+      });
+    });
+  });
+});

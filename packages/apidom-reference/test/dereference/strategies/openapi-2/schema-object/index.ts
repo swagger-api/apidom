d81fb00486f020327831968a31b918b1876e7d11
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-11-23 12:56:11 +0100

    test(reference): add location tests for OpenAPI 2.0 dereference
    Refs #3102

---
+++ packages/apidom-reference/test/dereference/strategies/openapi-2/schema-object/index.ts
@@ -0,0 +1,59 @@
+import path from 'node:path';
+import { assert } from 'chai';
+import { toValue } from '@swagger-api/apidom-core';
+import { mediaTypes } from '@swagger-api/apidom-ns-openapi-2';
+
+import { loadJsonFile } from '../../../../helpers';
+import { dereference } from '../../../../../src';
+
+const rootFixturePath = path.join(__dirname, 'fixtures');
+
+describe('dereference', function () {
+  context('strategies', function () {
+    context('openapi-2', function () {
+      context('Schema Object', function () {
+        context('given in definitions field', function () {
+          const fixturePath = path.join(rootFixturePath, 'definitions');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+
+        context('given in Parameter Object', function () {
+          const fixturePath = path.join(rootFixturePath, 'parameter-object');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+
+        context('given in Response Object', function () {
+          const fixturePath = path.join(rootFixturePath, 'response-object');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+      });
+    });
+  });
+});

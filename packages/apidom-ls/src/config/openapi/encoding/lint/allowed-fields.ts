6110a330baac7470f8721b1afb0a4bfc744a01b3
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-12-04 16:00:27 +0100

    Update allowed-fields.ts

--- packages/apidom-ls/src/config/openapi/encoding/lint/allowed-fields.ts
+++ packages/apidom-ls/src/config/openapi/encoding/lint/allowed-fields.ts
@@ -2,7 +2,7 @@ import { DiagnosticSeverity } from 'vscode-languageserver-types';
 
 import ApilintCodes from '../../../codes';
 import { LinterMeta } from '../../../../apidom-language-types';
-import { OpenAPI30 } from '../../target-specs';
+import { OpenAPI3 } from '../../target-specs';
 
 const allowedFieldsLint: LinterMeta = {
   code: ApilintCodes.NOT_ALLOWED_FIELDS,
@@ -12,7 +12,7 @@ const allowedFieldsLint: LinterMeta = {
   linterFunction: 'allowedFields',
   linterParams: [['contentType', 'headers', 'style', 'explode', 'allowReserved'], 'x-'],
   marker: 'key',
-  targetSpecs: OpenAPI30,
+  targetSpecs: OpenAPI3,
 };
 
 export default allowedFieldsLint;

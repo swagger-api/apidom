c89dab3b5479f75245a69a2abdd2008a904e5c55
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-12-05 13:45:40 +0100

    feat(ls): add missing OpenAPI 2.0 symbols (#3507)
    Refs #3479

---
+++ packages/apidom-ls/src/config/ads/symbols.ts
@@ -0,0 +1,12 @@
+const symbols = [
+  'main',
+  'info',
+  'principle',
+  'standard',
+  'standardIdentifier',
+  'requirement',
+  'requirementLevel',
+  'scenario',
+];
+
+export default symbols;

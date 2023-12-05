cfc50a5ab987c1fbf88146e27389eaf0e1eafd2b
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-12-05 13:32:58 +0100

    Update packages/apidom-ls/src/config/symbols.ts

--- packages/apidom-ls/src/config/symbols.ts
+++ packages/apidom-ls/src/config/symbols.ts
@@ -3,6 +3,6 @@ import asyncapiSymbols from './asyncapi/symbols';
 import openapiSymbols from './openapi/symbols';
 
 // creating list of unique symbols
-const symbols = Array.from(new Set([...adsSymbols, ...asyncapiSymbols, openapiSymbols]));
+const symbols = Array.from(new Set([...adsSymbols, ...asyncapiSymbols, ...openapiSymbols]));
 
 export default symbols;

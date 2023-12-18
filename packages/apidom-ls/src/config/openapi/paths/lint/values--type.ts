8feeeb3a1ad44bc6529b28b00a635563dd9dfc7f
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-12-18 17:38:40 +0100

    feat(ls): add rules for OpenAPI 2.0 Paths Object (#3553)
    Refs #3104

--- packages/apidom-ls/src/config/openapi/paths/lint/values--type.ts
+++ packages/apidom-ls/src/config/openapi/paths/lint/values--type.ts
@@ -2,10 +2,10 @@ import { DiagnosticSeverity } from 'vscode-languageserver-types';
 
 import ApilintCodes from '../../../codes';
 import { LinterMeta } from '../../../../apidom-language-types';
-import { OpenAPI3 } from '../../target-specs';
+import { OpenAPI2, OpenAPI3 } from '../../target-specs';
 
 const valuesTypeLint: LinterMeta = {
-  code: ApilintCodes.OPENAPI3_0_PATHS_VALUES_TYPE,
+  code: ApilintCodes.OPENAPI2_PATHS_VALUES_TYPE,
   source: 'apilint',
   message: 'Paths Object values must be of Path Item Object shape',
   severity: DiagnosticSeverity.Error,
@@ -13,7 +13,7 @@ const valuesTypeLint: LinterMeta = {
   linterParams: [['pathItem']],
   marker: 'key',
   data: {},
-  targetSpecs: OpenAPI3,
+  targetSpecs: [...OpenAPI2, ...OpenAPI3],
 };
 
 export default valuesTypeLint;

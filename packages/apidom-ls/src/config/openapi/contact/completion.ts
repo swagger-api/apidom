075079d0a3fedf2fa4b07d5f8a57f271be52cd65
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-12-18 16:49:37 +0100

    feat(ls): add rules for OpenAPI 2.0 Contact Object
    Refs #3104

--- packages/apidom-ls/src/config/openapi/contact/completion.ts
+++ packages/apidom-ls/src/config/openapi/contact/completion.ts
@@ -3,7 +3,7 @@ import {
   CompletionFormat,
   CompletionType,
 } from '../../../apidom-language-types';
-import { OpenAPI3 } from '../target-specs';
+import { OpenAPI, OpenAPI2, OpenAPI3 } from '../target-specs';
 
 const completion: ApidomCompletionItem[] = [
   {
@@ -17,7 +17,20 @@ const completion: ApidomCompletionItem[] = [
       kind: 'markdown',
       value: 'The identifying name of the contact person/organization.',
     },
-    targetSpecs: OpenAPI3,
+    targetSpecs: OpenAPI,
+  },
+  {
+    label: 'url',
+    insertText: 'url',
+    kind: 14,
+    format: CompletionFormat.QUOTED,
+    type: CompletionType.PROPERTY,
+    insertTextFormat: 2,
+    documentation: {
+      kind: 'markdown',
+      value: 'The URL pointing to the contact information. This MUST be in the format of a URL.',
+    },
+    targetSpecs: OpenAPI2,
   },
   {
     label: 'url',
@@ -31,6 +44,20 @@ const completion: ApidomCompletionItem[] = [
       value: 'The URL pointing to the contact information. This MUST be in the form of a URL.',
     },
     targetSpecs: OpenAPI3,
+  },
+  {
+    label: 'email',
+    insertText: 'email',
+    kind: 14,
+    format: CompletionFormat.QUOTED,
+    type: CompletionType.PROPERTY,
+    insertTextFormat: 2,
+    documentation: {
+      kind: 'markdown',
+      value:
+        'The email address of the contact person/organization. This MUST be in the format of an email address.',
+    },
+    targetSpecs: OpenAPI2,
   },
   {
     label: 'email',
--- packages/apidom-ls/src/config/openapi/contact/lint/url--format-uri.ts
+++ packages/apidom-ls/src/config/openapi/contact/lint/url--format-uri.ts
@@ -2,10 +2,10 @@ import { DiagnosticSeverity } from 'vscode-languageserver-types';
 
 import ApilintCodes from '../../../codes';
 import { LinterMeta } from '../../../../apidom-language-types';
-import { OpenAPI3 } from '../../target-specs';
+import { OpenAPI } from '../../target-specs';
 
 const urlFormatURILint: LinterMeta = {
-  code: ApilintCodes.OPENAPI3_0_CONTACT_FIELD_URL_FORMAT_URI,
+  code: ApilintCodes.OPENAPI2_CONTACT_FIELD_URL_FORMAT_URI,
   source: 'apilint',
   message: "'url' value must be a valid URL",
   severity: DiagnosticSeverity.Error,
@@ -13,7 +13,7 @@ const urlFormatURILint: LinterMeta = {
   marker: 'value',
   target: 'url',
   data: {},
-  targetSpecs: OpenAPI3,
+  targetSpecs: OpenAPI,
 };
 
 export default urlFormatURILint;

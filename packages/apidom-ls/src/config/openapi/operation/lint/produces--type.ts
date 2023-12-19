f9493492c43a0b30976bcd4d377ac203ae301cb1
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-12-19 11:26:05 +0100

    feat(ls): add rules for OpenAPI 2.0 Operation Object
    Refs #3104

--- packages/apidom-ls/src/config/openapi/operation/lint/produces--type.ts
+++ packages/apidom-ls/src/config/openapi/operation/lint/produces--type.ts
@@ -2,19 +2,19 @@ import { DiagnosticSeverity } from 'vscode-languageserver-types';
 
 import ApilintCodes from '../../../codes';
 import { LinterMeta } from '../../../../apidom-language-types';
-import { OpenAPI2, OpenAPI3 } from '../../target-specs';
+import { OpenAPI2 } from '../../target-specs';
 
-const tagsTypeLint: LinterMeta = {
-  code: ApilintCodes.OPENAPI2_OPERATION_FIELD_TAGS_TYPE,
+const producesTypeLint: LinterMeta = {
+  code: ApilintCodes.OPENAPI2_OPERATION_FIELD_PRODUCES_TYPE,
   source: 'apilint',
-  message: "'tags' must be an array of strings",
+  message: "'produces' must be an array of strings",
   severity: DiagnosticSeverity.Error,
   linterFunction: 'apilintArrayOfType',
   linterParams: ['string'],
   marker: 'key',
-  target: 'tags',
+  target: 'produces',
   data: {},
-  targetSpecs: [...OpenAPI2, ...OpenAPI3],
+  targetSpecs: OpenAPI2,
 };
 
-export default tagsTypeLint;
+export default producesTypeLint;
--- packages/apidom-ls/src/config/openapi/operation/lint/summary--type.ts
+++ packages/apidom-ls/src/config/openapi/operation/lint/summary--type.ts
@@ -2,10 +2,10 @@ import { DiagnosticSeverity } from 'vscode-languageserver-types';
 
 import ApilintCodes from '../../../codes';
 import { LinterMeta } from '../../../../apidom-language-types';
-import { OpenAPI3 } from '../../target-specs';
+import { OpenAPI2, OpenAPI3 } from '../../target-specs';
 
 const summaryTypeLint: LinterMeta = {
-  code: ApilintCodes.OPENAPI3_0_OPERATION_FIELD_SUMMARY_TYPE,
+  code: ApilintCodes.OPENAPI2_OPERATION_FIELD_SUMMARY_TYPE,
   source: 'apilint',
   message: 'summary must be a string',
   severity: DiagnosticSeverity.Error,
@@ -14,7 +14,7 @@ const summaryTypeLint: LinterMeta = {
   marker: 'value',
   target: 'summary',
   data: {},
-  targetSpecs: OpenAPI3,
+  targetSpecs: [...OpenAPI2, ...OpenAPI3],
 };
 
 export default summaryTypeLint;

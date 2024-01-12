9c1e8124533a9b5b24bf9fd0142f8a7be43c44e2
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-01-12 10:09:35 +0100

    feat(ls): add rules for OpenAPI 2.0 Responses Object
    Refs #3606

--- packages/apidom-ls/src/config/openapi/responses/lint/allowed-fields-2-0.ts
+++ packages/apidom-ls/src/config/openapi/responses/lint/allowed-fields-2-0.ts
@@ -3,7 +3,7 @@ import { DiagnosticSeverity } from 'vscode-languageserver-types';
 
 import ApilintCodes from '../../../codes';
 import { LinterMeta } from '../../../../apidom-language-types';
-import { OpenAPI3 } from '../../target-specs';
+import { OpenAPI2 } from '../../target-specs';
 
 /**
  * Validation here is based on IANA HTTP Status code registry: https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
@@ -11,19 +11,17 @@ import { OpenAPI3 } from '../../target-specs';
  * Clarification of OpenAPI specification what are accepted HTTP Status code comes from https://github.com/OAI/OpenAPI-Specification/issues/2471.
  */
 
-const allowedFieldsLint: LinterMeta = {
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const allowedFields2_0Lint: LinterMeta = {
   code: ApilintCodes.NOT_ALLOWED_FIELDS,
   source: 'apilint',
   message:
     'Responses Object uses HTTP Status Codes outside of allowed IANA HTTP Status code registry',
   severity: DiagnosticSeverity.Error,
   linterFunction: 'allowedFields',
-  linterParams: [
-    ['default', '1XX', '2XX', '3XX', '4XX', '5XX', ...range(100, 600).map(String)],
-    'x-',
-  ],
+  linterParams: [['default', ...range(100, 600).map(String)], 'x-'],
   marker: 'key',
-  targetSpecs: OpenAPI3,
+  targetSpecs: OpenAPI2,
 };
 
-export default allowedFieldsLint;
+export default allowedFields2_0Lint;

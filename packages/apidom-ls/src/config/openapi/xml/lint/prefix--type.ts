6629b8ab7158ccdf3b2a6f08070d12e0a8d38169
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-01-12 10:48:05 +0100

    feat(ls): add rules for OpenAPI 2.0 XML Object
    Refs #3614

--- packages/apidom-ls/src/config/openapi/xml/lint/prefix--type.ts
+++ packages/apidom-ls/src/config/openapi/xml/lint/prefix--type.ts
@@ -2,10 +2,10 @@ import { DiagnosticSeverity } from 'vscode-languageserver-types';
 
 import ApilintCodes from '../../../codes';
 import { LinterMeta } from '../../../../apidom-language-types';
-import { OpenAPI3 } from '../../target-specs';
+import { OpenAPI2, OpenAPI3 } from '../../target-specs';
 
 const prefixTypeLint: LinterMeta = {
-  code: ApilintCodes.OPENAPI3_0_XML_FIELD_PREFIX_TYPE,
+  code: ApilintCodes.OPENAPI2_XML_FIELD_PREFIX_TYPE,
   source: 'apilint',
   message: 'prefix must be a string',
   severity: DiagnosticSeverity.Error,
@@ -14,7 +14,7 @@ const prefixTypeLint: LinterMeta = {
   marker: 'value',
   target: 'prefix',
   data: {},
-  targetSpecs: OpenAPI3,
+  targetSpecs: [...OpenAPI2, ...OpenAPI3],
 };
 
 export default prefixTypeLint;

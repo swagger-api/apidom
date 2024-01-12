1f539a3855d545bf6ee5d91f95ce72fa087494e0
Vladimír Gorej <vladimir.gorej@gmail.com>
2024-01-12 10:55:57 +0100

    feat(ls): add rules for OpenAPI 2.0 XML Object (#3666)
    Refs #3614

--- packages/apidom-ls/src/config/openapi/xml/completion.ts
+++ packages/apidom-ls/src/config/openapi/xml/completion.ts
@@ -3,9 +3,23 @@ import {
   CompletionFormat,
   CompletionType,
 } from '../../../apidom-language-types';
-import { OpenAPI30, OpenAPI31, OpenAPI3 } from '../target-specs';
+import { OpenAPI2, OpenAPI30, OpenAPI31, OpenAPI3 } from '../target-specs';
 
 const completion: ApidomCompletionItem[] = [
+  {
+    label: 'name',
+    insertText: 'name',
+    kind: 14,
+    format: CompletionFormat.QUOTED,
+    type: CompletionType.PROPERTY,
+    insertTextFormat: 2,
+    documentation: {
+      kind: 'markdown',
+      value:
+        'Replaces the name of the element/attribute used for the described schema property. When defined within the Items Object (`items`), it will affect the name of the individual XML elements within the list. When defined alongside `type` being `array` (outside the `items`), it will affect the wrapping element and only if `wrapped` is `true`. If `wrapped` is `false`, it will be ignored.',
+    },
+    targetSpecs: OpenAPI2,
+  },
   {
     label: 'name',
     insertText: 'name',
@@ -29,9 +43,36 @@ const completion: ApidomCompletionItem[] = [
     insertTextFormat: 2,
     documentation: {
       kind: 'markdown',
+      value: 'The URL of the namespace definition. Value SHOULD be in the form of a URL.',
+    },
+    targetSpecs: OpenAPI2,
+  },
+  {
+    label: 'namespace',
+    insertText: 'namespace',
+    kind: 14,
+    format: CompletionFormat.QUOTED,
+    type: CompletionType.PROPERTY,
+    insertTextFormat: 2,
+    documentation: {
+      kind: 'markdown',
       value: 'The URI of the namespace definition. This MUST be in the form of an absolute URI.',
     },
     targetSpecs: OpenAPI3,
+  },
+  {
+    label: 'prefix',
+    insertText: 'prefix ',
+    kind: 14,
+    format: CompletionFormat.QUOTED,
+    type: CompletionType.PROPERTY,
+    insertTextFormat: 2,
+    documentation: {
+      kind: 'markdown',
+      value:
+        'The prefix to be used for the [name](https://github.com/OAI/OpenAPI-Specification/blob/main/versions/2.0.md#xmlName).',
+    },
+    targetSpecs: OpenAPI2,
   },
   {
     label: 'prefix',
@@ -73,7 +114,7 @@ const completion: ApidomCompletionItem[] = [
       value:
         'Declares whether the property definition translates to an attribute instead of an element. Default value is `false`.',
     },
-    targetSpecs: OpenAPI3,
+    targetSpecs: [...OpenAPI2, ...OpenAPI3],
   },
   {
     label: 'wrapped',
@@ -87,7 +128,7 @@ const completion: ApidomCompletionItem[] = [
       value:
         'MAY be used only for an array definition. Signifies whether the array is wrapped (for example, `<books><book/><book/></books>`) or unwrapped (`<book/><book/>`). Default value is `false`. The definition takes effect only when defined alongside `type` being `array` (outside the `items`).',
     },
-    targetSpecs: OpenAPI3,
+    targetSpecs: [...OpenAPI2, ...OpenAPI3],
   },
 ];
 
--- packages/apidom-ls/src/config/openapi/xml/lint/namespace--format-uri.ts
+++ packages/apidom-ls/src/config/openapi/xml/lint/namespace--format-uri.ts
@@ -2,10 +2,10 @@ import { DiagnosticSeverity } from 'vscode-languageserver-types';
 
 import ApilintCodes from '../../../codes';
 import { LinterMeta } from '../../../../apidom-language-types';
-import { OpenAPI3 } from '../../target-specs';
+import { OpenAPI2, OpenAPI3 } from '../../target-specs';
 
 const namespaceFormatURILint: LinterMeta = {
-  code: ApilintCodes.OPENAPI3_0_XML_FIELD_NAMESPACE_FORMAT_URI,
+  code: ApilintCodes.OPENAPI2_XML_FIELD_NAMESPACE_FORMAT_URI,
   source: 'apilint',
   message: 'namespace MUST be in the format of an absolute URL.',
   severity: DiagnosticSeverity.Error,
@@ -14,7 +14,7 @@ const namespaceFormatURILint: LinterMeta = {
   marker: 'value',
   target: 'namespace',
   data: {},
-  targetSpecs: OpenAPI3,
+  targetSpecs: [...OpenAPI2, ...OpenAPI3],
 };
 
 export default namespaceFormatURILint;

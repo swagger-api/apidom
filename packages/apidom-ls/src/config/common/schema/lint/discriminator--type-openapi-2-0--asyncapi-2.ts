f3c799f706a47baaea36820259e8f595a04afbb9
Vladimír Gorej <vladimir.gorej@gmail.com>
2024-01-22 14:57:46 +0100

    feat(ls): add allowed fields lint rule for OpenAPI 2.0 Schema Object (#3709)
    Refs #3613

--- packages/apidom-ls/src/config/common/schema/lint/discriminator--type-openapi-2-0--asyncapi-2.ts
+++ packages/apidom-ls/src/config/common/schema/lint/discriminator--type-openapi-2-0--asyncapi-2.ts
@@ -6,7 +6,7 @@ import { AsyncAPI2 } from '../../../asyncapi/target-specs';
 import { OpenAPI2 } from '../../../openapi/target-specs';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-const discriminatorTypeOpenAPI2__AsyncAPI2Lint: LinterMeta = {
+const discriminatorTypeOpenAPI2_0__AsyncAPI2Lint: LinterMeta = {
   code: ApilintCodes.SCHEMA_DISCRIMINATOR,
   source: 'apilint',
   message: "'discriminator' value must be a string",
@@ -19,4 +19,4 @@ const discriminatorTypeOpenAPI2__AsyncAPI2Lint: LinterMeta = {
   targetSpecs: [...AsyncAPI2, ...OpenAPI2],
 };
 
-export default discriminatorTypeOpenAPI2__AsyncAPI2Lint;
+export default discriminatorTypeOpenAPI2_0__AsyncAPI2Lint;

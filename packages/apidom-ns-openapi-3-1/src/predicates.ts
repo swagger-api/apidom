bf6702a1b5272f94d4ac5e5c6fbdc4f4b2de0620
Vladimír Gorej <vladimir.gorej@gmail.com>
2024-02-12 09:23:46 +0100

    feat(ns-openapi-3-1): add Security Schema Object predicate (#3811)

--- packages/apidom-ns-openapi-3-1/src/predicates.ts
+++ packages/apidom-ns-openapi-3-1/src/predicates.ts
@@ -29,6 +29,7 @@ import ResponseElement from './elements/Response';
 import ResponsesElement from './elements/Responses';
 import SchemaElement from './elements/Schema';
 import SecurityRequirementElement from './elements/SecurityRequirement';
+import SecuritySchemeElement from './elements/SecurityScheme';
 import ServerElement from './elements/Server';
 import ServerVariableElement from './elements/ServerVariable';
 import MediaTypeElement from './elements/MediaType';
@@ -299,6 +300,16 @@ export const isSecurityRequirementElement = createPredicate(
       element instanceof SecurityRequirementElement ||
       (hasBasicElementProps(element) &&
         isElementType('securityRequirement', element) &&
+        primitiveEq('object', element));
+  },
+);
+
+export const isSecuritySchemeElement = createPredicate(
+  ({ hasBasicElementProps, isElementType, primitiveEq }) => {
+    return (element: unknown): element is SecuritySchemeElement =>
+      element instanceof SecuritySchemeElement ||
+      (hasBasicElementProps(element) &&
+        isElementType('securityScheme', element) &&
         primitiveEq('object', element));
   },
 );

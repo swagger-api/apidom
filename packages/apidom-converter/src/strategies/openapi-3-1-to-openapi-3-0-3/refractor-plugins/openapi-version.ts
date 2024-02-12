e3ecb821914ebd79017a29af2f2d63f27a6775b4
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-02-12 10:07:28 +0100

    refactor(converter): allow plugins to perform mutable operations
    This change will make plugins creation much simpler.
    ApiDOM is deep cloned before the plugins touch it.
    That way from outside, the converter still remains
    immutable, but the plugins work in mutable way.

--- packages/apidom-converter/src/strategies/openapi-3-1-to-openapi-3-0-3/refractor-plugins/openapi-version.ts
+++ packages/apidom-converter/src/strategies/openapi-3-1-to-openapi-3-0-3/refractor-plugins/openapi-version.ts
@@ -1,9 +1,9 @@
-import { OpenapiElement as Openapi30Element } from '@swagger-api/apidom-ns-openapi-3-0';
+import { OpenapiElement } from '@swagger-api/apidom-ns-openapi-3-1';
 
 const openAPIVersionRefractorPlugin = () => () => ({
   visitor: {
-    OpenapiElement() {
-      return new Openapi30Element('3.0.3');
+    OpenapiElement(element: OpenapiElement) {
+      (element.content as unknown as string) = '3.0.3';
     },
   },
 });

8938a8e2ac4caca9d80143daeb5717851ce678bd
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-02-12 12:04:31 +0100

    feat(ns-openapi-3-0): include hasElementSourceMap in toolbox

--- packages/apidom-ns-openapi-3-0/src/refractor/toolbox.ts
+++ packages/apidom-ns-openapi-3-0/src/refractor/toolbox.ts
@@ -1,12 +1,39 @@
-import { createNamespace, isStringElement } from '@swagger-api/apidom-core';
+import {
+  isElement,
+  isStringElement,
+  isArrayElement,
+  isObjectElement,
+  isMemberElement,
+  createNamespace,
+  includesClasses,
+  hasElementSourceMap,
+} from '@swagger-api/apidom-core';
 
 import * as openApi3_0Predicates from '../predicates';
-import * as refractorPredicates from './predicates';
 import openApi3_0Namespace from '../namespace';
+
+export type Predicates = typeof openApi3_0Predicates & {
+  isElement: typeof isElement;
+  isStringElement: typeof isStringElement;
+  isArrayElement: typeof isArrayElement;
+  isObjectElement: typeof isObjectElement;
+  isMemberElement: typeof isMemberElement;
+  includesClasses: typeof includesClasses;
+  hasElementSourceMap: typeof hasElementSourceMap;
+};
 
 const createToolbox = () => {
   const namespace = createNamespace(openApi3_0Namespace);
-  const predicates = { ...refractorPredicates, ...openApi3_0Predicates, isStringElement };
+  const predicates: Predicates = {
+    ...openApi3_0Predicates,
+    isElement,
+    isStringElement,
+    isArrayElement,
+    isObjectElement,
+    isMemberElement,
+    includesClasses,
+    hasElementSourceMap,
+  };
 
   return { predicates, namespace };
 };

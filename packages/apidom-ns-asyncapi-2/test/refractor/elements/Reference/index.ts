e3b1848afe1c6c34568e692731e0a9633ba53368
Vladimír Gorej <vladimir.gorej@gmail.com>
2024-02-24 14:49:36 +0100

    fix(ns-asyncapi-2): retain meta & attributes during refracting (#3858)
    This change is specific to cases when semantic ApiDOM is refractored
    from generic ApiDOM.
    
    Refs #3842

--- packages/apidom-ns-asyncapi-2/test/refractor/elements/Reference/index.ts
+++ packages/apidom-ns-asyncapi-2/test/refractor/elements/Reference/index.ts
@@ -1,5 +1,5 @@
-import { expect } from 'chai';
-import { sexprs } from '@swagger-api/apidom-core';
+import { assert, expect } from 'chai';
+import { ObjectElement, sexprs, toValue } from '@swagger-api/apidom-core';
 
 import { ReferenceElement } from '../../../../src';
 
@@ -12,6 +12,34 @@ describe('refractor', function () {
         });
 
         expect(sexprs(referenceElement)).toMatchSnapshot();
+      });
+
+      context('given generic ApiDOM element', function () {
+        let referenceElement: ReferenceElement;
+
+        beforeEach(function () {
+          referenceElement = ReferenceElement.refract(
+            new ObjectElement(
+              { $ref: '#/path/to/somewhere' },
+              { classes: ['example'] },
+              { attr: true },
+            ),
+          ) as ReferenceElement;
+        });
+
+        specify('should refract to semantic ApiDOM tree', function () {
+          expect(sexprs(referenceElement)).toMatchSnapshot();
+        });
+
+        specify('should deepmerge meta', function () {
+          assert.deepEqual(toValue(referenceElement.meta), {
+            classes: ['json-reference', 'asyncapi-reference', 'example', 'reference-element'],
+          });
+        });
+
+        specify('should deepmerge attributes', function () {
+          assert.isTrue(referenceElement.attributes.get('attr').equals(true));
+        });
       });
     });
   });

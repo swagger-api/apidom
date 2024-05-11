cca4624806c15e4268d3b24f4694e69504b7c32f
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-05-11 20:33:51 +0200

    fix: types

--- packages/apidom-reference/src/Reference.ts
+++ packages/apidom-reference/src/Reference.ts
@@ -1,26 +1,26 @@
-import { ParseResultElement } from '@swagger-api/apidom-core';
+import { Element } from '@swagger-api/apidom-core';
 
 import { ReferenceSet } from './types';
 
-export interface ReferenceOptions {
+export interface ReferenceOptions<T = Element> {
   readonly uri: string;
   readonly depth?: number;
   readonly refSet?: ReferenceSet;
-  readonly value: ParseResultElement;
+  readonly value: T;
 }
 
-class Reference {
+class Reference<T = Element> {
   public readonly uri: string;
 
   public readonly depth: number;
 
-  public readonly value: ParseResultElement;
+  public readonly value: T;
 
   public refSet?: ReferenceSet;
 
   public readonly errors: Array<Error>;
 
-  constructor({ uri, depth = 0, refSet, value }: ReferenceOptions) {
+  constructor({ uri, depth = 0, refSet, value }: ReferenceOptions<T>) {
     this.uri = uri;
     this.value = value;
     this.depth = depth;

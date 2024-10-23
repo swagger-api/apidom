c2a0455a02eaabff521df1e4008ec061e251a51f
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-10-23 22:27:11 +0200

    test(apidom-parser-adapter-asyncapi-yaml-2): run tests in ESM

--- packages/apidom-parser-adapter-asyncapi-yaml-2/test/perf/lexical-analysis.ts
+++ packages/apidom-parser-adapter-asyncapi-yaml-2/test/perf/lexical-analysis.ts
@@ -1,10 +1,11 @@
-require('@babel/register')({ extensions: ['.js', '.ts'], rootMode: 'upward' });
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import Benchmark from 'benchmark';
+import type { Deferred, Event } from 'benchmark';
+import { lexicalAnalysis } from '@swagger-api/apidom-parser-adapter-yaml-1-2';
 
-const fs = require('node:fs');
-const path = require('node:path');
-const Benchmark = require('benchmark');
-const { lexicalAnalysis } = require('@swagger-api/apidom-parser-adapter-yaml-1-2');
-
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const fixturePath = path.join(__dirname, 'fixtures/asyncapi.yaml');
 const source = fs.readFileSync(fixturePath).toString();
 
@@ -13,22 +14,22 @@ const options = {
   defer: true,
   minSamples: 1400,
   expected: '1,117 ops/sec ±2.33% (1473 runs sampled)',
-  async fn(deferred) {
+  async fn(deferred: Deferred) {
     await lexicalAnalysis(source);
     deferred.resolve();
   },
 };
 
-module.exports = options;
+export default options;
 
 // we're running as a script
-if (module.parent === null) {
+if (import.meta.url === `file://${process.argv[1]}`) {
   const bench = new Benchmark({
     ...options,
-    onComplete(event) {
+    onComplete(event: Event) {
       console.info(String(event.target));
     },
-    onError(event) {
+    onError(event: Event) {
       console.error(event);
     },
   });

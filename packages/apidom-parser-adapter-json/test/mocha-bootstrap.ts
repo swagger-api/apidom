b80b33416826f53a76af9d9dba6f92cf452f79d2
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-11-07 14:08:15 +0000

    build(parser-adapter-json): use nodenext for TypeScript modules
    Refs #4385

--- packages/apidom-parser-adapter-json/test/mocha-bootstrap.ts
+++ packages/apidom-parser-adapter-json/test/mocha-bootstrap.ts
@@ -3,9 +3,9 @@ import { jestSnapshotPlugin, addSerializer } from 'mocha-chai-jest-snapshot';
 import jsdomGlobal from 'jsdom-global';
 
 // @ts-ignore
-import * as jestApiDOMSerializer from '../../../scripts/jest-serializer-apidom';
+import * as jestApiDOMSerializer from '../../../scripts/jest-serializer-apidom.mjs';
 // @ts-ignore
-import * as jestStringSerializer from '../../../scripts/jest-serializer-string';
+import * as jestStringSerializer from '../../../scripts/jest-serializer-string.mjs';
 
 /**
  * Configure snapshot testing.

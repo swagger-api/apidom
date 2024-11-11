17b5fc298568edaa6185a28302da86c65408fb8d
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-11-11 12:58:02 +0100

    build(parser-adapter-openapi-json-2): use nodenext for TypeScript modules (#4463)
    Refs #4385

--- packages/apidom-parser-adapter-openapi-json-2/test/media-types.ts
+++ packages/apidom-parser-adapter-openapi-json-2/test/media-types.ts
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 import ApiDOMParser from '@swagger-api/apidom-parser';
 
-import * as openApiJsonAdapter from '../src/adapter';
+import * as openApiJsonAdapter from '../src/adapter.ts';
 
 describe('given adapter is used in parser', function () {
   const parser = new ApiDOMParser().use(openApiJsonAdapter);

b2da8e0f1feeafdbbeaaa152cbc9960baa8a101c
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-11-13 11:20:18 +0100

    build(parser-adapter-asyncapi-json-2): use nodenext for TypeScript modules (#4487)
    Refs #4385

--- packages/apidom-parser-adapter-asyncapi-json-2/test/adapter.ts
+++ packages/apidom-parser-adapter-asyncapi-json-2/test/adapter.ts
@@ -5,7 +5,7 @@ import { assert, expect } from 'chai';
 import { isParseResultElement, sexprs } from '@swagger-api/apidom-core';
 import { isAsyncApi2Element } from '@swagger-api/apidom-ns-asyncapi-2';
 
-import * as adapter from '../src/adapter';
+import * as adapter from '../src/adapter.ts';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const jsonSpec = fs.readFileSync(path.join(__dirname, 'fixtures', 'sample-api.json')).toString();

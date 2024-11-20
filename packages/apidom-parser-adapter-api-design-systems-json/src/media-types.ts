34015e1a339864e7ac810d758038aefcb37768c8
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-11-20 12:58:24 +0100

    chore(parser-adapter-api-design-systems-json): use API Extractor for TypeScript rollup
    Refs #4382

--- packages/apidom-parser-adapter-api-design-systems-json/src/media-types.ts
+++ packages/apidom-parser-adapter-api-design-systems-json/src/media-types.ts
@@ -1,5 +1,8 @@
 import { mediaTypes, ApiDesignSystemsMediaTypes } from '@swagger-api/apidom-ns-api-design-systems';
 
+/**
+ * @public
+ */
 const jsonMediaTypes = new ApiDesignSystemsMediaTypes(
   ...mediaTypes.filterByFormat('generic'),
   ...mediaTypes.filterByFormat('json'),

42274d6620e7ccd40e1d4e2822c0c965777c42c1
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-11-20 09:48:53 +0100

    chore(json-path): use API Extractor for TypeScript rollup
    Refs #4382

--- packages/apidom-json-path/src/index.ts
+++ packages/apidom-json-path/src/index.ts
@@ -4,5 +4,8 @@ export { default as EvaluationJsonPathError } from './errors/EvaluationJsonPathE
 export type { EvaluationJsonPathErrorOptions } from './errors/EvaluationJsonPathError.ts';
 export { default as MultiEvaluationJsonPathError } from './errors/MultiEvaluationJsonPathError.ts';
 export type { MultiEvaluationJsonPathErrorOptions } from './errors/MultiEvaluationJsonPathError.ts';
+export type { default as JsonPathError } from './errors/JsonPathError.ts';
 export { default as evaluate } from './evaluate.ts';
+export type { Evaluate } from './evaluate.ts';
 export { default as evaluateMulti } from './evaluate-multi.ts';
+export type { EvaluateMulti, JSONPathEvalTuple } from './evaluate-multi.ts';

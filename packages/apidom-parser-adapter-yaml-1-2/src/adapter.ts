cd7ce0c93d4b96bced79652019d643acd3651fbf
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-11-20 09:08:51 +0100

    chore(parser-adapter-yaml-1-2): use API Extractor for TypeScript rollup
    Refs #4382

--- packages/apidom-parser-adapter-yaml-1-2/src/adapter.ts
+++ packages/apidom-parser-adapter-yaml-1-2/src/adapter.ts
@@ -1,5 +1,9 @@
 import { createNamespace } from '@swagger-api/apidom-core';
 
 export { default as mediaTypes } from './media-types.ts';
+export type { YamlMediaTypes } from './media-types.ts';
 
+/**
+ * @public
+ */
 export const namespace = createNamespace();
--- packages/apidom-parser-adapter-yaml-1-2/src/media-types.ts
+++ packages/apidom-parser-adapter-yaml-1-2/src/media-types.ts
@@ -1,11 +1,17 @@
 import { MediaTypes } from '@swagger-api/apidom-core';
 
+/**
+ * @public
+ */
 export class YamlMediaTypes extends MediaTypes<string> {
   latest() {
     return this[1];
   }
 }
 
+/**
+ * @public
+ */
 const mediaTypes = new YamlMediaTypes('text/yaml', 'application/yaml');
 
 export default mediaTypes;

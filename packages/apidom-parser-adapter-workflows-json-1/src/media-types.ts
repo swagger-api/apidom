2a4a6d0b828521215e5a16182c2a1e14d88894c4
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-11-21 10:59:21 +0100

    chore(parser-adapter-workflows-json-1): use API Extractor for TypeScript rollup (#4528)
    Refs #4382

--- packages/apidom-parser-adapter-workflows-json-1/src/media-types.ts
+++ packages/apidom-parser-adapter-workflows-json-1/src/media-types.ts
@@ -1,5 +1,8 @@
 import { mediaTypes, WorkflowsMediaTypes } from '@swagger-api/apidom-ns-workflows-1';
 
+/**
+ * @public
+ */
 const jsonMediaTypes = new WorkflowsMediaTypes(
   ...mediaTypes.filterByFormat('generic'),
   ...mediaTypes.filterByFormat('json'),

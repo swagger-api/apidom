e6d773fe949bfc3ffadd148247aff4a02a2fd12a
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-12-02 10:06:58 +0100

    fix: add explicit base

--- packages/apidom-playground/vite.config.mjs
+++ packages/apidom-playground/vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: 'https://swagger-api.github.io/apidom/',
   plugins: [react()],
   server: {
     port: 3000,

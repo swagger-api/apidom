51a3aa2ef4172760a0e3d75ece889209067b237f
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-12-02 09:45:57 +0100

    build(playground): simplify vite config & remove clutter from output

--- packages/apidom-playground/vite.config.mjs
+++ packages/apidom-playground/vite.config.mjs
@@ -2,25 +2,27 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'remove-sourcemaps',
-      transform(code) {
-        return {
-          code,
-          map: { mappings: '' },
-        };
-      },
-    },
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
+    sourcemap: false,
+    sourcemapExcludeSources: true,
   },
   assetsInclude: ['**/*.wasm'],
   build: {
-    target: 'esnext',
+    sourcemap: false,
+    sourcemapExcludeSources: true,
+    target: '',
     outDir: 'build',
-    chunkSizeWarningLimit: 825,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      external: ['fs', 'path'],
+      onwarn(warning, warn) {
+        if (warning.message.includes('Use of eval')) return;
+        if (warning.message.includes('Module "fs" has been externalized')) return;
+        if (warning.message.includes('Module "path" has been externalized')) return;
+        warn(warning);
+      },
+    },
   },
 });

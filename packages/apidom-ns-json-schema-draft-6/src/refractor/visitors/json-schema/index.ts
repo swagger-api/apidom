86ddd8b029e5e98d00c0551e0bca228d2ef5522e
Oliwia Rogala <oliwia.rogala@smartbear.com>
2025-01-17 12:19:44 +0100

    feat(ns-json-schema-draft-6): handle inheritance of $schema and $id (#4658)
    Refs #1819

--- packages/apidom-ns-json-schema-draft-6/src/refractor/visitors/json-schema/index.ts
+++ packages/apidom-ns-json-schema-draft-6/src/refractor/visitors/json-schema/index.ts
@@ -4,9 +4,12 @@ import { ObjectElement, BooleanElement } from '@swagger-api/apidom-core';
 import {
   FixedFieldsVisitor,
   FixedFieldsVisitorOptions,
+  ParentSchemaAwareVisitor,
+  ParentSchemaAwareVisitorOptions,
   FallbackVisitor,
   FallbackVisitorOptions,
   SpecPath,
+  JSONSchemaVisitor as JSONSchemaDraft4Visitor,
 } from '@swagger-api/apidom-ns-json-schema-draft-4';
 
 import JSONSchemaElement from '../../../elements/JSONSchema.ts';
@@ -16,12 +19,17 @@ import JSONSchemaElement from '../../../elements/JSONSchema.ts';
  */
 export interface JSONSchemaVisitorOptions
   extends FixedFieldsVisitorOptions,
+    ParentSchemaAwareVisitorOptions,
     FallbackVisitorOptions {}
 
 /**
  * @public
  */
-class JSONSchemaVisitor extends Mixin(FixedFieldsVisitor, FallbackVisitor) {
+class JSONSchemaVisitor extends Mixin(
+  FixedFieldsVisitor,
+  ParentSchemaAwareVisitor,
+  FallbackVisitor,
+) {
   declare public element: JSONSchemaElement;
 
   declare protected readonly specPath: SpecPath<['document', 'objects', 'JSONSchema']>;
@@ -31,8 +39,18 @@ class JSONSchemaVisitor extends Mixin(FixedFieldsVisitor, FallbackVisitor) {
     this.specPath = always(['document', 'objects', 'JSONSchema']);
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  get defaultDialectIdentifier(): string {
+    return 'http://json-schema.org/draft-06/schema#';
+  }
+
   ObjectElement(objectElement: ObjectElement) {
     this.element = new JSONSchemaElement();
+    this.handleDialectIdentifier(objectElement);
+    this.handleSchemaIdentifier(objectElement);
+
+    // for further processing consider this Schema Element as parent for all embedded Schema Elements
+    this.parent = this.element;
 
     return FixedFieldsVisitor.prototype.ObjectElement.call(this, objectElement);
   }
@@ -42,6 +60,18 @@ class JSONSchemaVisitor extends Mixin(FixedFieldsVisitor, FallbackVisitor) {
     this.element.classes.push('boolean-json-schema');
 
     return result;
+  }
+
+  handleDialectIdentifier(objectElement: ObjectElement): void {
+    return JSONSchemaDraft4Visitor.prototype.handleDialectIdentifier.call(this, objectElement);
+  }
+
+  handleSchemaIdentifier(objectElement: ObjectElement, identifierKeyword: string = '$id'): void {
+    return JSONSchemaDraft4Visitor.prototype.handleSchemaIdentifier.call(
+      this,
+      objectElement,
+      identifierKeyword,
+    );
   }
 }
 

fde049b78229aad342a97f2e1f7b564fea4889d9
Vladimir Gorej <vladimir.gorej@gmail.com>
2025-04-23 11:57:45 +0200

    fix: schema object

--- packages/apidom-ls/src/config/openapi/schema/completion.ts
+++ packages/apidom-ls/src/config/openapi/schema/completion.ts
@@ -2,5 +2,11 @@ import jsonSchema202012completion from '../../json-schema/2020-12/json-schema/co
 import { compose, assoc } from '../../json-schema/2020-12/target-specs.ts';
 import commonSchemaCompletion from '../../common/schema/completion.ts';
 import { OpenAPI31 } from '../target-specs.ts';
+import { ApidomCompletionItem } from '../../../apidom-language-types.ts';
 
-export default compose([jsonSchema202012completion, commonSchemaCompletion], assoc(OpenAPI31));
+const completion: ApidomCompletionItem[] = [
+  ...compose([jsonSchema202012completion], assoc(OpenAPI31)),
+  ...commonSchemaCompletion,
+];
+
+export default completion;

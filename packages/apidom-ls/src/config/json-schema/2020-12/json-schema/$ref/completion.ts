0a72bd3e93617d327acad026b9aea18fca97bb46
Vladimir Gorej <vladimir.gorej@gmail.com>
2025-04-23 10:50:04 +0200

    fix: typo

--- packages/apidom-ls/src/config/json-schema/2020-12/json-schema/$ref/completion.ts
+++ packages/apidom-ls/src/config/json-schema/2020-12/json-schema/$ref/completion.ts
@@ -18,7 +18,7 @@ const completion: ApidomCompletionItem[] = [
     documentation: {
       kind: 'markdown',
       value:
-        'The "$ref" keyword is an applicator that is used to reference a statically identified schema. Its results are the results of the referenced schema. Note that this definition of how the results are determined means that other keywords can appear alongside of "$ref" in the same schema object.\n\\\n\\\nhe value of the "$ref" keyword MUST be a string which is a URI-Reference. Resolved against the current URI base, it produces the URI of the schema to apply. This resolution is safe to perform on schema load, as the process of evaluating an instance cannot change how the reference resolves.',
+        'The "$ref" keyword is an applicator that is used to reference a statically identified schema. Its results are the results of the referenced schema. Note that this definition of how the results are determined means that other keywords can appear alongside of "$ref" in the same schema object.\n\\\n\\\nThe value of the "$ref" keyword MUST be a string which is a URI-Reference. Resolved against the current URI base, it produces the URI of the schema to apply. This resolution is safe to perform on schema load, as the process of evaluating an instance cannot change how the reference resolves.',
     },
     targetSpecs: JSONSchema202012,
   },

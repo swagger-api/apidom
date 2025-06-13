a268157926cfa3d19033d30460ac2b6bea683626
lukaszzazulak <155975313+lukaszzazulak@users.noreply.github.com>
2025-06-13 09:19:29 +0200

    Update packages/apidom-ls/src/config/openapi/parameter/lint/style--type.ts
    Co-authored-by: Vladimír Gorej <vladimir.gorej@gmail.com>

--- packages/apidom-ls/src/config/openapi/parameter/lint/style--type.ts
+++ packages/apidom-ls/src/config/openapi/parameter/lint/style--type.ts
@@ -5,7 +5,7 @@ import { LinterMeta } from '../../../../apidom-language-types.ts';
 import { OpenAPI3 } from '../../target-specs.ts';
 
 const styleEqualsLint: LinterMeta = {
-  code: ApilintCodes.OPENAPI3_0_PARAMETER_FIELD_STYLE_VALUES,
+  code: ApilintCodes.OPENAPI3_0_PARAMETER_FIELD_STYLE_EQUALS,
   source: 'apilint',
   message: 'style must be one of allowed values: form, simple',
   severity: DiagnosticSeverity.Error,

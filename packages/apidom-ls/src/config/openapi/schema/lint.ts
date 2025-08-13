3d1dae69d6befef48f807dcb3935c0eda1a55328
Robert Hebel <robert.hebel@smartbear.com>
2025-08-13 16:25:35 +0200

    feat: add schema rule

--- packages/apidom-ls/src/config/openapi/schema/lint.ts
+++ packages/apidom-ls/src/config/openapi/schema/lint.ts
@@ -82,6 +82,7 @@ import minimumValueLint from '../../common/schema/lint/minimum-maximum--value.ts
 import minLengthValueLint from '../../common/schema/lint/min-length-max-length--value.ts';
 import minPropertiesValueLint from '../../common/schema/lint/min-properties-max-properties--value.ts';
 import minItemsValueLint from '../../common/schema/lint/min-items-max-items--value.ts';
+import readOnlyWriteOnlyLint from '../../common/schema/lint/read-only-write-only.ts';
 
 const schemaLints = [
   ...compose([jsonSchema202012Lint], assoc(OpenAPI31)),
@@ -153,6 +154,7 @@ const schemaLints = [
   propertyNamesNonObjectLint,
   propertyNamesTypeLint,
   readOnlyTypeLint,
+  readOnlyWriteOnlyLint,
   requiredDefinedLint,
   requiredNonObjectLint,
   requiredTypeLint,

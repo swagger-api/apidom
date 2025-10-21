33995da10128fe3282c6ba7d29cb19021803aeca
lukasz.zazulak <lukasz.zazulak@smartbear.com>
2025-10-21 11:56:55 +0200

    feat: async v3 binding elements

---
+++ packages/apidom-ns-asyncapi-3/src/elements/bindings/anypointmq/AnypointmqChannelBinding.ts
@@ -0,0 +1,5 @@
+import { AnypointmqChannelBindingElement } from '@swagger-api/apidom-ns-asyncapi-2';
+
+class AnypointmqChannelBinding extends AnypointmqChannelBindingElement {}
+
+export default AnypointmqChannelBinding;

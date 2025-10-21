7eb7bf111212a446d8930974391fd1e19085433e
lukasz.zazulak <lukasz.zazulak@smartbear.com>
2025-10-21 13:15:04 +0200

    feat: async v3 binding elements

---
+++ packages/apidom-ns-asyncapi-3/src/elements/bindings/ibmmq/IbmmqOperationBinding.ts
@@ -0,0 +1,5 @@
+import { IbmmqOperationBindingElement } from '@swagger-api/apidom-ns-asyncapi-2';
+
+class IbmmqOperationBinding extends IbmmqOperationBindingElement {}
+
+export default IbmmqOperationBinding;
---
+++ packages/apidom-ns-asyncapi-3/src/elements/bindings/ibmmq/IbmmqServerBinding.ts
@@ -0,0 +1,5 @@
+import { IbmmqServerBindingElement } from '@swagger-api/apidom-ns-asyncapi-2';
+
+class IbmmqServerBinding extends IbmmqServerBindingElement {}
+
+export default IbmmqServerBinding;

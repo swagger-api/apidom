71cc9faa80511fc2f46ee8fc73af890153cfada8
lukasz.zazulak <lukasz.zazulak@smartbear.com>
2025-10-21 13:17:35 +0200

    feat: async v3 binding elements

---
+++ packages/apidom-ns-asyncapi-3/src/elements/bindings/jms/JmsOperationBinding.ts
@@ -0,0 +1,5 @@
+import { JmsOperationBindingElement } from '@swagger-api/apidom-ns-asyncapi-2';
+
+class JmsOperationBinding extends JmsOperationBindingElement {}
+
+export default JmsOperationBinding;
---
+++ packages/apidom-ns-asyncapi-3/src/elements/bindings/jms/JmsServerBinding.ts
@@ -0,0 +1,5 @@
+import { JmsServerBindingElement } from '@swagger-api/apidom-ns-asyncapi-2';
+
+class JmsServerBinding extends JmsServerBindingElement {}
+
+export default JmsServerBinding;

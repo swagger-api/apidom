b072f5ff4063b724a30167d13994e28fc382a179
ShikhaSaboo <shikhacka@gmail.com>
2025-11-04 18:01:27 +0530

    feat: fix eslint issue

--- packages/apidom-ns-asyncapi-3/test/index.ts
+++ packages/apidom-ns-asyncapi-3/test/index.ts
@@ -1,4 +1,3 @@
 describe('apidom-ns-asyncapi-3 basic refractor', () => {
-  it('parses a simple asyncapi v3 fixture into elements', () => {
-  });
+  it('parses a simple asyncapi v3 fixture into elements', () => {});
 });

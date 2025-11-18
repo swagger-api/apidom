85f531f5e5f0cc14de7d6bd77154050a3fafcbd9
ShikhaSaboo <shikhacka@gmail.com>
2025-11-18 12:32:27 +0530

    chore: fix lint issue

--- packages/apidom-parser-adapter-asyncapi-json-3/test/mocha-bootstrap.ts
+++ packages/apidom-parser-adapter-asyncapi-json-3/test/mocha-bootstrap.ts
@@ -1,10 +1,9 @@
 import * as chai from 'chai';
 import { jestSnapshotPlugin, addSerializer } from 'mocha-chai-jest-snapshot';
-
-// eslint-disable-next-line import/no-relative-packages
-import * as jestApiDOMSerializer from '../../../scripts/jest-serializer-apidom.mjs';
-// eslint-disable-next-line import/no-relative-packages
-import * as jestStringSerializer from '../../../scripts/jest-serializer-string.mjs';
+// @ts-ignore
+import * as jestApiDOMSerializer from 'apidom-monorepo/scripts/jest-serializer-apidom.mjs';
+// @ts-ignore
+import * as jestStringSerializer from 'apidom-monorepo/scripts/jest-serializer-string.mjs';
 
 chai.use(jestSnapshotPlugin());
 addSerializer(jestApiDOMSerializer);

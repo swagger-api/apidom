b83bfc1bb1d1efbb5828f0264d056937d43f9634
ShikhaSaboo <shikhacka@gmail.com>
2025-11-18 14:02:14 +0530

    chore: update import

--- packages/apidom-parser-adapter-asyncapi-json-3/test/mocha-bootstrap.ts
+++ packages/apidom-parser-adapter-asyncapi-json-3/test/mocha-bootstrap.ts
@@ -1,9 +1,10 @@
 import * as chai from 'chai';
 import { jestSnapshotPlugin, addSerializer } from 'mocha-chai-jest-snapshot';
+
 // @ts-ignore
-import * as jestApiDOMSerializer from 'apidom-monorepo/scripts/jest-serializer-apidom.mjs';
+import * as jestApiDOMSerializer from '../../../scripts/jest-serializer-apidom.mjs';
 // @ts-ignore
-import * as jestStringSerializer from 'apidom-monorepo/scripts/jest-serializer-string.mjs';
+import * as jestStringSerializer from '../../../scripts/jest-serializer-string.mjs';
 
 chai.use(jestSnapshotPlugin());
 addSerializer(jestApiDOMSerializer);

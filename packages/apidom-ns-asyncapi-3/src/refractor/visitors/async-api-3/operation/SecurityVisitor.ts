54e3e275adbff596932bb1edf437a13b4fae137e
ShikhaSaboo <shikhacka@gmail.com>
2025-12-12 15:03:31 +0530

    chore: resolve review comment

--- packages/apidom-ns-asyncapi-3/src/refractor/visitors/async-api-3/operation/SecurityVisitor.ts
+++ packages/apidom-ns-asyncapi-3/src/refractor/visitors/async-api-3/operation/SecurityVisitor.ts
@@ -30,7 +30,7 @@ class SecurityVisitor extends Mixin(SpecificationVisitor, FallbackVisitor) {
 
       if (isReferenceLikeElement(item)) {
         element = this.toRefractedElement(['document', 'objects', 'Reference'], item);
-        element.setMetaProperty('referenced-element', 'SecurityScheme');
+        element.setMetaProperty('referenced-element', 'securityScheme');
       } else {
         element = this.toRefractedElement(['document', 'objects', 'SecurityScheme'], item);
       }

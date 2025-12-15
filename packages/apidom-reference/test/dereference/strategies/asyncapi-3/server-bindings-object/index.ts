c9ba7487be7883c9594cd1471ad141b836a38243
ShikhaSaboo <shikhacka@gmail.com>
2025-12-15 15:52:00 +0530

    test(ref): test for resolving ref for server bindings in asyncapi3

---
+++ packages/apidom-reference/test/dereference/strategies/asyncapi-3/server-bindings-object/index.ts
@@ -0,0 +1,33 @@
+import path from 'node:path';
+import { assert } from 'chai';
+import { toValue } from '@swagger-api/apidom-core';
+import { mediaTypes } from '@swagger-api/apidom-ns-asyncapi-3';
+import { fileURLToPath } from 'node:url';
+
+import { loadJsonFile } from '../../../../helpers.ts';
+import { dereference } from '../../../../../src/index.ts';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const rootFixturePath = path.join(__dirname, 'fixtures');
+
+describe('dereference', function () {
+  context('strategies', function () {
+    context('asyncapi-3', function () {
+      context('Server Bindings Object', function () {
+        context('given in components/securitySchemes field', function () {
+          const fixturePath = path.join(rootFixturePath, 'server-object');
+
+          specify('should dereference', async function () {
+            const rootFilePath = path.join(fixturePath, 'root.json');
+            const actual = await dereference(rootFilePath, {
+              parse: { mediaType: mediaTypes.latest('json') },
+            });
+            const expected = loadJsonFile(path.join(fixturePath, 'dereferenced.json'));
+
+            assert.deepEqual(toValue(actual), expected);
+          });
+        });
+      });
+    });
+  });
+});

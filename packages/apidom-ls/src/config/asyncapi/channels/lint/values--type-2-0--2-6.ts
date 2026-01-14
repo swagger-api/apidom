79eabad90baa171fb7faaef568de22f3683c527f
Oliwia Rogala <oliwia.rogala@smartbear.com>
2026-01-14 09:27:56 +0100

    feat(ls): update names of async 2 rules for modified keywords

--- packages/apidom-ls/src/config/asyncapi/channels/lint/values--type-2-0--2-6.ts
+++ packages/apidom-ls/src/config/asyncapi/channels/lint/values--type-2-0--2-6.ts
@@ -4,18 +4,17 @@ import ApilintCodes from '../../../codes.ts';
 import { LinterMeta } from '../../../../apidom-language-types.ts';
 import { AsyncAPI2 } from '../../target-specs.ts';
 
-const schemasValuesTypeLint: LinterMeta = {
-  code: ApilintCodes.ASYNCAPI2_COMPONENTS_FIELD_SCHEMAS_VALUES_TYPE,
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const valuesType2_0__2_6Lint: LinterMeta = {
+  code: ApilintCodes.ASYNCAPI2_CHANNELS_VALUES_TYPE,
   source: 'apilint',
-  message: '"schemas" members must be either a schema object or a boolean JSON schema',
+  message: 'Channels Object values must be of Channel Item Object shape',
   severity: DiagnosticSeverity.Error,
   linterFunction: 'apilintChildrenOfElementsOrClasses',
-  linterParams: [['schema', 'boolean']],
+  linterParams: [['channelItem']],
   marker: 'key',
-  markerTarget: 'schemas',
-  target: 'schemas',
   data: {},
   targetSpecs: AsyncAPI2,
 };
 
-export default schemasValuesTypeLint;
+export default valuesType2_0__2_6Lint;

93107984ed18794be94720b99495a09f8c4dbfc6
Oliwia Rogala <oliwia.rogala@smartbear.com>
2026-01-14 10:03:44 +0100

    fix(ls): use codes specific to AsyncAPI 3 Channel Object

--- packages/apidom-ls/src/config/asyncapi/channel/lint/bindings--type.ts
+++ packages/apidom-ls/src/config/asyncapi/channel/lint/bindings--type.ts
@@ -5,7 +5,7 @@ import { LinterMeta } from '../../../../apidom-language-types.ts';
 import { AsyncAPI3 } from '../../target-specs.ts';
 
 const bindingsTypeLint: LinterMeta = {
-  code: ApilintCodes.ASYNCAPI2_CHANNEL_ITEM_FIELD_BINDINGS_TYPE,
+  code: ApilintCodes.ASYNCAPI3_CHANNEL_FIELD_BINDINGS_TYPE,
   source: 'apilint',
   message: 'bindings must be an object',
   severity: DiagnosticSeverity.Error,
--- packages/apidom-ls/src/config/asyncapi/channel/lint/parameters--type.ts
+++ packages/apidom-ls/src/config/asyncapi/channel/lint/parameters--type.ts
@@ -5,7 +5,7 @@ import { LinterMeta } from '../../../../apidom-language-types.ts';
 import { AsyncAPI3 } from '../../target-specs.ts';
 
 const parametersTypeLint: LinterMeta = {
-  code: ApilintCodes.ASYNCAPI2_CHANNEL_ITEM_FIELD_PARAMETERS_TYPE,
+  code: ApilintCodes.ASYNCAPI3_CHANNEL_FIELD_PARAMETERS_TYPE,
   source: 'apilint',
   message: '"parameters" must be of Parameters Object shape',
   severity: DiagnosticSeverity.Error,

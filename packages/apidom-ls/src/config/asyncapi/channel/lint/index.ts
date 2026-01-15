818116d231eac29da336fb03aa25096de309b4fb
Oliwia Rogala <oliwia.rogala@smartbear.com>
2026-01-15 15:14:38 +0100

    fix(ls): add missing rules for channel ref

--- packages/apidom-ls/src/config/asyncapi/channel/lint/index.ts
+++ packages/apidom-ls/src/config/asyncapi/channel/lint/index.ts
@@ -2,7 +2,16 @@ import allowedFieldsLint from './allowed-fields.ts';
 import descriptionTypeLint from './description--type.ts';
 import parametersTypeLint from './parameters--type.ts';
 import bindingsTypeLint from './bindings--type.ts';
+import $refValidLint from './$ref--valid.ts';
+import $refNoSiblingsLint from './$ref--no-siblings.ts';
 
-const lints = [descriptionTypeLint, parametersTypeLint, bindingsTypeLint, allowedFieldsLint];
+const lints = [
+  descriptionTypeLint,
+  parametersTypeLint,
+  bindingsTypeLint,
+  allowedFieldsLint,
+  $refValidLint,
+  $refNoSiblingsLint,
+];
 
 export default lints;

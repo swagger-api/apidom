4a5629d575849665360c87f864efa72023094179
Robert Hebel <robert.hebel@smartbear.com>
2026-01-21 11:00:58 +0100

    feat(ls): add AsyncAPI 3 completion for new keywords

--- packages/apidom-ls/src/config/asyncapi/operation-reply-address/completion.ts
+++ packages/apidom-ls/src/config/asyncapi/operation-reply-address/completion.ts
@@ -1,5 +1,39 @@
-import { ApidomCompletionItem } from '../../../apidom-language-types.ts';
+import {
+  ApidomCompletionItem,
+  CompletionFormat,
+  CompletionType,
+} from '../../../apidom-language-types.ts';
+import { AsyncAPI3 } from '../target-specs.ts';
 
-const completion: ApidomCompletionItem[] = [];
+const completion: ApidomCompletionItem[] = [
+  {
+    label: 'location',
+    insertText: 'location',
+    kind: 14,
+    format: CompletionFormat.QUOTED,
+    type: CompletionType.PROPERTY,
+    insertTextFormat: 2,
+    documentation: {
+      kind: 'markdown',
+      value:
+        '**REQUIRED.** A [runtime expression](https://www.asyncapi.com/docs/reference/specification/v3.0.0#runtimeExpression) that specifies the location of the reply address.',
+    },
+    targetSpecs: AsyncAPI3,
+  },
+  {
+    label: 'description',
+    insertText: 'description',
+    kind: 14,
+    format: CompletionFormat.QUOTED,
+    type: CompletionType.PROPERTY,
+    insertTextFormat: 2,
+    documentation: {
+      kind: 'markdown',
+      value:
+        'An optional description of the address. [CommonMark syntax](https://spec.commonmark.org/) can be used for rich text representation.',
+    },
+    targetSpecs: AsyncAPI3,
+  },
+];
 
 export default completion;

5e29dad9488cb1d3bc021105b0967cbfc2c2ef94
robert-hebel-sb <robert.hebel@smartbear.com>
2026-02-03 10:05:07 +0100

    feat(ls): add AsyncAPI 3 documentation for new keywords (#5102)

--- packages/apidom-ls/src/config/asyncapi/tag/documentation.ts
+++ packages/apidom-ls/src/config/asyncapi/tag/documentation.ts
@@ -22,11 +22,6 @@ const documentation = [
     targetSpecs: [...AsyncAPI2, ...AsyncAPI3],
   },
   {
-    target: 'externalDocs',
-    docs: '[External Documentation Object](https://www.asyncapi.com/docs/reference/specification/v3.0.0#externalDocumentationObject) &#124; [Reference Object](https://www.asyncapi.com/docs/reference/specification/v3.0.0#referenceObject)\n\\\n\\\nAdditional external documentation for this tag.',
-    targetSpecs: AsyncAPI3,
-  },
-  {
     docs: '#### [Tag Object](https://www.asyncapi.com/docs/reference/specification/v2.6.0#tagObject)\n\nAllows adding meta data to a single tag.\n\n##### Fixed Fields\nField Name | Type | Description\n---|:---:|---\nname | `string` | **REQUIRED.** The name of the tag.\ndescription | `string` | A short description for the tag. [CommonMark syntax](https://spec.commonmark.org/) can be used for rich text representation.\nexternalDocs | [External Documentation Object](https://www.asyncapi.com/docs/reference/specification/v2.6.0#externalDocumentationObject) | Additional external documentation for this tag.\n\nThis object MAY be extended with [Specification Extensions](https://www.asyncapi.com/docs/reference/specification/v2.6.0#specificationExtensions).\n\n##### Tag Object Example\n\n\n\\\nJSON\n```json\n{\n\t"name": "user",\n\t"description": "User-related messages"\n}\n```\n\n\n\\\nYAML\n```yaml\nname: user\ndescription: User-related messages\n```',
     targetSpecs: AsyncAPI2,
   },

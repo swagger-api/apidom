42b719218d65f06eeaba991ea8f4f1e677d00430
ShikhaSaboo <shikhacka@gmail.com>
2026-04-24 13:19:24 +0530

    chore: add lint fix

--- packages/apidom-ls/src/config/asyncapi/asyncapi3/lint/allowed-fields.ts
+++ packages/apidom-ls/src/config/asyncapi/asyncapi3/lint/allowed-fields.ts
@@ -5,26 +5,26 @@ import { LinterMeta } from '../../../../apidom-language-types.ts';
 import { AsyncAPI3 } from '../../target-specs.ts';
 
 const allowedFieldsLint: LinterMeta = {
-	code: ApilintCodes.NOT_ALLOWED_FIELDS,
-	source: 'apilint',
-	message: 'Object includes not allowed fields',
-	severity: DiagnosticSeverity.Error,
-	linterFunction: 'allowedFields',
-	linterParams: [
-		[
-			'asyncapi',
-			'id',
-			'info',
-			'servers',
-			'defaultContentType',
-			'channels',
-			'operations',
-			'components',
-		],
-		'x-',
-	],
-	marker: 'key',
-	targetSpecs: AsyncAPI3,
+  code: ApilintCodes.NOT_ALLOWED_FIELDS,
+  source: 'apilint',
+  message: 'Object includes not allowed fields',
+  severity: DiagnosticSeverity.Error,
+  linterFunction: 'allowedFields',
+  linterParams: [
+    [
+      'asyncapi',
+      'id',
+      'info',
+      'servers',
+      'defaultContentType',
+      'channels',
+      'operations',
+      'components',
+    ],
+    'x-',
+  ],
+  marker: 'key',
+  targetSpecs: AsyncAPI3,
 };
 
 export default allowedFieldsLint;

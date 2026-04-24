c99277e9efca2f7154bf44a8d9944138fd4ce60b
Shikha Saboo Lakhotia <102152915+cka121@users.noreply.github.com>
2026-04-24 15:01:55 +0530

    feat: add missing lint rule for allowed fields for AsyncAPI3 (swg-16802) (#5165)

--- packages/apidom-ls/src/config/asyncapi/asyncapi3/lint/allowed-fields.ts
+++ packages/apidom-ls/src/config/asyncapi/asyncapi3/lint/allowed-fields.ts
@@ -1,4 +1,30 @@
-import { LinterMeta } from '../../../../apidom-language-types.ts';
+import { DiagnosticSeverity } from 'vscode-languageserver-types';
 
-const allowedFieldsLint: LinterMeta = {};
+import ApilintCodes from '../../../codes.ts';
+import { LinterMeta } from '../../../../apidom-language-types.ts';
+import { AsyncAPI3 } from '../../target-specs.ts';
+
+const allowedFieldsLint: LinterMeta = {
+  code: ApilintCodes.NOT_ALLOWED_FIELDS,
+  source: 'apilint',
+  message: 'Object includes not allowed fields',
+  severity: DiagnosticSeverity.Error,
+  linterFunction: 'allowedFields',
+  linterParams: [
+    [
+      'asyncapi',
+      'id',
+      'info',
+      'servers',
+      'defaultContentType',
+      'channels',
+      'operations',
+      'components',
+    ],
+    'x-',
+  ],
+  marker: 'key',
+  targetSpecs: AsyncAPI3,
+};
+
 export default allowedFieldsLint;

a23130ae78c598153ea21dbaa66338a81cc9c2e7
Robert Hebel <robert.hebel@smartbear.com>
2026-08-28 10:53:50 +0200

    docs(ls): fix anchor links for 3.1.x spec

--- packages/apidom-ns-openapi-3-2/src/refractor/plugins/normalize-parameters.ts
+++ packages/apidom-ns-openapi-3-2/src/refractor/plugins/normalize-parameters.ts
@@ -37,7 +37,7 @@ const plugin =
     /**
      * Establishes identity between two Parameter Objects.
      *
-     * {@link https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.2.md#parameter-object}
+     * {@link https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.2.0.md#operation-parameters}
      */
     const parameterEquals = (parameter1: ParameterElement, parameter2: ParameterElement) => {
       if (!predicates.isParameterElement(parameter1)) return false;
